1d1d5f527e712e6d8fb12631c9ebfb987f693309
Mark Little <mlittle@redhat.com>
2017-04-25 20:17:56 +0100

    Moved for restructuring.
    https://github.com/nmcl/scratch/issues/11

---
+++ excelsior/extended/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export CLASSPATH=.:arjuna-5.6.0.Final-SNAPSHOT.jar:common-5.2.12.Final-SNAPSHOT.jar:txoj-5.6.0.Final-SNAPSHOT.jar:jboss-logging.jar:$CLASSPATH
+
+javac -classpath $CLASSPATH AtomicTest.java
+
+java -classpath $CLASSPATH AtomicTest
