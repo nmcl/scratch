985b0e70cf3df46da82c1311842f92aed139bc06
Mark Little <mlittle@redhat.com>
2017-04-26 18:17:20 +0100

    Updated common jar

--- excelsior/extended/run_performance.sh
+++ excelsior/extended/run_performance.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# JETVMPROP="-Djet.gc.heaplimit:1174244058 -Djet.gc.ratio:0 "
+# JETVMPROP="-Djet.gc.heaplimit:0 -Djet.gc.ratio:11 "
 # export JETVMPROP
 cd /Users/marklittle/github/scratch/excelsior/extended
 "/Users/marklittle/github/scratch/excelsior/extended/performance"
