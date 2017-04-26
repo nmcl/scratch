5f4618d5baef60afa15a6975b450a401e8c6d52c
Mark Little <mlittle@redhat.com>
2017-04-26 18:12:58 +0100

    Tracking down property file issue

--- excelsior/basic/run_example.sh
+++ excelsior/basic/run_example.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # JETVMPROP="-Djet.gc.heaplimit:0 -Djet.gc.ratio:11 "
 # export JETVMPROP
-cd /Users/marklittle/github/scratch/excelsior
-"/Users/marklittle/github/scratch/excelsior/example"
+cd /Users/marklittle/github/scratch/excelsior/basic
+"/Users/marklittle/github/scratch/excelsior/basic/example"
