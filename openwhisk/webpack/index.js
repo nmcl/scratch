0c03b310c4939147d75ffb72c8a8ead81af4c554
nmcl <mlittle@redhat.com>
2017-12-31 16:09:12 +0000

    Initial webpack example

---
+++ openwhisk/webpack/index.js
@@ -0,0 +1,6 @@
+function myAction(args) {
+    const leftPad = require("left-pad")
+    const lines = args.lines || [];
+    return { padded: lines.map(l => leftPad(l, 30, ".")) }
+}
+global.main = myAction;
