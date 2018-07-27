dc6fe7f52182fbce3c8f99a0812af84c95f0afb4
Mark Little <mlittle@redhat.com>
2018-07-26 21:36:35 -0400

    Modified version

---
+++ graalvm/sample/Blender_mod.java
@@ -0,0 +1,65 @@
+public class Blender_mod {
+
+    private static class Color {
+        double r, g, b;
+
+        private Color(double r, double g, double b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public static Color black() {
+            return new Color(0, 0, 0);
+        }
+
+        public void add(Color other) {
+            r += other.r;
+            g += other.g;
+            b += other.b;
+        }
+
+        public void add(double nr, double ng, double nb) {
+            r += nr;
+            g += ng;
+            b += nb;
+        }
+
+        public void multiply(double factor) {
+            r *= factor;
+            g *= factor;
+            b *= factor;
+        }
+    }
+
+    private static final Color[][][] colors = new Color[100][100][100];
+
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            long t = System.nanoTime();
+            for (int i = 0; i < 100; i++) {
+                initialize(new Color(j / 20, 0, 1));
+            }
+            long d = System.nanoTime() - t;
+            System.out.println(d / 1_000_000 + " ms");
+        }
+    }
+
+    private static void initialize(Color id) {
+        for (int x = 0; x < colors.length; x++) {
+            Color[][] plane = colors[x];
+            for (int y = 0; y < plane.length; y++) {
+                Color[] row = plane[y];
+                for (int z = 0; z < row.length; z++) {
+                    Color color = Color.black();
+                    color.add(x / (double) colors.length, y / (double) row.length, z / (double) row.length);
+                    color.add(id);
+                    color.multiply(1 / 4d);
+                    // PEA moves allocation of `color` to this point so above
+                    // computation avoids memory accesses for its fields.
+		    if (z == y && y == x) row[z] = color;
+                }
+            }
+        }
+    }
+}
