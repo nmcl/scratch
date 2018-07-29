6180f6e93736408a470a685a213b8c31a0dc9f67
Mark Little <mlittle@redhat.com>
2018-07-29 08:45:26 -0400

    hacked narayana

---
+++ graalvm/transactions/arjunacore/src/narayana/rts/at/integration/src/test/java/org/jboss/narayana/rest/integration/test/integration/RecoveryIntegrationTestCase.java
@@ -0,0 +1,156 @@
+package org.jboss.narayana.rest.integration.test.integration;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.jbossts.star.provider.HttpResponseException;
+import org.jboss.jbossts.star.util.media.txstatusext.TransactionStatusElement;
+import org.jboss.narayana.rest.integration.api.Prepared;
+import org.jboss.narayana.rest.integration.api.Vote;
+import org.jboss.narayana.rest.integration.test.common.LoggingParticipant;
+import org.jboss.narayana.rest.integration.test.common.RestATManagementResource;
+import org.jboss.narayana.rest.integration.test.common.TransactionalService;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import java.io.File;
+
+/**
+ * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
+ */
+@RunWith(Arquillian.class)
+public final class RecoveryIntegrationTestCase extends AbstractIntegrationTestCase {
+
+    private static final String DEPENDENCIES = "Dependencies: org.jboss.narayana.rts,org.jboss.jts,org.codehaus.jettison\n";
+
+    private static final int RECOVERY_PERIOD = 4;
+
+    private static final int RECOVERY_WAIT_CYCLES = 8;
+
+    private static final String VM_ARGUMENTS = System.getProperty("server.jvm.args").trim()
+            + " -Dcom.arjuna.ats.arjuna.recovery.periodicRecoveryPeriod=" + RECOVERY_PERIOD;
+
+    private static final String BYTEMAN_ARGUMENTS = "-Dorg.jboss.byteman.verbose -Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:lib/byteman.jar=script:scripts/@BMScript@.btm,boot:lib/byteman.jar,listener:true";
+
+    @Deployment(name = DEPLOYMENT_NAME, managed = false, testable = false)
+    public static WebArchive getDeployment() {
+        return ShrinkWrap.create(WebArchive.class, DEPLOYMENT_NAME + ".war")
+                .addPackages(false, LoggingParticipant.class.getPackage()).addAsWebInfResource(new File("web.xml"), "web.xml")
+                .addAsManifestResource(new StringAsset(DEPENDENCIES), "MANIFEST.MF");
+    }
+
+    @After
+    public void after() {
+        super.after();
+        stopContainer();
+    }
+
+    @Test
+    public void testCrashAfterPrepare() throws Exception {
+        startContainer(VM_ARGUMENTS + " " + BYTEMAN_ARGUMENTS.replace("@BMScript@", "CrashAfterPrepare"));
+
+        txSupport.startTx();
+
+        enlistParticipant(txSupport.getDurableParticipantEnlistmentURI(), new Prepared());
+        enlistParticipant(txSupport.getDurableParticipantEnlistmentURI(), new Prepared());
+
+        Assert.assertEquals(2, txSupport.getTransactionInfo().getTwoPhaseAware().size());
+
+        try {
+            // JVM is killed here.
+            txSupport.commitTx();
+        } catch (HttpResponseException e) {
+        }
+
+        restartContainer(VM_ARGUMENTS);
+        registerDeserializer();
+
+        TransactionStatusElement status;
+        int cycles = 0;
+        JSONArray partricipantsInformation;
+
+        do {
+            Thread.sleep(RECOVERY_PERIOD * 2000);
+            partricipantsInformation = getParticipantsInformation();
+        } while (cycles++ < RECOVERY_WAIT_CYCLES && partricipantsInformation.length() > 0);
+
+        if (partricipantsInformation.length() > 0) {
+            Assert.fail("Recovery failed");
+        }
+    }
+
+    @Test
+    public void testCrashBeforeCommit() throws Exception {
+        startContainer(VM_ARGUMENTS + " " + BYTEMAN_ARGUMENTS.replace("@BMScript@", "CrashBeforeCommit"));
+
+        txSupport.startTx();
+
+        enlistParticipant(txSupport.getDurableParticipantEnlistmentURI(), new Prepared());
+        enlistParticipant(txSupport.getDurableParticipantEnlistmentURI(), new Prepared());
+
+        Assert.assertEquals(2, txSupport.getTransactionInfo().getTwoPhaseAware().size());
+
+        try {
+            // JVM is killed here.
+            txSupport.commitTx();
+        } catch (HttpResponseException e) {
+        }
+
+        restartContainer(VM_ARGUMENTS);
+        registerDeserializer();
+
+        TransactionStatusElement status;
+        int cycles = 0;
+
+        do {
+            Thread.sleep(RECOVERY_PERIOD * 2000);
+            status = null;
+            try {
+                // Updates coordinator's active transactions list
+                txSupport.getTransactions();
+                // After successful recovery transaction is removed and 404 is returned.
+                status = txSupport.getTransactionInfo().getStatus();
+            } catch (HttpResponseException e) {
+            }
+        } while (status != null && cycles++ < RECOVERY_WAIT_CYCLES);
+
+        if (status != null) {
+            Assert.fail("Recovery failed unexpected status " + status);
+        }
+    }
+
+    private void enlistParticipant(final String enlistmentUrl, final Vote vote) throws Exception {
+        Response clientResponse = ClientBuilder.newClient().target(DEPLOYMENT_URL + "/" + TransactionalService.PATH_SEGMENT)
+                .queryParam("participantEnlistmentUrl", enlistmentUrl).queryParam("vote", vote.getClass().getName()).request()
+                .post(null);
+
+        Assert.assertEquals(200, clientResponse.getStatus());
+    }
+
+    private void registerDeserializer() throws Exception {
+        Response clientResponse = ClientBuilder.newClient().target(DEPLOYMENT_URL + "/" + TransactionalService.PATH_SEGMENT)
+                .request().put(null);
+
+        Assert.assertEquals(204, clientResponse.getStatus());
+    }
+
+    private JSONArray getParticipantsInformation() {
+        try {
+            final Response response = ClientBuilder.newClient().target(DEPLOYMENT_URL + "/"
+                    + RestATManagementResource.BASE_URL_SEGMENT + "/"
+                    + RestATManagementResource.PARTICIPANTS_URL_SEGMENT).request().get();
+            return new JSONArray(response.readEntity(String.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
+}
---
+++ graalvm/transactions/arjunacore/src/narayana/rts/at/tx/src/test/java/org/jboss/jbossts/star/test/CoordinatorTest.java
@@ -0,0 +1,307 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2010
+ * @author JBoss Inc.
+ */
+package org.jboss.jbossts.star.test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.jboss.jbossts.star.util.TxMediaType;
+import org.jboss.jbossts.star.util.TxStatusMediaType;
+import org.jboss.jbossts.star.util.TxSupport;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class CoordinatorTest extends BaseTest {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        startContainer(TXN_MGR_URL);
+    }
+
+    /*
+      * Note: TxSupport methods throw exceptions if unexpected status codes are returned.
+      * use the TxSupport.httpRequest(...) if you want to specify other status codes
+      */
+
+    // list transactions
+    @Test
+    public void testListTransactions() throws IOException
+    {
+        TxSupport[] txns = {new TxSupport(), new TxSupport()};
+        int txnCount = new TxSupport().txCount();
+
+        for (TxSupport txn : txns)
+            txn.startTx();
+
+        // there should be txns.length more transactions
+        Assert.assertEquals(txnCount + txns.length, txns[0].txCount());
+
+        for (TxSupport txn : txns)
+            txn.commitTx();
+
+        // the number of transactions should be back to the original number
+        Assert.assertEquals(txnCount, txns[0].txCount());
+
+    }
+
+    // 1PC commit abort
+    @Test
+    public void test1PCAbort() throws Exception
+    {
+        TxSupport txn = new TxSupport();
+        String pUrl = PURL;
+        String pid = null;
+        String pVal;
+
+        pid = modifyResource(txn, pUrl, pid, "p1", "v1");
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v1");
+
+        txn.startTx();
+        pid = enlistResource(txn, pUrl + "?pId=" + pid);
+
+        modifyResource(txn, pUrl, pid, "p1", "v2");
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v2");
+
+        txn.rollbackTx();
+
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v1");
+    }
+
+    // 1PC commit
+    @Test
+    public void test1PCCommit() throws Exception
+    {
+        TxSupport txn = new TxSupport();
+        String pUrl = PURL;
+        String pid = null;
+        String pVal;
+
+        pid = modifyResource(txn, pUrl, pid, "p1", "v1");
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v1");
+
+        txn.startTx();
+        pid = enlistResource(txn, pUrl + "?pId=" + pid);
+
+        modifyResource(txn, pUrl, pid, "p1", "v2");
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v2");
+
+        txn.commitTx();
+
+        pVal = getResourceProperty(txn, pUrl, pid, "p1");
+        Assert.assertEquals(pVal, "v2");
+    }
+
+    // 2PC commit
+    @Test
+    public void test2PC() throws Exception
+    {
+        TxSupport txn = new TxSupport();
+        String pUrl = PURL;
+        String[] pid = new String[2];
+        String[] pVal = new String[2];
+
+        for (int i = 0; i < pid.length; i++) {
+            pid[i] = modifyResource(txn, pUrl, null, "p1", "v1");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+
+            Assert.assertEquals(pVal[i], "v1");
+        }
+
+        txn.startTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            enlistResource(txn, pUrl + "?pId=" + pid[i]);
+
+            modifyResource(txn, pUrl, pid[i], "p1", "v2");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+
+            Assert.assertEquals(pVal[i], "v2");
+        }
+
+        txn.rollbackTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+            Assert.assertEquals(pVal[i], "v1");
+        }
+    }
+
+    @Test
+    public void test2PCCommitWithoutResponse() throws Exception
+    {
+        TxSupport txn = new TxSupport();
+        String pUrl = PURL;
+        String[] pid = new String[2];
+        String[] pVal = new String[2];
+
+        for (int i = 0; i < pid.length; i++) {
+            pid[i] = modifyResource(txn, pUrl, null, "p1", "v1");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+            Assert.assertEquals(pVal[i], "v1");
+        }
+
+        txn.startTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            enlistResource(txn, PURL_NO_RESPONSE + "?pId=" + pid[i]);
+
+            modifyResource(txn, pUrl, pid[i], "p1", "v2");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+
+            Assert.assertEquals(pVal[i], "v2");
+        }
+
+        String status = txn.commitTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+            Assert.assertEquals(pVal[i], "v2");
+        }
+    }
+
+    @Test
+    public void test2PCRollbackWithoutResponse() throws Exception
+    {
+        TxSupport txn = new TxSupport();
+        String pUrl = PURL;
+        String[] pid = new String[2];
+        String[] pVal = new String[2];
+
+        for (int i = 0; i < pid.length; i++) {
+            pid[i] = modifyResource(txn, pUrl, null, "p1", "v1");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+            Assert.assertEquals(pVal[i], "v1");
+        }
+
+        txn.startTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            enlistResource(txn, PURL_NO_RESPONSE + "?pId=" + pid[i]);
+
+            modifyResource(txn, pUrl, pid[i], "p1", "v2");
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+
+            Assert.assertEquals(pVal[i], "v2");
+        }
+
+        String status = txn.rollbackTx();
+
+        for (int i = 0; i < pid.length; i++) {
+            pVal[i] = getResourceProperty(txn, pUrl, pid[i], "p1");
+            Assert.assertEquals(pVal[i], "v1");
+        }
+    }
+
+    // commit an invalid transaction
+    @Test
+    public void testCommitInvalidTx() throws IOException
+    {
+        // start a transaction
+        TxSupport txn = new TxSupport().startTx();
+
+        String terminator = txn.getTerminatorURI();
+        // mangle the terminator URI
+        //terminator = terminator.replace("/terminate", "_dead/terminate");
+        terminator += "/_dead";
+        // an attempt to commit on this URI should fail:
+        txn.httpRequest(new int[] {HttpURLConnection.HTTP_NOT_FOUND}, terminator, "PUT", TxMediaType.TX_STATUS_MEDIA_TYPE, TxStatusMediaType.TX_COMMITTED);
+        // commit it properly
+        txn.commitTx();
+    }
+    
+    @Test
+    public void testTimeoutCleanup() throws InterruptedException {
+        TxSupport txn = new TxSupport();
+        int txnCount = txn.txCount();
+        txn.startTx(1000);
+        txn.enlistTestResource(PURL, false);
+
+        // Let the txn timeout
+        Thread.sleep(2000);
+        
+        Assert.assertEquals(txnCount, txn.txCount());
+    }
+
+    @Test
+    public void testClientAPI() throws Exception {
+        Client client = ClientBuilder.newClient();
+
+        WebTarget resource = client.target(TXN_MGR_URL);
+        Response response = resource.request(MediaType.APPLICATION_FORM_URLENCODED).post(Entity.entity(new Form(), MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+
+        int status = response.getStatus();
+
+        Assert.assertEquals(HttpURLConnection.HTTP_CREATED, status);
+
+        response.close();
+
+        Invocation inv = resource.request(MediaType.APPLICATION_FORM_URLENCODED).buildPost(Entity.entity(new Form(), MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        response = inv.invoke();
+
+        response.close();
+
+        String r1 = resource.request("application/txlist").get(String.class);
+        Assert.assertTrue("response should have contained 2 transaction urls",r1.length() != 0);
+
+        String r2 = resource.request().get(String.class);
+        Assert.assertTrue("xml response should have contained 2 transaction urls",r2.length() != 0);
+    }
+
+    @Test
+    public void testFailureInSecondParticipantDuringCommit() {
+        final TxSupport txn = new TxSupport();
+        final int originalTxCount = txn.txCount();
+        final String pUrl = PURL;
+        final String[] pid = new String[2];
+        pid[0] = modifyResource(txn, pUrl, null, "p1", "v1");
+        pid[1] = modifyResource(txn, pUrl, null, "p1", "v1");
+
+        txn.startTx();
+
+        enlistResource(txn, pUrl + "?pId=" + pid[0]);
+        enlistResource(txn, pUrl + "?pId=" + pid[1] + "&fault=CRUNTIME");
+
+        modifyResource(txn, pUrl, pid[0], "p1", "v2");
+        modifyResource(txn, pUrl, pid[1], "p1", "v2");
+
+        Assert.assertEquals("v2", getResourceProperty(txn, pUrl, pid[0], "p1"));
+        Assert.assertEquals("v2", getResourceProperty(txn, pUrl, pid[1], "p1"));
+
+        txn.commitTx();
+
+        Assert.assertEquals("v2", getResourceProperty(txn, pUrl, pid[0], "p1"));
+        Assert.assertEquals("v1", getResourceProperty(txn, pUrl, pid[1], "p1"));
+
+        // Transaction should not be removed because of the failure
+        Assert.assertEquals(originalTxCount + 1, txn.txCount());
+    }
+
+}
