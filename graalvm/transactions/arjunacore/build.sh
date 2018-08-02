f96241733c42fc028e2d2acb09736ee13ca88922
Mark Little <mlittle@redhat.com>
2018-08-02 14:52:34 -0400

    removed for refactoring

--- graalvm/transactions/arjunacore/build.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/sh
-
-javac BasicExample.java
-cp lib/*.jar .
-jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
-jar -xvf common-5.5.1.Final-SNAPSHOT.jar
-jar -xvf jboss-logging.jar
-rm -rf *.jar META-INF *mappings
-jar -cmf manifest.txt BasicExample.jar BasicExample.class com org
