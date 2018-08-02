4a4de9dd6ebaaf381416b1b3db01c8d8f755b17c
Mark Little <mlittle@redhat.com>
2018-08-02 14:52:54 -0400

    refactored

--- graalvm/transactions/arjunacore/build_basic.sh
+++ graalvm/transactions/arjunacore/build_basic.sh
@@ -6,4 +6,4 @@ jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
 jar -xvf common-5.5.1.Final-SNAPSHOT.jar
 jar -xvf jboss-logging.jar
 rm -rf *.jar META-INF *mappings
-jar -cmf manifest.txt BasicExample.jar BasicExample.class com org
+jar -cmf manifest_basic.txt BasicExample.jar BasicExample.class com org
