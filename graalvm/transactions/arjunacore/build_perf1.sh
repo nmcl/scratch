d7b76f022c722fa8a7e51abc9002a40f96b37f80
Mark Little <mlittle@redhat.com>
2018-08-03 08:53:24 -0400

    more refactoring

--- graalvm/transactions/arjunacore/build_perf1.sh
+++ graalvm/transactions/arjunacore/build_perf1.sh
@@ -6,4 +6,4 @@ jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
 jar -xvf common-5.5.1.Final-SNAPSHOT.jar
 jar -xvf jboss-logging.jar
 rm -rf *.jar META-INF *mappings
-jar -cmf manifest_perf.txt Performance1.jar Performance1.class com org
+jar -cmf manifest_perf1.txt Performance1.jar Performance1.class com org
