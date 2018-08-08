da9f7e37df1a3186abcc39af2c5f8319bbd79f56
Mark Little <mlittle@redhat.com>
2018-08-08 18:27:38 +0100

    autobuild manifest.txt

--- graalvm/transactions/arjunacore/basic/build.sh
+++ graalvm/transactions/arjunacore/basic/build.sh
@@ -1,5 +1,8 @@
  #!/bin/sh
 
+echo "Main-Class: BasicExample
+arjuna-properties-file: jbossts-properties.xml" > manifest.txt
+
 javac BasicExample.java
 cp ../lib/*.jar .
 jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
@@ -7,3 +10,5 @@ jar -xvf common-5.5.1.Final-SNAPSHOT.jar
 jar -xvf jboss-logging.jar
 rm -rf *.jar META-INF *mappings
 jar -cmf manifest.txt BasicExample.jar BasicExample.class com org
+
+rm manifest.txt
