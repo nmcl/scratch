cb40e569fca5e4666832ffcced13764a39f2abf1
Mark Little <mlittle@redhat.com>
2018-08-09 17:45:33 +0100

    Not needed due to refactoring.
    https://github.com/nmcl/scratch/issues/27

--- graalvm/transactions/arjunacore/performance/build_perf1.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/sh
-
-javac Performance1.java
-cp lib/*.jar .
-jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
-jar -xvf common-5.5.1.Final-SNAPSHOT.jar
-jar -xvf jboss-logging.jar
-rm -rf *.jar META-INF *mappings
-jar -cmf manifest_perf1.txt Performance1.jar Performance1.class com org
--- graalvm/transactions/arjunacore/performance/build_perf3.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/sh
-
-javac Performance3.java
-cp lib/*.jar .
-jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
-jar -xvf common-5.5.1.Final-SNAPSHOT.jar
-jar -xvf jboss-logging.jar
-rm -rf *.jar META-INF *mappings
-jar -cmf manifest_perf3.txt Performance3.jar Performance3.class SimpleAbstractRecord.class SyncRecord.class com org
