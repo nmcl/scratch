4ff215b42c18c49d4703d2ff1a6015b0cc1c2079
Mark Little <mlittle@redhat.com>
2018-08-21 17:05:06 +0100

    initial basic test

--- graalvm/transactions/jta/basic/build.sh
+++ graalvm/transactions/jta/basic/build.sh
@@ -2,15 +2,16 @@
 
 rm *.class
 
-echo "Main-Class: BasicExample
+echo "Main-Class: BasicJTA
 arjuna-properties-file: jbossts-properties.xml" > manifest.txt
 
-javac BasicExample.java
+javac BasicJTA.java
 cp ../lib/*.jar .
-jar -xvf arjunacore-5.5.1.Final-SNAPSHOT.jar
+cp -r ../jta-1_1-classes/javax .
+jar -xvf narayana-jta-5.5.1.Final-SNAPSHOT.jar
 jar -xvf common-5.5.1.Final-SNAPSHOT.jar
 jar -xvf jboss-logging.jar
 rm -rf *.jar META-INF *mappings
-jar -cmf manifest.txt BasicExample.jar BasicExample.class com org
+jar -cmf manifest.txt BasicJTA.jar BasicJTA.class DummyXA.class com org javax
 
 rm manifest.txt
