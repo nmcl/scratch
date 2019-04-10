fc831506cf8fe1a44d96f3bd801ef7d839805cf7
Mark Little <m.c.little@ncl.ac.uk>
2019-04-10 12:34:57 -0400

    More playing

--- quarkus/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ quarkus/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -18,4 +18,15 @@ public class GreetingResourceTest {
              .body(is("hello"));
     }
 
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(is("hello " + uuid));
+    }
+
 }
