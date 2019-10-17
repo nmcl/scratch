9dedfa7c70f77a3f8fa4b75cad90252797329019
Mark Little <m.c.little@ncl.ac.uk>
2019-10-17 08:06:30 -0400

    Update DNA.java

--- world/DNA.java
+++ world/DNA.java
@@ -15,6 +15,7 @@ public class DNA
 	NUMBER_OF_ARMS,  // can be useful in combat or as predator/prey
 	NUMBER_OF_EYES,  // can be useful in combat or as predator/prey
 	NUMBER_OF_HEADS, // should normally be 1!
+	NUMBER_OF_HEARTS, // should normally be 1!
 	OVERALL_SIZE,  // size
 	BASE_TEMPERATURE,  // base needed to live
 	TEMPERATURE_THRESHOLD,  // how much less than base it can survive
