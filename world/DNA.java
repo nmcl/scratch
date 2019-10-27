9289156d017ba5def873f5e09969cb5977a230f7
Mark Little <m.c.little@ncl.ac.uk>
2019-10-27 17:11:21 -0400

    added more description

--- world/DNA.java
+++ world/DNA.java
@@ -1,6 +1,14 @@
 /**
  * A very simplistic view of DNA. Ignore chromosomes and strands for now. After all
  * it took billions of years for Nature to figure that out!
+ *
+ * There are two ways in which DNA can change:
+ * - through a Mutator when two instances of the creature mate.
+ * - randomly through the lifetime of a creature.
+ *
+ * Each time DNA changes it may result in a creature dying, or having a more
+ * limited lifespan. Alternatively it may result in the creature having a better
+ * chance of surviving.
  */
 
 public class DNA
@@ -16,7 +24,7 @@ public class DNA
 	NUMBER_OF_EYES,  // can be useful in combat or as predator/prey
 	NUMBER_OF_HEADS, // should normally be 1!
 	NUMBER_OF_HEARTS, // should normally be 1!
-	OVERALL_SIZE,  // size
+	OVERALL_SIZE,  // size between 1 and 100
 	BASE_TEMPERATURE,  // base needed to live
 	TEMPERATURE_THRESHOLD,  // how much less than base it can survive
 	TERRESTRIAL, // does the creature live on the land?
@@ -39,6 +47,24 @@ public class DNA
 	FLYING_SPEED // if 0 then can't fly even if have wings
     }
 
+    @Override
+    public boolean equals (Object dna)
+    {
+	if (this == dna)
+	    return true;
+
+	if (dna == null || dna.getClass() != this.getClass()) 
+            return false;
+    
+	for (int i = 0; i < SIZE_OF_DNA; i++)
+	{
+	    if (_theMagic[i] != dna.get(i))
+		return false;
+	}
+
+	return true;
+    }
+    
     public int getValue (int v)
     {
 	return _theMagic[v];
@@ -49,7 +75,7 @@ public class DNA
 	_theMagic[v] = n;
     }
 
-    private static final int SIZE_OF_MAGIC = 100;
+    private static final int SIZE_OF_DNA = 100;
     
     private int[] _theMagic = new int[SIZE_OF_MAGIC];
 }
