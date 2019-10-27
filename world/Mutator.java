de08acea873fcea01e597d5e969ef707583a63b5
Mark Little <m.c.little@ncl.ac.uk>
2019-10-27 14:26:45 -0400

    Create Mutator.java

---
+++ world/Mutator.java
@@ -0,0 +1,5 @@
+/**
+ * A mutator takes two DNA instances and generates a third, which is the one
+ * used by the next generation of creatures. Given randomness and other factors
+ * the created instance may be exactly the same as the parent DNA.
+ */
