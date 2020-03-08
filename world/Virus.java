1df859275bc9110d7f467447a8e3366403f7e77d
Mark Little <m.c.little@ncl.ac.uk>
2020-03-08 17:38:16 +0000

    added Virus.java

---
+++ world/Virus.java
@@ -0,0 +1,6 @@
+/**
+ * This class represents different types of virus, bacteria etc.
+ *
+ * At least at the moment it should be fine to have a single class
+ * simulate a range of real world microbial organisms.
+ */
