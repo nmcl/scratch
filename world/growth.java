4f5fdd7a0827cce88996b61a4ffbd2aed86b458c
Mark Little <m.c.little@ncl.ac.uk>
2020-03-22 17:28:34 +0000

    added

---
+++ world/growth.java
@@ -0,0 +1,49 @@
+public class growth
+{
+    public static void main (String[] args)
+    {
+	/*
+	 * y = a(1+r)^x
+	 */
+
+	double y = 0;
+	double a = 1; // initial value
+	double r = 0.3; // infection rate
+	int x = 0;
+	int t = 100;
+	
+	for (int i = 0; i < args.length; i++)
+        {
+	    if ("-help".equals(args[i]))
+	    {
+		System.out.println("growth [-infection <value>] [-time <value>] [-help]");
+		System.exit(0);
+	    }
+
+	    if ("-infection".equals(args[i]))
+	    {
+		Double inf = new Double(args[i+1]);
+
+		r = inf.doubleValue();
+	    }
+
+	    if ("-time".equals(args[i]))
+	    {
+		Integer time = new Integer(args[i+1]);
+
+		t = time.intValue();
+	    }
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+	    System.out.println("Time: "+i);
+
+	    y = a * (1+r);
+	    
+	    System.out.println("Infected: "+y);
+
+	    a = y;
+	}
+    }
+}
