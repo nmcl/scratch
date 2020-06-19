57906eb97a5bb3702e7efb2cd4bd010760a9d871
Mark Little <mlittle@redhat.com>
2020-06-19 16:08:21 +0100

    Create Spades.java

---
+++ shuffle/Spades.java
@@ -0,0 +1,12 @@
+/**                                                                                                                                                               
+ * The Spades suite.                                                                                                                                              
+ */
+
+public class Spades implements Suite
+{
+    public Card getCard () throws NoSuchElementException
+    {
+    }
+
+    private Card[13] _theCards;
+}
