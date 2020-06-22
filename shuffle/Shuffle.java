f6e4404efaee84ccbf231deda7b8aa9dd9492b3d
Mark Little <mlittle@redhat.com>
2020-06-22 16:56:47 +0100

    Create Shuffle.java

---
+++ shuffle/Shuffle.java
@@ -0,0 +1,15 @@
+
+public class Shuffle
+{
+
+    public enum Card { ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king };
+
+    public static void main (String args[])
+    {
+    }
+
+    private Card[] _hearts = new Card[13];
+    private Card[] _spaded = new Card[13];
+    private Card[] _clubs = new Card[13];
+    private Card[] _diamonds = new Card[13];
+}
