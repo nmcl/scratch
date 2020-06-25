030e2f7092aa25a124f11761ef6d2461f36b0647
Mark Little <mlittle@redhat.com>
2020-06-25 16:10:40 +0100

    Update Shuffle.java

--- shuffle/Shuffle.java
+++ shuffle/Shuffle.java
@@ -3,21 +3,40 @@ import java.util.Random;
 public class Shuffle
 {
 
-    public enum Card { ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king };
-
     public static void main (String args[])
     {
 	Random randCard = new Random();
 	Random randSuite = new Random();
 	boolean finished = false;
 
+	initialiseSuite(_hearts);
+	initialiseSuite(_spades);
+	initialiseSuite(_clubs);
+	initialiseSuite(_diamonds);
+
 	while (!finished)
 	{
+	    int theSuite = randSuite.nextInt(3);
 	}
     }
 
-    private Card[] _hearts = new Card[13];
-    private Card[] _spaded = new Card[13];
-    private Card[] _clubs = new Card[13];
-    private Card[] _diamonds = new Card[13];
+    private void initialiseSuite (int[] suite)
+    {
+	for (int i = 0; i < suite.length(); i++)
+	{
+	    if (i == 0)
+		suite[i] = 11;
+	    else
+	    {
+		if (i >= 10)
+		    suite[i] = 10;
+		else
+		    suite[i] = i;
+	    }
+    }
+
+    private int[] _hearts = new int[13];
+    private int[] _spades = new int[13];
+    private int[] _clubs = new int[13];
+    private int[] _diamonds = new int[13];
 }
