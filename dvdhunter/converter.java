ae7a48afeaccc09e4a9eee867fcf34907d2b1b25
Mark Little <mlittle@redhat.com>
2020-08-01 18:30:22 +0100

    initial code

---
+++ dvdhunter/converter.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class converter
+{
+    public static void main (String args[]) throws IOException
+    {
+	String fileName = "FF74D200-9E9B-4A7C-8A58-70CEAA7FF488.html";
+
+	URL url = new URL("file://"+System.getProperty("user.dir")+"/"+fileName);
+	Scanner sc = new Scanner(url.openStream());
+	StringBuffer sb = new StringBuffer();
+
+	while (sc.hasNext())
+	{
+	    sb.append(sc.next());
+	}
+
+	String result = sb.toString();
+
+	// Removing the HTML tags
+
+	result = result.replaceAll("<[^>]*>", "");
+	System.out.println("Contents of the web page: "+result);
+    }
+}
