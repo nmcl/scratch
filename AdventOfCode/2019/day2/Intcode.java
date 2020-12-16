f6591564aa908fe5e811db7d71a97fb08eebc979
Mark Little <mlittle@redhat.com>
2020-12-16 11:56:00 +0000

    Update Intcode.java

--- AdventOfCode/2019/day2/Intcode.java
+++ AdventOfCode/2019/day2/Intcode.java
@@ -1,5 +1,23 @@
 public class Intcode
 {
+    public enum OPCODE {
+        ADD(1), MULTIPLY(2), HALT(99);
+
+        OPCODE (int val)
+        {
+            this._val = val;
+        }
+
+        public int getVal ()
+        {
+            return _val;
+        }
+
+        private int _val;
+    }
+
+    public static final String DELIMITER = ",";
+    
     public static void main (String[] args)
     {
         for (int i = 0; i < args.length; i++)
@@ -11,4 +29,6 @@ public class Intcode
             }
         }
     }
+
+    private static final String DATA_FILE = "instructions.txt";
 }
