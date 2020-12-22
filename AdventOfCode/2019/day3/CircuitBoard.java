21d9d67c9e817ef563116d62117ad9c44a3e7fb4
Mark Little <mlittle@redhat.com>
2020-12-22 16:20:51 +0000

    Update CircuitBoard.java

--- AdventOfCode/2019/day3/CircuitBoard.java
+++ AdventOfCode/2019/day3/CircuitBoard.java
@@ -15,19 +15,27 @@ public class CircuitBoard
     public static final String SEPARATOR = ",";
 
     // default circuit board size
-    
+
     public static final int LENGTH = 1000;
     public static final int WIDTH = 1000;
+
+    public static final char UP = 'U';
+    public static final char DOWN = 'D';
+    public static final char LEFT = 'L';
+    public static final char RIGHT = 'R';
+
     public static void main (String[] args)
     {
         boolean verify = false;
         boolean dump = false;
+        int length = LENGTH;
+        int width = WIDTH;
 
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("[-help] [-verify] [-dump]");
+                System.out.println("[-help] [-verify] [-dump] [-length <length>] [-width <width>]");
                 System.exit(0);
             }
             
@@ -36,6 +44,20 @@ public class CircuitBoard
 
             if ("-dump".equals(args[i]))
                 dump = true;
+
+            if ("-width".equals(args[i]))
+                _width = Integer.parseInt(args[i+1]);
+
+            if ("-length".equals(args[i]))
+                _length = Integer.parseInt(args[i+1]);
+        }
+
+        _theBoard = new int[_length][_width];
+
+        for (int i = 0; i < _length; i++)
+        {
+            for (int j = 0; j < _width; j++)
+                _theBoard[i][j] = 0;
         }
 
         /*
@@ -72,6 +94,12 @@ public class CircuitBoard
                 dumpData(line1, line2);
             else
             {
+                if (plotLines(line1, line2))
+                {
+
+                }
+                else
+                    System.out.println("Error in plotting lines!");
             }
         }
         catch (Throwable ex)
@@ -90,6 +118,136 @@ public class CircuitBoard
         }
     }
 
+    private static final boolean plotLines (String[] line1, String[] lime2)
+    {
+        int xPos = _length/2;  // always start in the centre of the grid
+        int yPos = _width/2;
+
+        if (debug)
+            System.out.println("Starting at <"+xPos+", "+yPos+">");
+
+        for (String str : line)
+        {
+            if (debug)
+                System.out.println("Current position <"+xPos+", "+yPos+">");
+
+            switch (str.charAt(0))
+            {
+                case TestPlotter.LEFT:
+                {
+                    int left = Integer.parseInt(str.substring(1));
+
+                    if (debug)
+                        System.out.print("LEFT "+left+" command ");
+
+                    if (xPos - left < 0)
+                    {
+                        if (debug)
+                            System.out.println("moved pointer beyond range into the negative by "+(xPos-left));
+
+                        _length += left;
+
+                        xPos = 0;
+                    }
+                    else
+                    {
+                        if (debug)
+                            System.out.println("moved pointer by "+(xPos-left));
+
+                        xPos -= left;
+                    }
+                }
+                break;
+                case TestPlotter.RIGHT:
+                {
+                    int right = Integer.parseInt(str.substring(1));
+
+                    if (debug)
+                        System.out.print("RIGHT "+right+" command ");
+
+                    if (xPos + right > _length)
+                    {
+                        if (debug)
+                            System.out.println("moved pointer beyond range into the positive by "+(xPos+right));
+
+                        _length += right;
+
+                        xPos += right;
+                    }
+                    else
+                    {
+                        if (debug)
+                            System.out.println("moved pointer by "+(xPos+right));
+
+                        xPos += right;
+                    }
+                }
+                break;
+                case TestPlotter.UP:
+                {
+                    int up = Integer.parseInt(str.substring(1));
+
+                    if (debug)
+                        System.out.print("UP "+up+" command ");
+
+                    if (yPos + up > _width)
+                    {
+                        if (debug)
+                            System.out.println("moved pointer beyond range into the positive by "+(yPos+up));
+
+                        _width += up;
+
+                        yPos += up;
+                    }
+                    else
+                    {
+                        if (debug)
+                            System.out.println("moved pointer by "+(yPos+up));
+
+                        yPos += up;
+                    }
+                }
+                break;
+                case TestPlotter.DOWN:
+                {
+                    int down = Integer.parseInt(str.substring(1));
+
+                    if (debug)
+                        System.out.print("DOWN "+down+" command ");
+
+                    if (yPos - down < 0)
+                    {
+                        if (debug)
+                            System.out.println("moved pointer beyond range into the negative by "+(yPos-down));
+
+                        _width += down;
+
+                        yPos = 0;
+                    }
+                    else
+                    {
+                        if (debug)
+                            System.out.println("moved pointer by "+(yPos-down));
+
+                        yPos -= down;
+                    }
+                }
+                break;
+                default:
+                {
+                    System.out.println("Unknown instruction: "+str.charAt(0));
+
+                    return false;
+                }
+            }
+        }
+
+        if (debug)
+            System.out.println("length: "+_length+" and width: "+_width);
+
+        return true;
+    }
+
     private static final void dumpData (String[] line1, String[] line2)
     {
         System.out.println("Wire 1:");
@@ -104,6 +262,10 @@ public class CircuitBoard
             System.out.println(str);
         }
     }
+
+    private static int[][] _theBoard = null;
+    private static int _length = 0;
+    private static int _width = 0;
 
     private static final String DATA_FILE = "data.txt";
 }
