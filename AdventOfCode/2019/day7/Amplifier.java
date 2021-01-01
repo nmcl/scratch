cb7a9214ce3df4bc1a2b9558a3523f1a83c4c448
Mark Little <mlittle@redhat.com>
2021-01-01 09:51:15 +0000

    Initial cut at an Amplifier module.

--- AdventOfCode/2019/day7/Amplifier.java
+++ AdventOfCode/2019/day7/Amplifier.java
@@ -1,11 +1,39 @@
 public class Amplifier
 {
-    public Amplifier (boolean debug)
+    public Amplifier (int ps, String[] commands, boolean debug)
     {
         _computer = new Intcode(debug);
+        _phaseSetting = ps;
+        _commands = commands;
         _debug = debug;
     }
 
+    public final void resetState ()
+    {
+        _computer = new Intcode(_debug);
+    }
+
+    public final void setPhaseSetting (int ps)
+    {
+        _phaseSetting = ps;
+    }
+
+    public final void setCommands (String[] commands)
+    {
+        _commands = commands;
+    }
+
+    public final int executeCommands ()
+    {
+        String[] currentCommands = new String[_commands.length];
+
+        System.arraycopy(_commands, _commands.length, currentCommands, 0, _commands.length);
+
+        return _computer.parseAndExecute(currentCommands, _phaseSetting);
+    }
+
     private Intcode _computer;
+    private int _phaseSetting;
+    private String[] _commands;
     private boolean _debug;
 }
