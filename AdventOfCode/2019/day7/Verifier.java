e52ec6f7f4479871825d830ef40db6d9eaba4197
Mark Little <mlittle@redhat.com>
2021-01-02 10:09:53 +0000

    Update Verifier.java

--- AdventOfCode/2019/day7/Verifier.java
+++ AdventOfCode/2019/day7/Verifier.java
@@ -6,6 +6,8 @@ public class Verifier
     public static final int MAX_THRUSTER_SIGNAL_1 = 43210;
     public static final String[] EXAMPLE_2_COMMANDS = {"3","23","3","24","1002","24","10","24","1002","23","-1","23","101","5","23","23","1","24","23","23","4","23","99","0","0"};
     public static final int MAX_THRUSTER_SIGNAL_2 = 54321;
+    public static final String[] EXAMPLE_3_COMMANDS = {"3","31","3","32","1002","32","10","32","1001","31","-2","31","1007","31","0","33","1002","33","7","33","1","33","31","31","1","32","31","31","4","31","99","0","0","0"};
+    public static final int MAX_THRUSTER_SIGNAL_3 = 65210;
 
     public Verifier (Vector<String> permutations, boolean debug)
     {
@@ -18,7 +20,15 @@ public class Verifier
         if (verifyInstance(EXAMPLE_1_COMMANDS, MAX_THRUSTER_SIGNAL_1))
         {
             if (verifyInstance(EXAMPLE_2_COMMANDS, MAX_THRUSTER_SIGNAL_2))
-                return true;
+            {
+                if (verifyInstance(EXAMPLE_3_COMMANDS, MAX_THRUSTER_SIGNAL_3))
+                    return true;
+                else
+                {
+                    if (_debug)
+                        System.out.println("Verify failed for "+EXAMPLE_3_COMMANDS);
+                }
+            }
             else
             {
                 if (_debug)
