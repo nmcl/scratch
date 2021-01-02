9be4870e39499a95992250126ec02c695b7dc7ca
Mark Little <mlittle@redhat.com>
2021-01-02 17:22:50 +0000

    Update Verifier.java

--- AdventOfCode/2019/day7/part1/Verifier.java
+++ AdventOfCode/2019/day7/part1/Verifier.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Verifier extends AmplifierSeries
 {
     public static final String[] EXAMPLE_1_COMMANDS = {"3","15","3","16","1002","16","10","16","1","16","15","15","4","15","99","0","0"};
-    public static final int MAX_THRUSTER_SIGNAL_1 = 43210;
+    public static final int MAX_THRUSTER_SIGNAL_1 = 43210;  // 4,3,2,1,0
     public static final String[] EXAMPLE_2_COMMANDS = {"3","23","3","24","1002","24","10","24","1002","23","-1","23","101","5","23","23","1","24","23","23","4","23","99","0","0"};
-    public static final int MAX_THRUSTER_SIGNAL_2 = 54321;
+    public static final int MAX_THRUSTER_SIGNAL_2 = 54321;  // 0,1,2,3,4
     public static final String[] EXAMPLE_3_COMMANDS = {"3","31","3","32","1002","32","10","32","1001","31","-2","31","1007","31","0","33","1002","33","7","33","1","33","31","31","1","32","31","31","4","31","99","0","0","0"};
-    public static final int MAX_THRUSTER_SIGNAL_3 = 65210;
+    public static final int MAX_THRUSTER_SIGNAL_3 = 65210;  // 1,0,4,3,2
 
     public Verifier (Vector<String> permutations, boolean debug)
     {
