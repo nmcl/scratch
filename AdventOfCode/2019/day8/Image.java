631306926cb074b3277200e3a08d9bfb47d24311
Mark Little <mlittle@redhat.com>
2021-01-03 17:24:28 +0000

    Update Image.java

--- AdventOfCode/2019/day8/Image.java
+++ AdventOfCode/2019/day8/Image.java
@@ -1,11 +1,13 @@
 public class Image
 {
-    public Image (int width, int height)
+    public Image (int width, int height, String data)
     {
         _width = width;
         _height = height;
+        _data = data;
     }
 
     private int _width;
     private int _height;
+    private String _data;
 }
