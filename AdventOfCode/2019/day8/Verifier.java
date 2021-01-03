0648354dff8297d44d2b97cdd56a30986a0857db
Mark Little <mlittle@redhat.com>
2021-01-03 17:19:41 +0000

    Initial verifier.

---
+++ AdventOfCode/2019/day8/Verifier.java
@@ -0,0 +1,16 @@
+public class Verifier
+{
+    public static final String EXAMPLE_DATA = "123456789012";
+    public static final int EXAMPLE_WIDTH = 3;
+    public static final int EXAMPLE_HEIGHT = 2;
+
+    public Verifier ()
+    {
+
+    }
+
+    public final boolean verify ()
+    {
+        return true;
+    }
+}
