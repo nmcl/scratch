d259e3df244027ab18ce10d17c8e6b4cc6b04610
Mark Little <mlittle@redhat.com>
2021-01-07 13:46:13 +0000

    More tests.

--- AdventOfCode/2019/day9/Verifier.java
+++ AdventOfCode/2019/day9/Verifier.java
@@ -5,9 +5,9 @@ public class Verifier
     public static final String[] EXAMPLE_1_COMMANDS = {"109","1","204","-1","1001","100","1","100","1008","100","16","101","1006","101","0","99"};
     public static final String[] EXAMPLE_1_RESULTS = {"109","1","204","-1","1001","100","1","100","1008","100","16","101","1006","101","0","99"};
     public static final String[] EXAMPLE_2_COMMANDS = {"1102","34915192","34915192","7","4","7","99","0"};
-    public static final String[] EXAMPLE_2_RESULTS = {""};
+    public static final String EXAMPLE_2_RESULTS = "1219070632396864";
     public static final String[] EXAMPLE_3_COMMANDS = {"104","1125899906842624","99"};
-    public static final String[] EXAMPLE_3_RESULTS = {"1125899906842624"};
+    public static final String EXAMPLE_3_RESULTS = "1125899906842624";
 
     public Verifier (boolean debug)
     {
@@ -39,6 +39,39 @@ public class Verifier
             else
                 index++;
         }
+
+        if (verified)
+        {
+            instructions = new Vector<String>();
+            results = new Vector<String>();
+
+            instructions.addAll(Arrays.asList(EXAMPLE_2_COMMANDS));
+
+            theComputer = new Intcode(instructions, 0, _debug);
+            results = theComputer.executeProgram();
+
+            verified = false;
+
+            if (EXAMPLE_2_RESULTS.equals(results.elementAt(0)))
+            {
+                instructions = new Vector<String>();
+                results = new Vector<String>();
+
+                instructions.addAll(Arrays.asList(EXAMPLE_3_COMMANDS));
+
+                theComputer = new Intcode(instructions, 0, _debug);
+                results = theComputer.executeProgram();
+
+                if (EXAMPLE_3_RESULTS.equals(results.elementAt(0)))
+                    verified = true;
+                else
+                    System.out.println("Verify failed for EXAMPLE_3_COMMANDS");
+            }
+            else
+                System.out.println("Verify failed for EXAMPLE_2_COMMANDS");
+        }
+        else
+            System.out.println("Verify failed for EXAMPLE_1_COMMANDS");
 
         return verified;
     }
