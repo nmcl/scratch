d6f927b92286edf99f96217412ddc704680354d7
Mark Little <mlittle@redhat.com>
2021-01-19 16:13:47 +0000

    Update EHPR.java

--- AdventOfCode/2019/day11/EHPR.java
+++ AdventOfCode/2019/day11/EHPR.java
@@ -10,34 +10,17 @@ public class EHPR
     public static void main (String[] args)
     {
         boolean debug = false;
-        boolean runVerifier = false;
 
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-help] [-verify] [-debug]");
+                System.out.println("Usage: [-help] [-debug]");
                 System.exit(0);
             }
 
             if ("-debug".equals(args[i]))
                 debug = true;
-
-            if ("-verify".equals(args[i]))
-                runVerifier = true;
-        }
-
-
-        if (runVerifier)
-        {
-            Verifier theVerifier = new Verifier(debug);
-
-            if (theVerifier.verify())
-                System.out.println("Verified ok!");
-            else
-                System.out.println("Verify failed!");
-                
-            System.exit(0);
         }
 
         /*
