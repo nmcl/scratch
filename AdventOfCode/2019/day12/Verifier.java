ea24ca872ad2680467491e1bd85ac38ecbc290f1
Mark Little <mlittle@redhat.com>
2021-01-24 16:29:56 +0000

    Create Verifier.java

---
+++ AdventOfCode/2019/day12/Verifier.java
@@ -0,0 +1,10 @@
+public class Verifier
+{
+    public static final String EXAMPLE1_DATA = "example1.txt";
+    public static final String EXAMPLE2_DATA = "example2.txt";
+
+    public Verifier ()
+    {
+        
+    }
+}
