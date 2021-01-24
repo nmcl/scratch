b69c109e0c284f77a2020566781a17702bded5e2
Mark Little <mlittle@redhat.com>
2021-01-24 11:51:08 +0000

    Create Moon.java

---
+++ AdventOfCode/2019/day12/Moon.java
@@ -0,0 +1,11 @@
+public class Moon
+{
+    public Moon (int x, int y, int z)
+    {
+        _position = new Coordinate(x, y, z);
+        _velocity = new Velocity();  // the x, y, and z velocity of each moon starts at 0.
+    }
+
+    private Coordinate _position;
+    private Velocity _velocity;
+}
