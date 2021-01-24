2ee5316f0088b8b10ad14404dbcc3dd779b5eb5e
Mark Little <mlittle@redhat.com>
2021-01-24 17:16:26 +0000

    Update Verifier.java

--- AdventOfCode/2019/day12/Verifier.java
+++ AdventOfCode/2019/day12/Verifier.java
@@ -3,8 +3,21 @@ public class Verifier
     public static final String EXAMPLE1_DATA = "example1.txt";
     public static final String EXAMPLE2_DATA = "example2.txt";
 
-    public Verifier ()
+    public Verifier (boolean debug)
     {
-        
+        _activator = null;
+        _debug = debug;
     }
+
+    public final boolean verify ()
+    {
+        boolean result = false;
+
+        _activator = new Activate(EXAMPLE1_DATA, _debug);
+
+        return result;
+    }
+
+    private Activate _activator;
+    private boolean _debug;
 }
