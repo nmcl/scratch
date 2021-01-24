46b17731e9ff17e27f17be1595bec3db6985fc3a
Mark Little <mlittle@redhat.com>
2021-01-24 17:16:28 +0000

    Create Activate.java

---
+++ AdventOfCode/2019/day12/Activate.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+public class Activate
+{
+    public static final String X_VALUE = "x=";
+    public static final String Y_VALUE = "y=";
+    public static final String Z_VALUE = "z=";
+
+    public static final String DEMARCATION = ",";
+
+    public Activate (String dataFile, boolean debug)
+    {
+        _dataFile = dataFile;
+        _debug = debug;
+
+        createSystem();
+    }
+
+    private void createSystem ()
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(_dataFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                parseLine(line);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+    }
+
+    private void parseLine (String line)
+    {
+        if (_debug)
+            System.out.println("Parsing data: "+line);
+
+        int endX = line.indexOf(DEMARCATION);
+        String xData = line.substring(X_VALUE.length()+1, endX);
+
+        if (_debug)
+            System.out.println("xData "+xData);
+
+        int startY = endX + xData.length();
+        String yData = line.substring(startY+Y_VALUE.length(), line.indexOf(DEMARCATION, startY));
+
+        if (_debug)
+            System.out.println("yData "+yData);
+
+        int endZ = line.length()-1;
+        String zData = line.substring(line.indexOf(Z_VALUE)+Z_VALUE.length(), endZ);
+
+        if (_debug)
+            System.out.println("zData "+zData);
+    }
+    
+    private Vector<Moon> _system;
+    private String _dataFile;
+    private boolean _debug;
+}
