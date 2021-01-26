282c9ad7bef7300439b05fe843195e6c6c2d9d11
Mark Little <mlittle@redhat.com>
2021-01-26 11:35:38 +0000

    Update Moon.java

--- AdventOfCode/2019/day12/Moon.java
+++ AdventOfCode/2019/day12/Moon.java
@@ -28,51 +28,57 @@ public class Moon
             int x1 = _position.getX();
             int x2 = otherMoon._position.getX();
 
+            System.out.println("**comparing "+x1+" and "+x2);
+
             if (x1 > x2)
             {
-                _velocity.setX(x1 -1);
-                otherMoon._velocity.setX(x2 +1);
+                _velocity.decrementX();
+                otherMoon._velocity.incrementX();
             }
             else
             {
                 if (x1 < x2)
                 {
-                    _velocity.setX(x1 +1);
-                    otherMoon._velocity.setX(x2 -1);
+                    _velocity.incrementX();
+                    otherMoon._velocity.decrementX();
                 }
             }
 
             int y1 = _position.getY();
             int y2 = otherMoon._position.getY();
 
+            System.out.println("**comparing "+y1+" and "+y2);
+
             if (y1 > y2)
             {
-                _velocity.setY(y1 -1);
-                otherMoon._velocity.setY(y2 +1);
+                _velocity.decrementY();
+                otherMoon._velocity.incrementY();
             }
             else
             {
                 if (y1 < y2)
                 {
-                    _velocity.setY(y1 +1);
-                    otherMoon._velocity.setY(y2 -1);
+                    _velocity.incrementY();
+                    otherMoon._velocity.decrementY();
                 }
             }
 
             int z1 = _position.getZ();
             int z2 = otherMoon._position.getZ();
 
+            System.out.println("**comparing "+z1+" and "+z2);
+
             if (z1 > z2)
             {
-                _velocity.setZ(z1 -1);
-                otherMoon._velocity.setZ(z2 +1);
+                _velocity.decrementZ();
+                otherMoon._velocity.incrementZ();
             }
             else
             {
                 if (z1 < z2)
                 {
-                    _velocity.setZ(z1 +1);
-                    otherMoon._velocity.setZ(z2 -1);
+                    _velocity.incrementZ();
+                    otherMoon._velocity.decrementZ();
                 }
             }
         }
