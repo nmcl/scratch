9af92020fc0a1bdafab55640536606ab37b4a680
Mark Little <mlittle@redhat.com>
2021-01-28 10:17:16 +0000

    Brute force! Yes, not the long term solution!

--- AdventOfCode/2019/day12/part2/Verifier.java
+++ AdventOfCode/2019/day12/part2/Verifier.java
@@ -1,11 +1,17 @@
+import java.util.*;
+
 public class Verifier
 {
     public static final String EXAMPLE1_DATA = "example1.txt";
     public static final String EXAMPLE2_DATA = "example2.txt";
 
+    public static final int EXAMPLE1_REPEAT_TIME = 2772;
+    public static final long EXAMPLE2_REPEAT_TIME = 4686774924L;
+
     public Verifier (boolean debug)
     {
         _activator = null;
+        _histories = new Vector<Moon[]>();
         _debug = debug;
     }
 
@@ -19,75 +25,31 @@ public class Verifier
     private final boolean verifyExample1 ()
     {
         boolean result = false;
+        Moon[] moonsAsArray = null;
 
         _activator = new MoonSystem(EXAMPLE1_DATA, _debug);
+        moonsAsArray = new Moon[_activator.getMoons().size()];
+
+        _histories.add(_activator.duplicateTheSystem().toArray(moonsAsArray));
 
         if (_debug)
             System.out.println("Activated: "+_activator);
         
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < EXAMPLE1_REPEAT_TIME; i++)
         {
             _activator.applyGravity();
-
-            if (_debug)
-            {
-                System.out.println("Moon positions:\n"+_activator.moonCoordinates());
-                System.out.println("Moon velocities:\n"+_activator.moonVelocities());
-            }
+            _histories.add(_activator.duplicateTheSystem().toArray(moonsAsArray));
         }
 
-        System.out.println("Moon positions after 2 steps:\n"+_activator.moonCoordinates());
-        System.out.println("Moon velocitie after 2 stepss:\n"+_activator.moonVelocities());
+        System.out.println("Moon positions:\n"+_activator.moonCoordinates());
+        System.out.println("Moon velocities:\n"+_activator.moonVelocities());
+        
+        result = true;
 
-        Moon theMoon = _activator.getMoons().elementAt(1);
-        Velocity theVelocity = theMoon.getVelocity();
-
-        if (theVelocity.equals(EXAMPLE1_STEP2_MOON2))
+        for (int i = 0; (i < moonsAsArray.length) && result; i++)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                _activator.applyGravity();
-            }
-
-            System.out.println("Moon positions after 4 steps:\n"+_activator.moonCoordinates());
-            System.out.println("Moon velocities after 4 steps:\n"+_activator.moonVelocities());
-
-            theMoon = _activator.getMoons().elementAt(0);
-            theVelocity = theMoon.getVelocity();
-
-            if (theVelocity.equals(EXAMPLE1_STEP4_MOON1))
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    _activator.applyGravity();
-                }
-
-                System.out.println("Moon positions after 7 steps:\n"+_activator.moonCoordinates());
-                System.out.println("Moon velocities after 7 steps:\n"+_activator.moonVelocities());
-
-                theMoon = _activator.getMoons().elementAt(3);
-                Coordinate theCoordinate = theMoon.getPosition();
-
-                if (theCoordinate.equals(EXAMPLE1_STEP7_MOON4))
-                {
-                    for (int i = 0; i < 3; i++)
-                    {
-                        _activator.applyGravity();
-                    }
-
-                    System.out.println("Moon positions after 10 steps:\n"+_activator.moonCoordinates());
-                    System.out.println("Moon velocities after 10 steps:\n"+_activator.moonVelocities());
-
-                    theMoon = _activator.getMoons().elementAt(2);
-                    theCoordinate = theMoon.getPosition();
-
-                    if (theCoordinate.equals(EXAMPLE1_STEP10_MOON3))
-                    {
-                        if (_activator.totalEnergy() == EXAMPLE1_TOTAL_ENERGY)
-                            result = true;
-                    }
-                }
-            }
+            if (!_histories.elementAt(0)[i].equals(_activator.getMoons().elementAt(i)))
+                result = false;
         }
 
         return result;
@@ -96,80 +58,40 @@ public class Verifier
     private final boolean verifyExample2 ()
     {
         boolean result = false;
+        Moon[] moonsAsArray = null;
 
         _activator = new MoonSystem(EXAMPLE2_DATA, _debug);
+        moonsAsArray = new Moon[_activator.getMoons().size()];
+
+        _histories.add(_activator.duplicateTheSystem().toArray(moonsAsArray));
 
         if (_debug)
             System.out.println("Activated: "+_activator);
         
-        for (int i = 0; i < 20; i++)
+        long index = 0;
+
+        while (index < EXAMPLE2_REPEAT_TIME)
         {
             _activator.applyGravity();
-
-            if (_debug)
-            {
-                System.out.println("Moon positions:\n"+_activator.moonCoordinates());
-                System.out.println("Moon velocities:\n"+_activator.moonVelocities());
-            }
+            _histories.add(_activator.duplicateTheSystem().toArray(moonsAsArray));
+            index++;
         }
 
-        System.out.println("Moon positions after 20 steps:\n"+_activator.moonCoordinates());
-        System.out.println("Moon velocities after 20 steps:\n"+_activator.moonVelocities());
+        System.out.println("Moon positions:\n"+_activator.moonCoordinates());
+        System.out.println("Moon velocities:\n"+_activator.moonVelocities());
+        
+        result = true;
 
-        Moon theMoon = _activator.getMoons().elementAt(1);
-        Velocity theVelocity = theMoon.getVelocity();
-
-        if (theVelocity.equals(EXAMPLE2_STEP20_MOON2))
+        for (int i = 0; (i < moonsAsArray.length) && result; i++)
         {
-            for (int i = 0; i < 20; i++)
-            {
-                _activator.applyGravity();
-            }
-
-            System.out.println("Moon positions after 40 steps:\n"+_activator.moonCoordinates());
-            System.out.println("Moon velocities after 40 steps:\n"+_activator.moonVelocities());
-
-            theMoon = _activator.getMoons().elementAt(0);
-            theVelocity = theMoon.getVelocity();
-
-            if (theVelocity.equals(EXAMPLE2_STEP40_MOON1))
-            {
-                for (int i = 0; i < 30; i++)
-                {
-                    _activator.applyGravity();
-                }
-
-                System.out.println("Moon positions after 70 steps:\n"+_activator.moonCoordinates());
-                System.out.println("Moon velocities after 70 steps:\n"+_activator.moonVelocities());
-
-                theMoon = _activator.getMoons().elementAt(3);
-                Coordinate theCoordinate = theMoon.getPosition();
-
-                if (theCoordinate.equals(EXAMPLE2_STEP70_MOON4))
-                {
-                    for (int i = 0; i < 30; i++)
-                    {
-                        _activator.applyGravity();
-                    }
-
-                    System.out.println("Moon positions after 100 steps:\n"+_activator.moonCoordinates());
-                    System.out.println("Moon velocities after 100 steps:\n"+_activator.moonVelocities());
-
-                    theMoon = _activator.getMoons().elementAt(2);
-                    theCoordinate = theMoon.getPosition();
-
-                    if (theCoordinate.equals(EXAMPLE2_STEP100_MOON3))
-                    {
-                        if (_activator.totalEnergy() == EXAMPLE2_TOTAL_ENERGY)
-                            result = true;
-                    }
-                }
-            }
+            if (!_histories.elementAt(0)[i].equals(_activator.getMoons().elementAt(i)))
+                result = false;
         }
 
         return result;
     }
 
     private MoonSystem _activator;
+    private Vector<Moon[]> _histories;
     private boolean _debug;
 }
