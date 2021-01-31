1047816f5802d4ed38bcade8649e582ff3b2bd2a
Mark Little <mlittle@redhat.com>
2021-01-31 14:11:29 +0000

    Update ArcadeCabinet.java

---
+++ AdventOfCode/2019/day13/ArcadeCabinet.java
@@ -0,0 +1,23 @@
+public class ArcadeCabinet
+{
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-help] [-verify] [-debug]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
