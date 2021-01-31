ec484afcd74c2ba1f548afb73c30a1723dd97e27
Mark Little <mlittle@redhat.com>
2021-01-31 14:12:00 +0000

    no verify option

--- AdventOfCode/2019/day13/ArcadeCabinet.java
+++ AdventOfCode/2019/day13/ArcadeCabinet.java
@@ -3,21 +3,17 @@ public class ArcadeCabinet
     public static void main (String[] args)
     {
         boolean debug = false;
-        boolean verify = false;
 
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-help] [-verify] [-debug]");
+                System.out.println("Usage: [-help] [-debug]");
                 System.exit(0);
             }
 
             if ("-debug".equals(args[i]))
                 debug = true;
-
-            if ("-verify".equals(args[i]))
-                verify = true;
         }
     }
 }
