89f57fdd822ee3887ff9c4e2eddd64e388e04202
Mark Little <mlittle@redhat.com>
2021-01-31 14:08:55 +0000

    Update Screen.java

---
+++ AdventOfCode/2019/day13/Screen.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Screen
+{
+    public Screen ()
+    {
+        _tiles = new Vector<Tile>();
+    }
+
+    public final void updateTile (Coordinate position, int id)
+    {
+
+    }
+
+    private Vector<Tile> _tiles;
+}
