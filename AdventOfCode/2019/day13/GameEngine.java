f2eca29f1c0e88047db75356648a7dcb2bf7ba48
Mark Little <mlittle@redhat.com>
2021-01-31 17:56:28 +0000

    Update GameEngine.java

--- AdventOfCode/2019/day13/GameEngine.java
+++ AdventOfCode/2019/day13/GameEngine.java
@@ -4,11 +4,79 @@ import java.util.*;
 
 public class GameEngine
 {
-    public GameEngine (boolean debug)
+    public GameEngine (Vector<String> instructions, boolean debug)
     {
+        _computer = new Intcode(instructions, INITIAL_INPUT, debug);
         _debug = debug;
+    }
+
+    public final boolean playGame ()
+    {
+        int x = 0;
+        int y = 0;
+        int id = TileId.EMPTY;
+
+        while (!_computer.hasHalted())
+        {
+            _computer.singleStepExecution(INITIAL_INPUT);
+
+            if (_computer.hasOutput())
+            {
+                x = Integer.parseInt(_computer.getOutput());
+            }
+            else
+            {
+                System.out.println("Error - no x value given!");
+
+                return false;
+            }
+
+            if (!_computer.hasHalted())
+            {
+                _computer.singleStepExecution(INITIAL_INPUT);
+
+                if (_computer.hasOutput())
+                {
+                    y = Integer.parseInt(_computer.getOutput());
+                }
+                else
+                {
+                    System.out.println("Error - no y value given!");
+
+                    return false;
+                }
+
+                if (!_computer.hasHalted())
+                {
+                    _computer.singleStepExecution(INITIAL_INPUT);
+
+                    if (_computer.hasOutput())
+                    {
+                        id = Integer.parseInt(_computer.getOutput());
+                    }
+                    else
+                    {
+                        System.out.println("Error - no id value given!");
+
+                        return false;
+                    }
+                }
+
+                System.out.println("**got "+x+" "+y+" "+TileId.idToString(id));
+            }
+            else
+            {
+                System.out.println("Error - computer halted after x!");
+
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private Intcode _computer;
     private boolean _debug;
+
+    private static final int INITIAL_INPUT = 0; // nothing specified in the overview
 }
