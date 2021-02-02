c3be1b30b81655da4849c5390d327d75f5017e4a
Mark Little <mlittle@redhat.com>
2021-02-02 13:41:51 +0000

    Update Joystick.java

---
+++ AdventOfCode/2019/day13/part2/Joystick.java
@@ -0,0 +1,11 @@
+public class Joystick
+{
+    public static final int NEUTRAL_POSITION = 0;
+    public static final int TILTED_LEFT = -1;
+    public static final int TILTED_RIGHT = 1;
+
+    private Joystick ()
+    {
+        // stop instantiation.
+    }
+}
