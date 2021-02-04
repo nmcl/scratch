8ffef4968b43e82586eb382b8d3c9959f2949d76
Mark Little <mlittle@redhat.com>
2021-02-04 11:54:01 +0000

    Update SegmentDisplay.java

--- AdventOfCode/2019/day13/part2/SegmentDisplay.java
+++ AdventOfCode/2019/day13/part2/SegmentDisplay.java
@@ -2,6 +2,18 @@ public class SegmentDisplay
 {
     public SegmentDisplay ()
     {
-        
+        _score = 0;
     }
+
+    public final int getScore ()
+    {
+        return _score;
+    }
+
+    public final void setScore (int s)
+    {
+        _score = s;
+    }
+    
+    private int _score;
 }
