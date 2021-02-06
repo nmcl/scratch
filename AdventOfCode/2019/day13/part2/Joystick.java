9d5e57370d8a6c5f13d2216c3fb1f32a89ea4d98
Mark Little <mlittle@redhat.com>
2021-02-06 18:41:47 +0000

    Update Joystick.java

--- AdventOfCode/2019/day13/part2/Joystick.java
+++ AdventOfCode/2019/day13/part2/Joystick.java
@@ -4,8 +4,36 @@ public class Joystick
     public static final int TILTED_LEFT = -1;
     public static final int TILTED_RIGHT = 1;
 
-    private Joystick ()
+    public Joystick ()
     {
-        // stop instantiation.
+        _position = NEUTRAL_POSITION;
     }
+
+    public final int getPosition ()
+    {
+        return _position;
+    }
+
+    public final void setPosition (int p)
+    {
+        _position = p;
+    }
+
+    @Override
+    public String toString ()
+    {
+        switch (_position)
+        {
+            case NEUTRAL_POSITION:
+                return "neutral position";
+            case TILTED_LEFT:
+                return "tilted left";
+            case TILTED_RIGHT:
+                RETURN "tilted right";
+            default:
+                return "unknown";
+        }
+    }
+    
+    private int _position;
 }
