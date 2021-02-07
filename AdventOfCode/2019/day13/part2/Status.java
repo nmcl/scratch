c5b509204ebd6d1dd52f80b19c0c632df464564c
Mark Little <mlittle@redhat.com>
2021-02-07 13:16:16 +0000

    added pring and waiting on input status.

--- AdventOfCode/2019/day13/part2/Status.java
+++ AdventOfCode/2019/day13/part2/Status.java
@@ -8,7 +8,27 @@ public class Status
     public static final int PAUSED = 1;
     public static final int RUNNING = 2;
     public static final int HALTED = 3;
+    public static final int WAITING_FOR_INPUT = 4;
 
+    public static final String statusToString (int status)
+    {
+        switch (status)
+        {
+            case CREATED:
+                return "CREATED";
+            case PAUSED:
+                return "PAUSED";
+            case RUNNING:
+                return "RUNNING";
+            case HALTED:
+                return "HALTED";
+            case WAITING_FOR_INPUT:
+                return "WAITING_FOR_INPUT";
+            default:
+                return "UNKNOWN";
+        }
+    }
+    
     private Status ()
     {
         // stop instantiation.
