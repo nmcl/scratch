9647bd87cca4b8349e1bbd3cd5a0240a3d8ecc08
Mark Little <mlittle@redhat.com>
2021-02-09 14:01:17 +0000

    Update Reaction.java

--- AdventOfCode/2019/day14/Reaction.java
+++ AdventOfCode/2019/day14/Reaction.java
@@ -1,9 +1,13 @@
 public class Reaction
 {
-    public Reaction (Chemical name, Chemical[] reactants)
+    public Reaction (Chemical name, Chemical[] reactants, int[] quantities)
     {
         _name = name;
+        _reactants = reactants;
+        _quantities = quantities;
     }
 
     private Chemical _name;
+    private Chemical[] _reactants;
+    private int[] _quantities;
 }
