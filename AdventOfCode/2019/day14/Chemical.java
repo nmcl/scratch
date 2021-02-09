9d8491207daf15ac629a001877ed46dc78dc616c
Mark Little <mlittle@redhat.com>
2021-02-09 13:30:57 +0000

    Update Chemical.java

--- AdventOfCode/2019/day14/Chemical.java
+++ AdventOfCode/2019/day14/Chemical.java
@@ -1,4 +1,36 @@
-public interface Chemical
+public class Chemical
 {
-    
+    public Chemical (String name)
+    {
+        _name = name;
+    }
+
+    public final String getName ()
+    {
+        return _name;
+    }
+
+    @Override
+    public String toString ()
+    {
+        return _name;
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (obj instanceof Chemical)
+        {
+            Chemical temp = (Chemical) obj;
+
+            return (temp._name.equals(_name));
+        }
+
+        return false;
+    }
+
+    private String _name;
 }
