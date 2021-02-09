17e2eb6e8c6ac0abddcbe08272a92604aec9a0f7
Mark Little <mlittle@redhat.com>
2021-02-09 14:08:58 +0000

    Create Nanofactory.java

---
+++ AdventOfCode/2019/day14/Nanofactory.java
@@ -0,0 +1,23 @@
+public class Nanofactory
+{
+    public static final void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-help] [-debug] [-verify]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
