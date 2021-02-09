82bbf738382d70a93c5d89790b37ffb98686b98b
Mark Little <mlittle@redhat.com>
2021-02-09 16:28:01 +0000

    Update Parser.java

--- AdventOfCode/2019/day14/Parser.java
+++ AdventOfCode/2019/day14/Parser.java
@@ -1,10 +1,51 @@
+import java.util.*;
+import java.io.*;
+
 public class Parser
 {
     public static final String DELIMITER = ",";
     public static final String PRODUCES = "=>";
-    
-    public Parser ()
-    {
+    public static final String SPACE = " ";
 
+    public Parser (boolean debug)
+    {
+        _debug = debug;
     }
+
+    public void loadData (String fileName)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        String[] values = null;
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                //values = line.split(Intcode.DELIMITER);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+    }
+
+    private boolean _debug;
 }
