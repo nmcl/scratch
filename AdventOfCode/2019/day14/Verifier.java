544cd0555aa6c73ccf2ffe36c1c02bc239b8fa39
Mark Little <mlittle@redhat.com>
2021-02-09 15:53:46 +0000

    Update Verifier.java

--- AdventOfCode/2019/day14/Verifier.java
+++ AdventOfCode/2019/day14/Verifier.java
@@ -1,3 +1,13 @@
 public class Verifier
 {
+    public static final String EXAMPLE1_FILE = "example1.txt";
+    public static final int TOTAL_ORE_1 = 31;
+    public static final String EXAMPLE2_FILE = "example2.txt";
+    public static final int TOTAL_ORE_2 = 165;
+    public static final String EXAMPLE3_FILE = "example3.txt";
+    public static final int TOTAL_ORE_3 = 13312;
+    public static final String EXAMPLE4_FILE = "example4.txt";
+    public static final int TOTAL_ORE_4 = 180697;
+    public static final String EXAMPLE5_FILE = "example5.txt";
+    public static final int TOTAL_ORE_5 = 2210736;
 }
