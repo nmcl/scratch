50576345623b4126b3906adeeea6771c6708a7f5
Mark Little <mlittle@redhat.com>
2021-02-11 15:20:13 +0000

    Update Reaction.java

--- AdventOfCode/2019/day14/Reaction.java
+++ AdventOfCode/2019/day14/Reaction.java
@@ -1,6 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 public class Reaction
 {
-    public Reaction (Chemical name, int quantity, Chemical[] reactants, int[] quantities)
+    public Reaction ()
+    {
+        _chemicalCreated = null;
+        _quantityCreated = -1;
+        _reactants = null;
+        _quantities = null;
+    }
+
+    public Reaction (Chemical name, int quantity, Vector<Chemical> reactants, Vector<Integer> quantities)
     {
         _chemicalCreated = name;
         _quantityCreated = quantity;
@@ -8,8 +19,43 @@ public class Reaction
         _quantities = quantities;
     }
 
+    public final void setCreated (Chemical name, int quantity)
+    {
+        _chemicalCreated = name;
+        _quantityCreated = quantity;
+    }
+
+    public final void setReactants (Vector<Chemical> reactants, Vector<Integer> quantities)
+    {
+        _reactants = reactants;
+        _quantities = quantities;
+    }
+    
+    public final String chemicalCreated ()
+    {
+        return _chemicalCreated.getName();
+    }
+
+    @Override
+    public String toString ()
+    {
+        String str = "";
+
+        for (int i = 0; i < _quantities.size(); i++)
+        {
+            if (str != "")
+                str += " and ";
+
+            str += _reactants.elementAt(i)+"*"+_quantities.elementAt(i);
+        }
+
+        str += " create "+_quantityCreated+" of "+_chemicalCreated;
+
+        return str;
+    }
+
     private Chemical _chemicalCreated;
     private int _quantityCreated;
-    private Chemical[] _reactants;
-    private int[] _quantities;
+    private Vector<Chemical> _reactants;
+    private Vector<Integer> _quantities;
 }
