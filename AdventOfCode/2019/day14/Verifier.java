030bfef56a4ddf3232f8113fd6d0a521160d44d9
Mark Little <mlittle@redhat.com>
2021-02-12 14:57:31 +0000

    Update Verifier.java

--- AdventOfCode/2019/day14/Verifier.java
+++ AdventOfCode/2019/day14/Verifier.java
@@ -21,6 +21,15 @@ public class Verifier
         _theParser = new Parser(debug);
     }
 
+    /*
+     * 10 ORE => 10 A
+     * 1 ORE => 1 B
+     * 7 A, 1 B => 1 C
+     * 7 A, 1 C => 1 D
+     * 7 A, 1 D => 1 E
+     * 7 A, 1 E => 1 FUEL
+     */
+
     public final boolean verify ()
     {
         Vector<Reaction> reactions = _theParser.loadData(EXAMPLE1_FILE);
@@ -35,15 +44,23 @@ public class Verifier
 
         HashMap<Chemical, Integer> inventory = new HashMap<Chemical, Integer>();
         Reaction fuel = findReaction(Chemical.FUEL, reactions);
+        Vector<Reactant> inventory = new Vector<Reactant>();
 
         if (fuel != null)
         {
             boolean completed = false;
             int fuelNeeded = fuel.chemicalCreated().getAmount();
+            Vector<Reactant> reactants = fuel.getReactants();
+
+            /*
+             * Go through each reactant and try to create the
+             * required amount, storing excess in the inventory.
+             * So check the inventory first, of course.
+             */
         }
         else
             System.out.println("Error! No fuel required?!");
-
+        
         return false;
     }
 
@@ -55,11 +72,39 @@ public class Verifier
         {
             Reaction r = iter.nextElement();
 
-            if (r.chemicalCreated().equals(name))
+            if (r.chemicalCreated().getChemical().getName().equals(name))
                 return r;
         }
 
         return null;
+    }
+
+    private int takeFromInventory (Reactant name, Vector<Reactant> inventory, int amountNeeded)
+    {
+        Enumeration<Reactant> iter = inventory.elements();
+        int amount = -1;
+
+        while (iter.hasMoreElements())
+        {
+            Reactant r = iter.nextElement();
+
+            if (r.getChemical().equals(name.getChemical()))
+            {
+                int amountFound = r.getAmount();
+
+                if (amountFound > 0)
+                {
+                    if (amountFound >= amountNeeded)
+                        amount = amountNeeded;
+                    else
+                        amount = amountNeeded - amountFound;
+
+                    r.setAmount(amountFound - amount);
+                }
+            }
+        }
+
+        return amount;
     }
 
     private boolean _debug;
