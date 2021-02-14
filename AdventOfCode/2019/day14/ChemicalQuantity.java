b7793cb1ab52da47e62a5af4d5d3a6f708d27e11
Mark Little <mlittle@redhat.com>
2021-02-14 14:53:35 +0000

    Update ChemicalQuantity.java

--- AdventOfCode/2019/day14/ChemicalQuantity.java
+++ AdventOfCode/2019/day14/ChemicalQuantity.java
@@ -37,7 +37,7 @@ public class ChemicalQuantity
     @Override
     public String toString ()
     {
-        return _amount+" "+_chemical.getName();
+        return _amount+":"+_chemical.getName();
     }
 
     // just compare chemical names
