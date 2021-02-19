3ff546ff74fc31438b5eaadf10847e264e2f8e44
Mark Little <mlittle@redhat.com>
2021-02-19 11:06:08 +0000

    Update NanoFactory.java

--- AdventOfCode/2019/day14/NanoFactory.java
+++ AdventOfCode/2019/day14/NanoFactory.java
@@ -82,7 +82,7 @@ public class NanoFactory
                 System.out.println("Quantity which would be created from reaction: "+r.chemicalCreated().getAmount());
             }
 
-            for (int i = 0; i < chemicalAndQuantity.getAmount(); i++)
+            for (int i = 0; i < chemicalAndQuantity.getAmount(); i++)  /// fail!!!!
             {
             /*
              * TODO we may need to run the same reaction multiple
