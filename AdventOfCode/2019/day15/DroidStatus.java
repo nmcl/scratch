c9f888bee996d3f903c557485c380f4df8133c21
Mark Little <mlittle@redhat.com>
2021-02-28 15:04:25 +0000

    Create DroidStatus.java

---
+++ AdventOfCode/2019/day15/DroidStatus.java
@@ -0,0 +1,25 @@
+public class DroidStatus
+{
+    public static final int COLLISION = 0;
+    public static final int MOVED = 1;
+    public static final int ARRIVED = 2;
+
+    public static final String toString (int status)
+    {
+        switch (status)
+        {
+            case COLLISION:
+                return "COLLISION";
+            case MOVED:
+                return "MOVED";
+            case ARRIVED:
+                return "ARRIVED";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    private DroidStatus ()  // prevent instantiation
+    {
+    }
+}
