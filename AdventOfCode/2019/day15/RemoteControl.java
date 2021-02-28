a8799f18989598142853d7131e2205d16ac8417b
Mark Little <mlittle@redhat.com>
2021-02-28 15:16:19 +0000

    Update RemoteControl.java

--- AdventOfCode/2019/day15/RemoteControl.java
+++ AdventOfCode/2019/day15/RemoteControl.java
@@ -2,13 +2,18 @@ public class RemoteControl
 {
     public static void main (String[] args)
     {
+        boolean debug = false;
+
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[0]))
             {
-                System.out.println("Usage: [-help]");
+                System.out.println("Usage: [-debug] [-help]");
                 System.exit(0);
             }
+
+            if ("-debug".equals(args[1]))
+                debug = true;
         }
     }
 }
