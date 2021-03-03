47e8be5a6059eb95126ae0812d7d4b298db2971e
Mark Little <mlittle@redhat.com>
2021-03-03 16:27:38 +0000

    Update Tile.java

--- AdventOfCode/2019/day15/Tile.java
+++ AdventOfCode/2019/day15/Tile.java
@@ -1,5 +1,10 @@
 public class Tile
 {
+    public Tile (Coordinate coord)
+    {
+        this(coord, TileId.UNEXPLORED);
+    }
+
     public Tile (Coordinate coord, String type)
     {
         _position = coord;
@@ -20,6 +25,33 @@ public class Tile
     public String toString ()
     {
         return _type;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (this == obj)
+            return true;
+        
+        // only check location not content
+
+        if (getClass() == obj.getClass())
+        {
+            Tile temp = (Tile) obj;
+
+            return _position.equals(temp._position);
+        }
+
+        return false;
     }
 
     private Coordinate _position;
