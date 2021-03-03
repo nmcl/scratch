f7660921d8da1910d18dae4c753062afea2503df
Mark Little <mlittle@redhat.com>
2021-03-03 15:02:05 +0000

    Update Maze.java

--- AdventOfCode/2019/day15/Maze.java
+++ AdventOfCode/2019/day15/Maze.java
@@ -7,6 +7,16 @@ public class Maze
         _map = new Vector<Tile>();
     }
 
+    public final void addContent (Coordinate coord, String type)
+    {
+        if (!_map.contains(coord))
+            _map.add(new Tile(coord, type));
+    }
+
+    public final boolean visited (Coordinate coord)
+    {
+        return _map.contains(coord);
+    }
 
     @Override
     public String toString ()
