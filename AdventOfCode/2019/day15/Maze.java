026a1db8d8d2c1d7457cf9ba9585e71b3c1ada6f
Mark Little <mlittle@redhat.com>
2021-03-03 16:19:22 +0000

    Update Maze.java

--- AdventOfCode/2019/day15/Maze.java
+++ AdventOfCode/2019/day15/Maze.java
@@ -5,10 +5,16 @@ public class Maze
     public Maze ()
     {
         _map = new Vector<Tile>();
+        _maxX = 0;
+        _minX = 0;
+        _maxY = 0;
+        _minY = 0;
     }
 
     public final void addContent (Coordinate coord, String type)
     {
+        // don't add if already present
+
         if (!_map.contains(coord))
             _map.add(new Tile(coord, type));
     }
@@ -21,8 +27,15 @@ public class Maze
     @Override
     public String toString ()
     {
+        int x = _minX;
+        int y = _minY;
+        
         return "";
     }
 
     private Vector<Tile> _map;
+    private int _maxX;
+    private int _minX;
+    private int _maxY;
+    private int _minY;
 }
