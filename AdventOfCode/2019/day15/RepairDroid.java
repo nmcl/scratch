648c209b99925e02b354ce5bd30b61de9cc6b66e
Mark Little <mlittle@redhat.com>
2021-03-09 14:32:41 +0000

    Update RepairDroid.java

--- AdventOfCode/2019/day15/RepairDroid.java
+++ AdventOfCode/2019/day15/RepairDroid.java
@@ -53,6 +53,8 @@ public class RepairDroid
 
             response = tryToMove(String.valueOf(DroidMovement.NORTH), DroidMovement.getNextPosition(_currentLocation, DroidMovement.NORTH));
 
+            System.out.println("**1 got back "+DroidStatus.toString(response));
+
             if ((response != DroidStatus.ARRIVED) && (response != DroidStatus.MOVED))
             {
                 System.out.println("**Failed to move NORTH");
@@ -60,6 +62,8 @@ public class RepairDroid
                 System.out.println("\n"+_theMap);
 
                 response = tryToMove(String.valueOf(DroidMovement.EAST), DroidMovement.getNextPosition(_currentLocation, DroidMovement.EAST));
+
+                System.out.println("**2 got back "+DroidStatus.toString(response));
 
                 if ((response != DroidStatus.ARRIVED) && (response != DroidStatus.MOVED))
                 {
@@ -69,6 +73,8 @@ public class RepairDroid
 
                     response = tryToMove(String.valueOf(DroidMovement.SOUTH), DroidMovement.getNextPosition(_currentLocation, DroidMovement.SOUTH));
 
+                    System.out.println("**3 got back "+DroidStatus.toString(response));
+
                     if ((response != DroidStatus.ARRIVED) && (response != DroidStatus.MOVED))
                     {
                         System.out.println("**Failed to move SOUTH");
@@ -76,6 +82,8 @@ public class RepairDroid
                         System.out.println("\n"+_theMap);
 
                         response = tryToMove(String.valueOf(DroidMovement.WEST), DroidMovement.getNextPosition(_currentLocation, DroidMovement.WEST));
+
+                        System.out.println("**4 got back "+DroidStatus.toString(response));
 
                         if ((response != DroidStatus.ARRIVED) && (response != DroidStatus.MOVED))
                         {
@@ -108,6 +116,8 @@ public class RepairDroid
                 return backtrack();
         }
 
+        System.out.println("**replying with "+DroidStatus.toString(response));
+        
         return response;
     }
 
