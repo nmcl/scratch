0a17a08f62c4de57f0e6e795d86ae751d78cecdd
Mark Little <mlittle@redhat.com>
2021-03-18 12:00:28 +0000

    Update FFT.java

---
+++ AdventOfCode/2019/day16/FFT.java
@@ -0,0 +1,26 @@
+public class FFT
+{
+    public FFT ()
+    {
+        public static void main (String[] args)
+        {
+            boolean debug = false;
+            boolean verify = false;
+
+            for (int i = 0; i < args.length; i++)
+            {
+                if ("-help".equals(args[i]))
+                {
+                    System.out.println("Usage: [-verify] [-debug] [-help]");
+                    System.exit(0);
+                }
+
+                if ("-debug".equals(args[i]))
+                    debug = true;
+
+                if ("-verify".equals(args[i]))
+                    verify = true;
+            }
+        }
+    }
+}
