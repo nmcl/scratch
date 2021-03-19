455cac4fd227a92a9716fe33752ef998d669acba
Mark Little <mlittle@redhat.com>
2021-03-19 12:46:15 +0000

    Update Compute.java

--- AdventOfCode/2019/day16/Compute.java
+++ AdventOfCode/2019/day16/Compute.java
@@ -1,4 +1,16 @@
 public class Compute
 {
     public static final int[] BASE_PATTERN = {0, 1, 0, -1};
+
+    public Compute (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    public int[] process (int[] input, int numberOfPhases)
+    {
+        return null;
+    }
+
+    boolean _debug;
 }
