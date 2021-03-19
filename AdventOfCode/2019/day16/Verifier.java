7366864d1e7ceabbffebc60eee6b4627659abd98
Mark Little <mlittle@redhat.com>
2021-03-19 16:33:14 +0000

    Update Verifier.java

--- AdventOfCode/2019/day16/Verifier.java
+++ AdventOfCode/2019/day16/Verifier.java
@@ -11,12 +11,24 @@ public class Verifier
     public boolean verify ()
     {
         boolean result = false;
-
-        _fft.
+        int[] data = convert(INPUT_SIGNAL_1);
 
         return result;
     }
 
+    private int[] convert (String input)
+    {
+        int[] result = new int[input.length()];
+        char[] sequence = input.toCharArray();
+
+        for (int i = 0; i < result.length; i++)
+        {
+            result[i] = Character.getNumericValue(sequence[i]);
+        }
+
+        return result;
+    }
+    
     private boolean _debug;
     Compute _fft;
 }
