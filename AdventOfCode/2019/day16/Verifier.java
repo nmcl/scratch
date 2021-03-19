8947e94ae376c471190444f392e0c47782c7d28f
Mark Little <mlittle@redhat.com>
2021-03-19 16:27:34 +0000

    Update Verifier.java

--- AdventOfCode/2019/day16/Verifier.java
+++ AdventOfCode/2019/day16/Verifier.java
@@ -1,4 +1,22 @@
 public class Verifier
 {
     public static final String INPUT_SIGNAL_1 = "12345678";
+
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+        _fft = new Compute(_debug);
+    }
+
+    public boolean verify ()
+    {
+        boolean result = false;
+
+        _fft.
+
+        return result;
+    }
+
+    private boolean _debug;
+    Compute _fft;
 }
