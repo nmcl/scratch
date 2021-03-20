f63f4c3d1f972898069a8da76f21e58529e2958f
Mark Little <mlittle@redhat.com>
2021-03-20 14:17:33 +0000

    Update Compute.java

--- AdventOfCode/2019/day16/Compute.java
+++ AdventOfCode/2019/day16/Compute.java
@@ -21,10 +21,28 @@ public class Compute
         return results;
     }
 
+    /*
+     * 10-1010-10
+     * 01100-1-10
+     * 00111000
+     * 00011110
+     * 00001111
+     * 00000111
+     * 00000011
+     * 00000001
+     * 
+     * 010-1
+     * 001100-1-1
+     * 000111000-1-1-1
+     * 000011110000-1-1-1-1
+     * 000001111100000-1-1-1-1-1
+     */
+
     private int[] processPhase (int[] input)
     {
         int[] results = new int[input.length];
         int basePatternIndex = 0;
+        int basePatternLoop = 1;
         int basePatternLowerBounds = 1;
 
         for (int i = 0; i < input.length; i++)
@@ -34,11 +52,13 @@ public class Compute
             for (int j = 0; j < input.length; j++)
             {
                 int[] work = new int[input.length];
+                int pattern = BASE_PATTERN[basePatternIndex];
+
 
                 work[j] = input[j] * BASE_PATTERN[basePatternIndex];
 
                 System.out.println(input[j]+"*"+BASE_PATTERN[basePatternIndex]);
-                
+
                 basePatternIndex++;
 
                 if (basePatternIndex == BASE_PATTERN.length)
