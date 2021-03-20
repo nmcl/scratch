06febc7ce767187f219bd33b275854b85e320c06
Mark Little <mlittle@redhat.com>
2021-03-20 13:29:34 +0000

    Update FFT.java

--- AdventOfCode/2019/day16/FFT.java
+++ AdventOfCode/2019/day16/FFT.java
@@ -1,26 +1,27 @@
 public class FFT
 {
-    public FFT ()
+    public static void main (String[] args)
     {
-        public static void main (String[] args)
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
         {
-            boolean debug = false;
-            boolean verify = false;
-
-            for (int i = 0; i < args.length; i++)
+            if ("-help".equals(args[i]))
             {
-                if ("-help".equals(args[i]))
-                {
-                    System.out.println("Usage: [-verify] [-debug] [-help]");
-                    System.exit(0);
-                }
-
-                if ("-debug".equals(args[i]))
-                    debug = true;
-
-                if ("-verify".equals(args[i]))
-                    verify = true;
+                System.out.println("Usage: [-verify] [-debug] [-help]");
+                System.exit(0);
             }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
         }
+
+        Verifier theVerifier = Verifier(debug);
+
+        theVerifier.verify();
     }
 }
