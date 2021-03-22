302c6feb94792a53d01f60acca193b549966a273
Mark Little <mlittle@redhat.com>
2021-03-22 17:55:41 +0000

    Update Compute.java

--- AdventOfCode/2019/day16/Compute.java
+++ AdventOfCode/2019/day16/Compute.java
@@ -21,6 +21,27 @@ public class Compute
         return results;
     }
 
+    private int[] processPhase (int[] input)
+    {
+        int[] results = new int[input.length];
+
+        for (int i = 0; i < input.length; i++)
+        {
+            int[] basePattern = getBasePattern(i+1);
+
+            for (int j = 0; i < basePattern.length; j++)
+            {
+                int[] work = new int[input.length];
+
+                work[j] = input[j] * basePattern[j];
+
+                System.out.println(input[j]+"*"+basePattern[j]);
+            }
+        }
+
+        return results;
+    }
+
     /*
      * A reminder of the example ...
      * 
@@ -39,44 +60,6 @@ public class Compute
      * 00011110000-1-1-1-1000011110000-1-1-1-1  --> miss first digit [000011110000-1-1-1-1]
      * 00001111100000-1-1-1-1-1  --> miss first digit [000001111100000-1-1-1-1-1]
      */
-
-    private int[] processPhase (int[] input)
-    {
-        int[] results = new int[input.length];
-
-        getBasePattern(3);
-
-        /*
-        int basePatternLowerBounds = 0;
-
-        for (int i = 0; i < input.length; i++)
-        {
-            int value = 0;
-            int basePatternIndex = 1;
-            int basePatternLoop = 0;
-
-            for (int j = 0; j < input.length; j++)
-            {
-                int[] work = new int[input.length];
-                int pattern = BASE_PATTERN[basePatternIndex];
-
-
-                work[j] = input[j] * BASE_PATTERN[basePatternIndex];
-
-                System.out.println(input[j]+"*"+BASE_PATTERN[basePatternIndex]);
-
-                if (basePatternIndex == BASE_PATTERN.length)
-                    basePatternIndex = basePatternLowerBounds;
-
-                value += work[j];
-            }
-
-            results[i] = value;
-        }
-*/
-
-        return results;
-    }
 
     private final int[] getBasePattern (int phase)
     {
