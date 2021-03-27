9f59eb88c7c65a38f6c7b0155c9f90d60d9c5a4f
Mark Little <mlittle@redhat.com>
2021-03-27 15:38:26 +0000

    Update Verifier.java

--- AdventOfCode/2019/day16/part1/Verifier.java
+++ AdventOfCode/2019/day16/part1/Verifier.java
@@ -6,14 +6,14 @@ public class Verifier
     public static final int[] INPUT_PHASE_3 = {0, 3, 4, 1, 5, 5, 1, 8};
     public static final int[] INPUT_PHASE_4 = {0, 1, 0, 2, 9, 4, 9, 8};
 
-    public static String INPUT_SIGNAL_2 = "80871224585914546619083218645595";
-    public static int[] SIGNAL_2_OUTPUT = {2, 4, 1, 7, 6, 1, 7, 6};
+    public static final String INPUT_SIGNAL_2 = "80871224585914546619083218645595";
+    public static final int[] SIGNAL_2_OUTPUT = {2, 4, 1, 7, 6, 1, 7, 6};
 
-    public static String INPUT_SIGNAL_3 = "19617804207202209144916044189917";
-    public static int[] SIGNAL_3_OUTPUT = {7, 3, 7, 4, 5, 4, 1, 8};
+    public static final String INPUT_SIGNAL_3 = "19617804207202209144916044189917";
+    public static final int[] SIGNAL_3_OUTPUT = {7, 3, 7, 4, 5, 4, 1, 8};
 
-    public static String INPUT_SIGNAL_4 = "69317163492948606335995924319873";
-    public static int[] SIGNAL_4_OUTPUT = {5, 2, 4, 3, 2, 1, 3, 3};
+    public static final String INPUT_SIGNAL_4 = "69317163492948606335995924319873";
+    public static final int[] SIGNAL_4_OUTPUT = {5, 2, 4, 3, 2, 1, 3, 3};
 
     public Verifier (boolean debug)
     {
