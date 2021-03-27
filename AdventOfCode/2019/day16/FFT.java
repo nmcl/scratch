a6c6590138433551f81d71a290326471565a1f88
Mark Little <mlittle@redhat.com>
2021-03-27 14:27:19 +0000

    final.

--- AdventOfCode/2019/day16/FFT.java
+++ AdventOfCode/2019/day16/FFT.java
@@ -1,5 +1,10 @@
+import java.util.*;
+import java.io.*;
+
 public class FFT
 {
+    public static final String DATA_FILE = "data.txt";
+
     public static void main (String[] args)
     {
         boolean debug = false;
@@ -20,12 +25,58 @@ public class FFT
                 verify = true;
         }
 
-        Verifier theVerifier = new Verifier(debug);
-        boolean verified = theVerifier.verify();
+        if (verify)
+        {
+            Verifier theVerifier = new Verifier(debug);
+            boolean verified = theVerifier.verify();
 
-        if (verified)
-            System.out.println("Verified ok.");
+            if (verified)
+                System.out.println("Verified ok.");
+            else
+                System.out.println("Verify failed.");
+        }
         else
-            System.out.println("Verify failed.");
+        {
+            /*
+             * Open the data file and read it in.
+             */
+
+            BufferedReader reader = null;
+            String line = null;
+
+            try
+            {
+                reader = new BufferedReader(new FileReader(DATA_FILE));
+
+                line = reader.readLine();
+            }
+            catch (Throwable ex)
+            {
+                ex.printStackTrace();
+            }
+            finally
+            {
+                try
+                {
+                    reader.close();
+                }
+                catch (Throwable ex)
+                {
+                }
+            }
+
+            Compute fft = new Compute(debug);
+            int[] input = Util.convert(line);
+            int[] data = fft.process(input, 100);
+
+            System.out.print("First 8 digits: ");
+
+            for (int i = 0; i < 8; i++)
+            {
+                System.out.print(data[i]);
+            }
+
+            System.out.println();
+        }
     }
 }
