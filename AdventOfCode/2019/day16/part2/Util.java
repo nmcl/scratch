d7a71176bb509efceec4f483114086c4efe70823
Mark Little <mlittle@redhat.com>
2021-03-27 18:16:42 +0000

    Update Util.java

--- AdventOfCode/2019/day16/part2/Util.java
+++ AdventOfCode/2019/day16/part2/Util.java
@@ -1,5 +1,7 @@
 public class Util
 {
+    public static final int OFFSET = 7;
+
     public static int[] convert (String input)
     {
         int[] result = new int[input.length()];
@@ -11,6 +13,35 @@ public class Util
         }
 
         return result;
+    }
+
+    public static int[] replicate (String input, int times)
+    {
+        int[] data = convert(input);
+        int[] toReturn = new int[data.length * times];
+        int index = 0;
+
+        for (int i = 0; i < times; i++)
+        {
+            System.arraycopy(data, 0, toReturn, index, data.length);
+
+            index += data.length;
+        }
+
+        return toReturn;
+    }
+
+    public static int[] offset (String input)
+    {
+        int[] data = convert(input);
+        int[] offset = new int[OFFSET];
+
+        for (int i = 0; i < OFFSET; i++)
+        {
+            offset[i] = data[i];
+        }
+
+        return offset;
     }
 
     private Util ()
