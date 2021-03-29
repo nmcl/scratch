8735b546627aa9bb9ad00f6accb227d2c7162224
Mark Little <mlittle@redhat.com>
2021-03-29 14:35:25 +0100

    Update Verifier.java

--- AdventOfCode/2019/day16/part2/Verifier.java
+++ AdventOfCode/2019/day16/part2/Verifier.java
@@ -19,7 +19,7 @@ public class Verifier
     {
         boolean result = true;
         int[] signal = Util.replicate(INPUT_SIGNAL_1, Util.REPEAT_SIZE);
-        int[] data = _fft.process(signal, Util.PHASES);
+        int[] data = _fft.fastProcess(signal, Util.PHASES);
         long offset = Util.offset(INPUT_SIGNAL_1);
 
         //if (_debug)
