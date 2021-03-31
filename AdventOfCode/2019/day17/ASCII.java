0a74072f96ac69a5393183a835650518f096c0f6
Mark Little <mlittle@redhat.com>
2021-03-31 11:30:00 +0100

    Update ASCII.java

--- AdventOfCode/2019/day17/ASCII.java
+++ AdventOfCode/2019/day17/ASCII.java
@@ -1,1 +1,27 @@
 // Aft Scaffolding Control and Information Interface
+
+public class ASCII 
+{
+    public static final String INSTRUCTIONS = "instructions.txt";
+
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
+    }
+
+    private ASCII ()
+    {
+    }
+}
