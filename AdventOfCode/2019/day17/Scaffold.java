decad5764921c33ce1773152725d1bd19970a99b
Mark Little <mlittle@redhat.com>
2021-04-02 13:42:44 +0100

    Update Scaffold.java

--- AdventOfCode/2019/day17/Scaffold.java
+++ AdventOfCode/2019/day17/Scaffold.java
@@ -1,10 +1,63 @@
+import java.util.*;
+
 public class Scaffold
 {
-    public static final char SCAFFOLDING = '#';
-    public static final char OPEN_SPACE = '.';
-    
-    public Scaffold ()
-    {
+    public static final String SCAFFOLDING = "#";
+    public static final String OPEN_SPACE = ".";
 
+    // ASCII codes.
+
+    public static final String SCAFFOLDING_CODE = "35";
+    public static final String OPEN_SPACE_CODE = "46";
+    public static final String NEW_LINE = "10";
+
+    public Scaffold (boolean debug)
+    {
+        _image = new Vector<String>();
+        _debug = debug;
     }
+
+    public void addData (String value)
+    {
+        if ((SCAFFOLDING_CODE.equals(value)) || (OPEN_SPACE_CODE.equals(value)) || (NEW_LINE.equals(value)))
+            _image.add(value);
+        else
+        {
+            if (_debug)
+                System.out.println("Invalid code: "+value);
+        }
+    }
+
+    @Override
+    public String toString ()
+    {
+        Enumeration<String> iter = _image.elements();
+        String str = "";
+
+        while (iter.hasMoreElements())
+        {
+            String val = iter.nextElement();
+
+            switch (val)
+            {
+                case SCAFFOLDING_CODE:
+                {
+                    str += SCAFFOLDING;
+                }
+                break;
+                case OPEN_SPACE_CODE:
+                {
+                    str += OPEN_SPACE;
+                }
+                break;
+                default:
+                    str += "\n";
+            }
+        }
+
+        return str;
+    }
+
+    private Vector<String> _image;
+    private boolean _debug;
 }
