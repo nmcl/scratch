c03554045e153c147f69373cadbdb31e94eb1e76
Mark Little <mlittle@redhat.com>
2021-04-02 11:26:12 +0100

    Update Camera.java

--- AdventOfCode/2019/day17/Camera.java
+++ AdventOfCode/2019/day17/Camera.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Camera
 {
+    // ASCII codes.
+    
+    public static final int SCAFFOLDING_CODE = 35;
+    public static final int OPEN_SPACE_CODE = 46;
+    public static final int NEW_LINE = 10;
+
     public Camera (Vector<String> values, String initialInput, boolean debug)
     {
         _computer = new Intcode(values, initialInput, debug);
