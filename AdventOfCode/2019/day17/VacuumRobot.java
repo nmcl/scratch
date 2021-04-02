492d4d93926f0995a15f0660e66b75f0f385a9ec
Mark Little <mlittle@redhat.com>
2021-04-02 11:26:19 +0100

    Update VacuumRobot.java

--- AdventOfCode/2019/day17/VacuumRobot.java
+++ AdventOfCode/2019/day17/VacuumRobot.java
@@ -4,6 +4,7 @@ public class VacuumRobot
     public static final char DOWN = 'v';
     public static final char RIGHT = '<';
     public static final char LEFT = '>';
+    public static final char TUMBLING = 'X';
 
     public VacuumRobot ()
     {
