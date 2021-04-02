a344f8e63fdc2f5228639f187ef9acb3da0bd2ba
Mark Little <mlittle@redhat.com>
2021-04-02 14:13:45 +0100

    Update VacuumRobot.java

--- AdventOfCode/2019/day17/VacuumRobot.java
+++ AdventOfCode/2019/day17/VacuumRobot.java
@@ -1,13 +1,55 @@
 public class VacuumRobot
 {
-    public static final char UP = '^';
-    public static final char DOWN = 'v';
-    public static final char RIGHT = '<';
-    public static final char LEFT = '>';
-    public static final char TUMBLING = 'X';
+    public static final String UP = "^";
+    public static final String DOWN = "v";
+    public static final String RIGHT = "<";
+    public static final String LEFT = ">";
+    public static final String TUMBLING = "X";
 
-    public VacuumRobot ()
+    // ASCII codes.
+
+    public static final String UP_CODE = "94";
+    public static final String DOWN_CODE = "118";
+    public static final String RIGHT_CODE = "60";
+    public static final String LEFT_CODE = "62";
+    public static final String TUMBLING_CODE = "88";
+
+    public VacuumRobot (boolean debug)
     {
-
+        _debug = debug;
     }
+
+    public static final String print (String code)
+    {
+        switch (code)
+        {
+            case UP_CODE:
+                return UP;
+            case DOWN_CODE:
+                return DOWN;
+            case RIGHT_CODE:
+                return RIGHT;
+            case LEFT_CODE:
+                return LEFT;
+            default:
+                return TUMBLING;
+        }
+    }
+
+    public static final boolean isRobotCode (String code)
+    {
+        switch (code)
+        {
+            case UP_CODE:
+            case DOWN_CODE:
+            case RIGHT_CODE:
+            case LEFT_CODE:
+            case TUMBLING_CODE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean _debug;
 }
