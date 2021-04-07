bfb7840f66f9a7734efaf785b36b36165740f011
Mark Little <mlittle@redhat.com>
2021-04-07 15:36:16 +0100

    Update Cell.java

--- AdventOfCode/2019/day17/part2/Cell.java
+++ AdventOfCode/2019/day17/part2/Cell.java
@@ -4,8 +4,14 @@
 
 public class Cell
 {
-    public Cell ()
+    public Cell (Coordinate coord, String cellType, boolean debug)
     {
-
+        _coord = coord;
+        _scaffold = CellId.SCAFFOLDING.equals(cellType);
+        _debug = debug;
     }
+
+    private Coordinate _coord;
+    private boolean _scaffold;
+    private boolean _debug;
 }
