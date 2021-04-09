34441041ec3bdfe6ba44cb4cce61fa47b007d4d5
Mark Little <mlittle@redhat.com>
2021-04-09 12:33:51 +0100

    Create MovementProgram.java

---
+++ AdventOfCode/2019/day17/part2/MovementProgram.java
@@ -0,0 +1,21 @@
+public class MovementProgram
+{
+    public static final int ROUTINE_A = 0;
+    public static final int ROUTINE_B = 1;
+    public static final int ROUTINE_C = 2;
+    public static final int ROUTINE_D = 3;
+
+    public static void createMovementFunctions ()
+    {
+
+    }
+
+    public static void createMovementRoutine ()
+    {
+        
+    }
+
+    private MovementProgram ()
+    {
+    }
+}
