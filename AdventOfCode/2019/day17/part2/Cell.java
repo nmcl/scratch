a57aee5736f42b1608f40d8091e0bf022cb5fe0c
Mark Little <mlittle@redhat.com>
2021-04-09 14:17:37 +0100

    Update Cell.java

--- AdventOfCode/2019/day17/part2/Cell.java
+++ AdventOfCode/2019/day17/part2/Cell.java
@@ -11,6 +11,11 @@ public class Cell
         _debug = debug;
     }
 
+    public Cell (Coordinate coord)
+    {
+        this(coord, CellId.OPEN_SPACE, false); // some defaults
+    }
+
     public final String getContents ()
     {
         return _contents;
@@ -19,6 +24,31 @@ public class Cell
     public final Coordinate position ()
     {
         return _coord;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        return _coord.hashCode();
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (this == obj)
+            return true;
+        
+        if (getClass() == obj.getClass())
+        {
+            Cell temp = (Cell) obj;
+
+            return temp.position().equals(position());
+        }
+
+        return false;
     }
 
     @Override
