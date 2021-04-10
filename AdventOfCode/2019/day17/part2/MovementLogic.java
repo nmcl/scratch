b6c742c61624157590bc5d8ca3ab707a7f8757f8
Mark Little <mlittle@redhat.com>
2021-04-10 12:51:32 +0100

    Update MovementLogic.java

--- AdventOfCode/2019/day17/part2/MovementLogic.java
+++ AdventOfCode/2019/day17/part2/MovementLogic.java
@@ -15,6 +15,7 @@ public class MovementLogic
         _theMap = theMap;
         _path = new Stack<String>();
         _robotFacing = CellId.ROBOT_FACING_UP;
+        _currentPosition = new Coordinate(_theMap.findStartingPoint());
         _debug = debug;
     }
 
@@ -34,18 +35,81 @@ public class MovementLogic
 
     private void createPath ()
     {
-        Coordinate currentPosition = new Coordinate(_theMap.findStartingPoint());
-        
-        if (currentPosition != null)
+        if (_currentPosition != null)
         {
             // try R then L
 
+            if (!tryMoveRight())
+                tryMoveLeft();
         }
         else
             System.out.println("Robot not found!");
     }
 
-    private Coordinate leftPosition (Coordinate coord)
+    private boolean tryMoveRight ()
+    {
+        Coordinate coord = rightPosition(_currentPosition);
+
+        if (_theMap.isScaffold(coord))
+        {
+            _path.push(MOVE_RIGHT);
+            _currentPosition = coord;
+
+            return tryMoveRight();
+        }
+        else
+            return false;
+    }
+
+    private boolean tryMoveLeft ()
+    {
+        Coordinate coord = leftPosition(_currentPosition);
+
+        if (_theMap.isScaffold(coord))
+        {
+            _path.push(MOVE_LEFT);
+            _currentPosition = coord;
+
+            return tryMoveLeft();
+        }
+        else
+            return false;
+    }
+
+    private final Coordinate rightPosition (Coordinate coord)
+    {
+        int x = coord.getX();
+        int y = coord.getY();
+
+        switch (_robotFacing)
+        {
+            case CellId.ROBOT_FACING_DOWN:
+            {
+                x--;
+            }
+            break;
+            case CellId.ROBOT_FACING_UP:
+            {
+                x++;
+            }
+            break;
+            case CellId.ROBOT_FACING_LEFT:
+            {
+                y++;
+            }
+            break;
+            case CellId.ROBOT_FACING_RIGHT:
+            default:
+            {
+                y--;
+            }
+            break;
+        }
+
+        return new Coordinate(x, y);
+    }
+
+    private final Coordinate leftPosition (Coordinate coord)
     {
         int x = coord.getX();
         int y = coord.getY();
@@ -59,19 +123,28 @@ public class MovementLogic
             break;
             case CellId.ROBOT_FACING_UP:
             {
-
+                x--;
             }
+            break;
             case CellId.ROBOT_FACING_LEFT:
             {
-
+                y--;
             }
+            break;
             case CellId.ROBOT_FACING_RIGHT:
             default:
+            {
+                y++;
+            }
+            break;
         }
+
+        return new Coordinate(x, y);
     }
 
     private Map _theMap;
     private Stack<String> _path;
     private String _robotFacing;
+    private Coordinate _currentPosition;
     private boolean _debug;
 }
