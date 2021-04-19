91382893f6a0f51f6517f884ace1636be68458d0
Mark Little <mlittle@redhat.com>
2021-04-19 16:45:10 +0100

    Update Trail.java

--- AdventOfCode/2019/day17/part2/Trail.java
+++ AdventOfCode/2019/day17/part2/Trail.java
@@ -12,6 +12,26 @@ public class Trail extends Map
         super.createMapFromImage(_originalImage.scannedLines());
     }
 
+    public boolean trailTrodden (Coordinate coord)
+    {
+        Cell temp = new Cell(coord);
+        int index = super._theMap.indexOf(temp);
+
+        temp = super._theMap.get(index);
+
+        switch (temp.getContents())
+        {
+            case CellId.ROBOT_FACING_UP:
+            case CellId.ROBOT_FACING_DOWN:
+            case CellId.ROBOT_FACING_LEFT:
+            case CellId.ROBOT_FACING_RIGHT:
+                return true;
+            case CellId.SCAFFOLDING:
+            default:
+                return false;               
+        }
+    }
+
     public void changeElement (Coordinate coord, String type)
     {
         Cell temp = new Cell(coord);
