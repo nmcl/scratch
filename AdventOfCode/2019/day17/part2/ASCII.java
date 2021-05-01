0b64ddc67fa729bc039887a1972f556bc4977022
Mark Little <mlittle@redhat.com>
2021-05-01 16:35:02 +0100

    Update ASCII.java

--- AdventOfCode/2019/day17/part2/ASCII.java
+++ AdventOfCode/2019/day17/part2/ASCII.java
@@ -51,6 +51,12 @@ public class ASCII
         MovementLogic ml = new MovementLogic(theMap, debug);
 
         ml.createMovementFunctions();
+
+        System.out.println("\n\n");
+        String str = "L,4L,4L,6R,10L,6L,4L,4L,6R,10L,6L,12L,6R,10L,6R,8R,10L,6R,8R,10L,6L,4L,4L,6R,10L,6R,8R,10L,6L,12L,6R,10L,6R,8R,10L,6L,12L,6R,10L,6";
+
+        String repeated = str.replaceAll("(.+?)\\1+", "$1");
+        System.out.println(repeated);
     }
 
     private ASCII ()
