6cf5b0face9b8a1dd7c5f1c26863adeb74cea0da
Mark Little <mlittle@redhat.com>
2021-05-09 16:49:35 +0100

    Update Verifier.java

--- AdventOfCode/2019/day17/part2/Verifier.java
+++ AdventOfCode/2019/day17/part2/Verifier.java
@@ -7,24 +7,40 @@ import java.util.*;
 public class Verifier 
 {
     public static final String EXAMPLE = "R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2";
+    public static final String RESULT = "A,C,B,C,A,B,B";
 
     public static void main (String[] args)
     {
         boolean debug = false;
+        int maxCharacters = FunctionRoutine.MAX_CHARACTERS;
 
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-debug] [-help]");
+                System.out.println("Usage: [-char <number>] [-debug] [-help]");
                 System.exit(0);
             }
 
             if ("-debug".equals(args[i]))
                 debug = true;
+
+            if ("-char".equals(args[i]))
+            {
+                try
+                {
+                    maxCharacters = Integer.parseInt(args[i+1]);
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+
+                    System.exit(0);
+                }
+            }
         }
 
-        FunctionRoutine fr = new FunctionRoutine(EXAMPLE, debug);
+        FunctionRoutine fr = new FunctionRoutine(EXAMPLE, maxCharacters, debug);
         Vector<MovementFunction> functions = fr.createMovementFunctions();
 
         if (functions.size() != MovementLogic.NUMBER_OF_FUNCTIONS)
@@ -42,6 +58,11 @@ public class Verifier
             MovementRoutine mr = new MovementRoutine(EXAMPLE, functions, debug);
 
             System.out.println("Routine is "+mr.getMainRoutine());
+
+            if (RESULT.equals(mr.getMainRoutine()))
+                System.out.println("Verified ok.");
+            else
+                System.out.println("Verify failed!");
         }
     }
 
