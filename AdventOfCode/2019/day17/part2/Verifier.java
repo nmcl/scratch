42e6d93ab7fabcb267f84488f7065f8692f76adc
Mark Little <mlittle@redhat.com>
2021-05-09 15:43:11 +0100

    Create Verifier.java

---
+++ AdventOfCode/2019/day17/part2/Verifier.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+/*
+ * Aft Scaffolding Control and Information Interface.
+ */
+
+public class Verifier 
+{
+    public static final String EXAMPLE = "R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2";
+
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
+
+        Vector<String> values = IntcodeUtil.readValues(INSTRUCTIONS);
+        Camera theCamera = new Camera(values, debug);
+        Image theImage = theCamera.takePicture();
+        
+        if (debug)
+        {
+            System.out.println(theCamera);
+
+            Vector<Coordinate> intersections = theCamera.scanForIntersections();
+            Enumeration<Coordinate> iter = intersections.elements();
+
+            while (iter.hasMoreElements())
+            {
+                System.out.println("Intersection at: "+iter.nextElement());
+            }
+        }
+
+        Map theMap = new Map(theImage, debug);
+        VacuumRobot theRobot = new VacuumRobot(theMap, IntcodeUtil.readValues(INSTRUCTIONS), debug);
+
+        // print the camera's image/map
+        
+        System.out.println(theMap);
+
+        MovementLogic ml = new MovementLogic(theMap, debug);
+
+        Vector<MovementFunction> functions = ml.createMovementFunctions();
+
+        if (functions.size() != MovementLogic.NUMBER_OF_FUNCTIONS)
+            System.out.println("Got the wrong number of functions: "+functions.size());
+        else
+        {
+            if (debug)
+            {
+                for (int i = 0; i < MovementLogic.NUMBER_OF_FUNCTIONS; i++)
+                {
+                    System.out.println("Function "+(i+1)+" is "+functions.elementAt(i));
+                }
+            }
+
+            System.out.println("Routine is "+ml.getMainRoutine(functions));
+        }
+    }
+
+    private ASCII ()
+    {
+    }
+}
