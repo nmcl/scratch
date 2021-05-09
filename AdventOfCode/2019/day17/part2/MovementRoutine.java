b94b591316dd09447af351802dbddcbdbef2ed73
Mark Little <mlittle@redhat.com>
2021-05-09 11:02:14 +0100

    Update MovementRoutine.java

--- AdventOfCode/2019/day17/part2/MovementRoutine.java
+++ AdventOfCode/2019/day17/part2/MovementRoutine.java
@@ -1,7 +1,28 @@
+import java.util.*;
+
 public class MovementRoutine
 {
-    public MovementRoutine ()
+    public MovementRoutine (String fullCommand, Vector<MovementFunction> functions, boolean debug)
     {
-
+        _fullCommand = fullCommand;
+        _functions = functions;
+        _debug = debug;
     }
+
+    public String getMainRoutine ()
+    {
+        /*
+         * We know that Function A (1) is first in the sequence, followed by
+         * B (2) and then C (3) is last but we don't know the order they follow
+         * after the first.
+         */
+
+        String str = "A";
+
+        return str;
+    }
+
+    private String _fullCommand;
+    private Vector<MovementFunction> _functions;
+    private boolean _debug;
 }
