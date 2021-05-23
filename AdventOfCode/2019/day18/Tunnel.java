d91a91c1e26ae442fdc9e4243c8f487ad0841217
Mark Little <mlittle@redhat.com>
2021-05-23 10:54:56 +0100

    Update Tunnel.java

--- AdventOfCode/2019/day18/Tunnel.java
+++ AdventOfCode/2019/day18/Tunnel.java
@@ -1,14 +1,20 @@
 public class Tunnel
 {
+    public static final String TUNNEL_DATA = "tunnel.txt"
     public static final void main (String[] args)
     {
+        String dataFile = TUNNEL_DATA;
+
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-help]");
+                System.out.println("Usage: [-data <file>] [-help]");
                 System.exit(0);
             }
+            
+            if ("-data".equals(args[i]))
+                dataFile = args[i+1];
         }
     }
 }
