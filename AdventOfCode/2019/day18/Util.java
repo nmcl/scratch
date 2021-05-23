84af35da9ad19b083510073dcdf1274cd1ee32d0
Mark Little <mlittle@redhat.com>
2021-05-23 16:18:03 +0100

    Create Util.java

---
+++ AdventOfCode/2019/day18/Util.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Util
+{
+    public static final Vector<String> readMap (String inputFile)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<String> map = new Vector<String>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                map.add(line);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return map;
+    }
+
+    private Util ()
+    {
+    }
+}
