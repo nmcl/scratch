afcbf84807f32fb8132496a5c213463715297e0f
Mark Little <mlittle@redhat.com>
2021-05-23 16:18:05 +0100

    Update Map.java

--- AdventOfCode/2019/day18/Map.java
+++ AdventOfCode/2019/day18/Map.java
@@ -6,95 +6,12 @@ import java.util.*;
 
 public class Map
 {
-    public Map (Image img, boolean debug)
+    public Map (Vector<String> input, boolean debug)
     {
         _theMap = new Vector<Cell>();
-        _maxX = 0;
-        _maxY = 0;
-        _originalImage = img;
         _debug = debug;
 
-        createMapFromImage(img.scannedLines());
-    }
-
-    public Map (Map toCopy)
-    {
-        _theMap = new Vector<Cell>();
-        _maxX = 0;
-        _maxY = 0;
-        _originalImage = toCopy._originalImage;
-        _debug = toCopy._debug;
-
-        createMapFromImage(_originalImage.scannedLines());
-    }
-
-    public Coordinate findStartingPoint ()
-    {
-        Enumeration<Cell> iter = _theMap.elements();
-
-        while (iter.hasMoreElements())
-        {
-            Cell cur = iter.nextElement();
-
-            if (cur.getContents().equals(CellId.ROBOT_FACING_UP))
-                return cur.position();
-        }
-
-        return null;
-    }
-
-    public boolean isScaffold (Coordinate coord)
-    {
-        if (!valid(coord))
-            return false;
-
-        Cell temp = new Cell(coord);
-        int index = _theMap.indexOf(temp);
-
-        temp = _theMap.get(index);
-
-        return (temp.getContents().equals(CellId.SCAFFOLDING));
-    }
-
-    public boolean isOpenSpace (Coordinate coord)
-    {
-        if (_debug)
-            System.out.println("Checking space "+coord);
-
-        if (!valid(coord))
-            return false;
-
-        Cell temp = new Cell(coord);
-        int index = _theMap.indexOf(temp);
-
-        return (_theMap.get(index).getContents().equals(CellId.OPEN_SPACE));
-    }
-
-    public boolean theEnd (Coordinate coord)
-    {
-        int numberOfExits = 0;
-
-        Coordinate nCoord = new Coordinate(coord.getX(), coord.getY()+1);
-        Coordinate sCoord = new Coordinate(coord.getX(), coord.getY()-1);
-        Coordinate eCoord = new Coordinate(coord.getX()+1, coord.getY());
-        Coordinate wCoord = new Coordinate(coord.getX()-1, coord.getY());
-
-        if (!isOpenSpace(nCoord))
-            numberOfExits++;
-        
-        if (!isOpenSpace(sCoord))
-            numberOfExits++;
-        
-        if (!isOpenSpace(eCoord))
-            numberOfExits++;
-
-        if (!isOpenSpace(wCoord))
-            numberOfExits++;
-
-        if (_debug)
-            System.out.println("Number of exits for "+coord+" is "+numberOfExits);
-        
-        return (numberOfExits == 1);
+        createMap(input);
     }
 
     @Override
@@ -154,18 +71,6 @@ public class Map
         }
     }
 
-    protected Map ()
-    {
-        _theMap = new Vector<Cell>();
-        _maxX = 0;
-        _maxY = 0;
-        _originalImage = null;
-        _debug = false;
-    }
-
     protected Vector<Cell> _theMap;
-    protected int _maxX;
-    protected int _maxY;
-    protected Image _originalImage;
     protected boolean _debug;
 }
