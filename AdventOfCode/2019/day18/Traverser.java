365c867ae34e5c4dafde67fd64c6f4f672d6f5a1
Mark Little <mlittle@redhat.com>
2021-05-25 12:43:03 +0100

    Update Traverser.java

---
+++ AdventOfCode/2019/day18/Traverser.java
@@ -0,0 +1,11 @@
+public class Traverser
+{
+    public Traverser (Map theMap, boolean debug)
+    {
+        _theMap = theMap;
+        _debug = debug;
+    }
+
+    private Map _theMap;
+    private boolean _debug;
+}
