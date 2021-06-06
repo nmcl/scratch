a89768e2c830317d61b520ad553f70772c8fd5e1
Mark Little <mlittle@redhat.com>
2021-06-06 17:06:08 +0100

    Update Map.java

--- AdventOfCode/2019/day18/Map.java
+++ AdventOfCode/2019/day18/Map.java
@@ -20,6 +20,11 @@ public class Map
         return _entrance;
     }
 
+    public Node getStartingNode ()
+    {
+        return createGraph();
+    }
+
     @Override
     public String toString ()
     {
@@ -74,67 +79,70 @@ public class Map
         _maxY = y+1;
     }
 
-    // from the map create a graph
+     // from the map create a graph
 
-    Node createGraph ()  // temporary public for testing!
-    {
-        Enumeration<Cell> iter = _theMap.elements();
-        int x = 0;
-        int y = 0;
+     private Node createGraph ()  // temporary public for testing!
+     {
+         if (_nodeMap != null)
+            return _nodeMap[_entrance.getX()][_entrance.getY()];
 
-        _nodeMap = new Node[_maxX][_maxY];
-
-        while (iter.hasMoreElements())
-        {
-            _nodeMap[x][y] = new Node(iter.nextElement());
-
-            x++;
-
-            if (x == _maxX)
-            {
-                x = 0;
-                y++;
-            }
-        }
-
-        for (int i = 0; i < _maxX; i++)
-        {
-            for (int j = 0; j < _maxY; j++)
-            {
-                Node up = null;
-                Node down = null;
-                Node left = null;
-                Node right = null;
-
-                if (i -1 >= 0)
-                    left = _nodeMap[i-1][j];
-                
-                if (i +1 < _maxX)
-                    right = _nodeMap[i +1][j];
-
-                if (j -1 >= 0)
-                    up = _nodeMap[i][j -1];
-
-                if (j +1 < _maxY)
-                    down = _nodeMap[i][j +1];
-
-                _nodeMap[i][j].setLinks(up, down, left, right);
-            }
-        }
-
-        if (_debug)
-        {
-            for (x = 0; x < _maxX; x++)
-            {
-                for (y = 0; y < _maxY; y++)
-                {
-                    System.out.println("\n"+_nodeMap[x][y]);
-                }
-            }
-        }
-
-        return _nodeMap[_entrance.getX()][_entrance.getY()];
-    }
+         Enumeration<Cell> iter = _theMap.elements();
+         int x = 0;
+         int y = 0;
+ 
+         _nodeMap = new Node[_maxX][_maxY];
+ 
+         while (iter.hasMoreElements())
+         {
+             _nodeMap[x][y] = new Node(iter.nextElement());
+ 
+             x++;
+ 
+             if (x == _maxX)
+             {
+                 x = 0;
+                 y++;
+             }
+         }
+ 
+         for (int i = 0; i < _maxX; i++)
+         {
+             for (int j = 0; j < _maxY; j++)
+             {
+                 Node up = null;
+                 Node down = null;
+                 Node left = null;
+                 Node right = null;
+ 
+                 if (i -1 >= 0)
+                     left = _nodeMap[i-1][j];
+                 
+                 if (i +1 < _maxX)
+                     right = _nodeMap[i +1][j];
+ 
+                 if (j -1 >= 0)
+                     up = _nodeMap[i][j -1];
+ 
+                 if (j +1 < _maxY)
+                     down = _nodeMap[i][j +1];
+ 
+                 _nodeMap[i][j].setLinks(up, down, left, right);
+             }
+         }
+ 
+         if (_debug)
+         {
+             for (x = 0; x < _maxX; x++)
+             {
+                 for (y = 0; y < _maxY; y++)
+                 {
+                     System.out.println("\n"+_nodeMap[x][y]);
+                 }
+             }
+         }
+ 
+         return _nodeMap[_entrance.getX()][_entrance.getY()];
+     }
 
     private Vector<Cell> _theMap;
     private Node[][] _nodeMap;
