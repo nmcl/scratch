1317d19dfdc139f4035f6d79859abf0afe10cc64
Mark Little <mlittle@redhat.com>
2021-06-08 17:10:34 +0100

    Update CellId.java

--- AdventOfCode/2019/day18/CellId.java
+++ AdventOfCode/2019/day18/CellId.java
@@ -8,6 +8,9 @@ public class CellId
     public static final char OPEN_PASSAGE = '.';
     public static final char STONE_WALL = '#';
     
+    public static final char FIRST_KEY = 'a';
+    public static final char FIRST_DOOR = 'A';
+    
     private CellId ()
     {
     }
