808d0089fdb0ec4103f7c35acc9b2e339f18d9a4
Mark Little <mlittle@redhat.com>
2021-06-08 14:26:32 +0100

    Update Explorer.java

--- AdventOfCode/2019/day18/Explorer.java
+++ AdventOfCode/2019/day18/Explorer.java
@@ -1,3 +1,10 @@
+/*
+ * Go for all keys in sane direction until we hit a door that
+ * we can't pass then change direction.
+ *
+ * BFS from current location to next key.
+ */
+
 public class Explorer
 {
     public static final int MAXIMUM_KEYS = 26;
@@ -37,10 +44,8 @@ public class Explorer
     }
 
     // Encode the key set as a bitmask to make comparisons fast.
-    // go for all keys in sane direction until we hit a door that
-    // we can't pass then change direction.
-    // BFS from current location to next key.
-    
+
+
     private Node _start;
     private char[] _keys;
     private int _keysFound;
