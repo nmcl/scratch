248e142a04987fa3f0aca2dcb635d6c9088e4fbb
Mark Little <mlittle@redhat.com>
2021-06-13 09:15:02 +0100

    deleted Traverser as it became Explorer.

--- AdventOfCode/2019/day18/Explorer.java
+++ AdventOfCode/2019/day18/Explorer.java
@@ -1,6 +1,15 @@
 /*
- * Go for all keys in sane direction until we hit a door that
- * we can't pass then change direction.
+ * We need to get all of the keys. We only need to open doors if they prevent
+ * us from getting to a key.
+ * 
+ * - Order the keys and their locations first in the tunnels?
+ * - The order of keys is always a to z? Not necessarily.
+ * - If we get to a door and we don't have the key to get through it then backtrack
+ *   and find the key? Or don't even go there in the first place?
+ * - If another key is closer to our current location than going through the
+ *   next door then collect the key for shortest path.
+ * - Go for all keys in sae direction until we hit a door that
+ *   we can't pass then change direction.
  *
  * BFS from current location to next key.
  */
