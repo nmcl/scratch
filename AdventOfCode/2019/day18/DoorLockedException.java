afb4071a6f86bbf117ff14f0073d46de85dc1899
Mark Little <mlittle@redhat.com>
2021-06-21 14:02:54 +0100

    Create DoorLockedException.java

---
+++ AdventOfCode/2019/day18/DoorLockedException.java
@@ -0,0 +1,11 @@
+public class DoorLockedException extends Exception
+{
+    public DoorLockedException ()
+    {
+    }
+    
+    public DoorLockedException (String message)
+    {
+        super(message);
+    }
+}
