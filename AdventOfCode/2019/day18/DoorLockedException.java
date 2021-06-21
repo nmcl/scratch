bff17c9ba2910e62aad96ae46d29a221203486f8
Mark Little <mlittle@redhat.com>
2021-06-21 14:50:00 +0100

    Update DoorLockedException.java

--- AdventOfCode/2019/day18/DoorLockedException.java
+++ AdventOfCode/2019/day18/DoorLockedException.java
@@ -3,9 +3,18 @@ public class DoorLockedException extends Exception
     public DoorLockedException ()
     {
     }
-    
-    public DoorLockedException (String message)
+
+    public DoorLockedException (String message, char door)
     {
         super(message);
+
+        _door = door;
     }
+
+    public char keyNeeded ()
+    {
+        return (char) (_door + CellId.DOOR_CODE);
+    }
+
+    private char _door;
 }
