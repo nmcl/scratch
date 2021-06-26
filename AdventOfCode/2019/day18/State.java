c0e87f189ed1cadca9081f76a06e5758b9fddcd5
Mark Little <mlittle@redhat.com>
2021-06-26 15:02:51 +0100

    Update State.java

--- AdventOfCode/2019/day18/State.java
+++ AdventOfCode/2019/day18/State.java
@@ -5,13 +5,14 @@ public class State
         _coord = coord;
         _keys = keys;
         _steps = steps;
+        _id = Util.keycode(keys);
     }
 
     public Coordinate getPosition ()
     {
         return _coord;
     }
-    
+
     public int numberOfKeys ()
     {
         return _keys.size();
@@ -22,7 +23,25 @@ public class State
         return _steps;
     }
 
+    public boolean hasKey (char door)
+    {
+        char key = Character.toLowerCase(door);
+
+        return _keys.contains(door);
+    }
+
+    public Set<Character> getKeys ()
+    {
+        return _keys;
+    }
+
+    public String getIdentifier ()
+    {
+        return _id;
+    }
+
     private Coordinate _coord;
     private Set<Character> _keys;
     private int _steps;
+    private String _id;
 }
