eb25786e40756cd7ff03c8b76e63c5a8e4fccb23
Mark Little <mlittle@redhat.com>
2021-06-26 14:28:51 +0100

    Update State.java

--- AdventOfCode/2019/day18/State.java
+++ AdventOfCode/2019/day18/State.java
@@ -7,6 +7,21 @@ public class State
         _steps = steps;
     }
 
+    public Coordinate getPosition ()
+    {
+        return _coord;
+    }
+    
+    public int numberOfKeys ()
+    {
+        return _keys.size();
+    }
+
+    public int numberOfSteps ()
+    {
+        return _steps;
+    }
+
     private Coordinate _coord;
     private Set<Character> _keys;
     private int _steps;
