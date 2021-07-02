0d85f4eb5beec74730353272c5fef78952a09890
Mark Little <mlittle@redhat.com>
2021-07-02 12:29:07 +0100

    Remove unneeded code.

--- AdventOfCode/2019/day18/CellId.java
+++ AdventOfCode/2019/day18/CellId.java
@@ -7,8 +7,6 @@ public class CellId
     public static final char ENTRANCE = '@';
     public static final char OPEN_PASSAGE = '.';
     public static final char STONE_WALL = '#';
-    
-    public static final int DOOR_CODE = 32; // a-A
 
     private CellId ()
     {
