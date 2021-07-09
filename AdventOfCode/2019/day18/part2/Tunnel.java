b995769e0acdafa57787fc87d267fa63fbe5727a
Mark Little <mlittle@redhat.com>
2021-07-09 14:02:17 +0100

    Update Tunnel.java

--- AdventOfCode/2019/day18/part2/Tunnel.java
+++ AdventOfCode/2019/day18/part2/Tunnel.java
@@ -1,25 +1,21 @@
 import java.util.*;
 
-public class Tunnel
-{
+public class Tunnel {
     public static final String TUNNEL_DATA = "tunnel.txt";
 
-    public static final void main (String[] args)
-    {
+    public static final void main(String[] args) {
         String dataFile = TUNNEL_DATA;
         boolean debug = false;
         boolean verify = false;
 
-        for (int i = 0; i < args.length; i++)
-        {
-            if ("-help".equals(args[i]))
-            {
+        for (int i = 0; i < args.length; i++) {
+            if ("-help".equals(args[i])) {
                 System.out.println("Usage: [-data <file>] [-verify] [-debug] [-help]");
                 System.exit(0);
             }
-            
+
             if ("-data".equals(args[i]))
-                dataFile = args[i+1];
+                dataFile = args[i + 1];
 
             if ("-debug".equals(args[i]))
                 debug = true;
@@ -28,31 +24,30 @@ public class Tunnel
                 verify = true;
         }
 
-        if (verify)
-        {
+        if (verify) {
             Verifier theVerifier = new Verifier(debug);
 
             if (theVerifier.verify())
                 System.out.println("Verified ok.");
             else
                 System.out.println("Verify failed.");
-        }
-        else
-        {
+
+            System.exit(0);
+        } else {
             Vector<String> input = Util.readMap(TUNNEL_DATA);
             Map theMap = new Map(input, debug);
 
             System.out.println(theMap);
-    
+
             Explorer exp = new Explorer(theMap, debug);
-    
-            System.out.println("Number of keys and doors: "+theMap.numberOfKeys()+", "+theMap.numberOfDoors());
-    
+
+            System.out.println("Number of keys and doors: " + theMap.numberOfKeys() + ", " + theMap.numberOfDoors());
+
             System.out.println("\nTraversing map ...");
-            
+
             int steps = exp.findAllKeys();
 
-            System.out.println("Verified. Number of steps to find all keys: "+steps);
+            System.out.println("Verified. Number of steps to find all keys: " + steps);
         }
     }
 }
