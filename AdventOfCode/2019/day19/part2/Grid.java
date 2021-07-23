e7d647abbf680f69f1ba7f00eef9918cdadb0b98
Mark Little <mlittle@redhat.com>
2021-07-23 12:14:20 +0100

    Update Grid.java

--- AdventOfCode/2019/day19/part2/Grid.java
+++ AdventOfCode/2019/day19/part2/Grid.java
@@ -36,6 +36,34 @@ public class Grid
         return new Coordinate(x, y);
     }
 
+    private Coordinate rightCoordinate (int y, int x)
+    {
+        Intcode computer = new Intcode(_input, _debug);
+
+        computer.setInputs(""+x, ""+y);
+        computer.executeProgram();
+
+        Vector<String> outputs = computer.getOutputs();
+        Enumeration<String> iter = outputs.elements();
+
+        while (iter.hasMoreElements())
+        {
+            String value = iter.nextElement();
+
+            if (DroneStatus.BEING_PULLED.equals(value))
+            {
+                x++;
+
+                computer = new Intcode(_input, _debug);
+
+                computer.setInputs(""+x, ""+y);
+                computer.executeProgram();
+            }
+        }
+
+        return new Coordinate(x -1, y);
+    }
+
     private Vector<String> _input;
     private boolean _debug;
 }
