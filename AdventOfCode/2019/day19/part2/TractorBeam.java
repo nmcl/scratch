3b479009fcfd906da6e4d7d3e388c29f69df0b84
Mark Little <mlittle@redhat.com>
2021-07-23 12:14:18 +0100

    Update TractorBeam.java

--- AdventOfCode/2019/day19/part2/TractorBeam.java
+++ AdventOfCode/2019/day19/part2/TractorBeam.java
@@ -23,32 +23,6 @@ public class TractorBeam
         }
 
         Vector<String> values = IntcodeUtil.readValues(INSTRUCTIONS);
-        int count = 0;
-
-	    for (int x = 0; x < SPACE_SIZE; x++)
-        {
-	        for (int y = 0; y < SPACE_SIZE; y++)
-            {
-                Intcode computer = new Intcode(values, debug);
-
-                computer.setInputs(""+y, ""+x);
-
-		        computer.executeProgram();
-
-                Vector<String> outputs = computer.getOutputs();
-                Enumeration<String> iter = outputs.elements();
-
-                while (iter.hasMoreElements())
-                {
-                    String value = iter.nextElement();
-
-                    if (DroneStatus.BEING_PULLED.equals(value))
-                        count++;
-                }
-		    }
-	    }
-
-        System.out.println("Points affected by tractor beam: "+count);
     }
 
     private TractorBeam ()
