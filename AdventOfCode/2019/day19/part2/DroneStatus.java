4bd1f085703767b6d43e429fb242f4e8c5a054d7
Mark Little <mlittle@redhat.com>
2021-07-23 11:41:52 +0100

    initial code.

---
+++ AdventOfCode/2019/day19/part2/DroneStatus.java
@@ -0,0 +1,9 @@
+public class DroneStatus
+{
+    public static final String STATIONARY = "0";
+    public static final String BEING_PULLED = "1";
+
+    private DroneStatus ()
+    {
+    }
+}
---
+++ AdventOfCode/2019/day19/part2/TractorBeam.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class TractorBeam
+{
+    public static final String INSTRUCTIONS = "instructions.txt";
+
+    public static final int SPACE_SIZE = 50;
+
+    public static final void main (String[] args)
+    {
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
+
+        Vector<String> values = IntcodeUtil.readValues(INSTRUCTIONS);
+        int count = 0;
+
+	    for (int x = 0; x < SPACE_SIZE; x++)
+        {
+	        for (int y = 0; y < SPACE_SIZE; y++)
+            {
+                Intcode computer = new Intcode(values, debug);
+
+                computer.setInputs(""+y, ""+x);
+
+		        computer.executeProgram();
+
+                Vector<String> outputs = computer.getOutputs();
+                Enumeration<String> iter = outputs.elements();
+
+                while (iter.hasMoreElements())
+                {
+                    String value = iter.nextElement();
+
+                    if (DroneStatus.BEING_PULLED.equals(value))
+                        count++;
+                }
+		    }
+	    }
+
+        System.out.println("Points affected by tractor beam: "+count);
+    }
+
+    private TractorBeam ()
+    {
+    }
+}
