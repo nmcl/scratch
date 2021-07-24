a592ee2f6efd3de25ae8b9af19d7bff5b3af89c4
Mark Little <mlittle@redhat.com>
2021-07-24 13:58:02 +0100

    Update TractorBeam.java

--- AdventOfCode/2019/day19/part2/TractorBeam.java
+++ AdventOfCode/2019/day19/part2/TractorBeam.java
@@ -23,6 +23,31 @@ public class TractorBeam
         }
 
         Vector<String> values = IntcodeUtil.readValues(INSTRUCTIONS);
+        Grid theGrid = new Grid(values, debug);
+	    int sizeOfGrid = theGrid.getBeamGrid().size();
+
+	    for (int i = 0; i < sizeOfGrid; i++)
+        {
+	       CoordinatePair row = theGrid.getBeamGrid().get(i);
+           Coordinate leftCoord = row.getLeft();
+           Coordinate rightCoord = row.getRight();
+
+	        if (i < sizeOfGrid - (SPACE_SIZE - 1))
+            {
+                for (int j = leftCoord.getX(); j <= rightCoord.getX() - (SPACE_SIZE - 1); j++)
+                {
+                    CoordinatePair nextRow = theGrid.getBeamGrid().get(i + (SPACE_SIZE - 1));
+
+                    if (j >= nextRow.getLeft().getX() && j <= nextRow.getRight().getX())
+                    {
+                        int xMult = j * 10000;
+
+                        System.out.println("Value obtained: "+(xMult + leftCoord.getY()));
+                        System.exit(0);
+		            }
+		        }
+	        }
+	    }
     }
 
     private TractorBeam ()
