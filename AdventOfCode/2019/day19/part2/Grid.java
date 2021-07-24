55ccedc44877ed42f7b70b5c965db03e570200ae
Mark Little <mlittle@redhat.com>
2021-07-24 13:57:59 +0100

    Update Grid.java

--- AdventOfCode/2019/day19/part2/Grid.java
+++ AdventOfCode/2019/day19/part2/Grid.java
@@ -5,65 +5,91 @@ public class Grid
     public Grid (Vector<String> instructions, boolean debug)
     {
         _input = instructions;
+        _theGrid = new Vector<CoordinatePair>();
         _debug = debug;
+
+        findSquare();
+    }
+
+    public final Vector<CoordinatePair> getBeamGrid ()
+    {
+        return _theGrid;
+    }
+
+    private final void findSquare ()
+    {
+        int startY = 10;
+        int startX = 0;
+
+        Coordinate leftCoord = leftCoordinate(startY, startX);
+        Coordinate rightCoord = rightCoordinate(leftCoord.getY(), leftCoord.getX());
+
+        for (int i = startY + 1; i < 3000; i++)
+        {
+            leftCoord = leftCoordinate(i, leftCoord.getX());
+            rightCoord = rightCoordinate(i, rightCoord.getX());
+
+            System.out.println("adding "+leftCoord+" "+rightCoord);
+
+            _theGrid.add(new CoordinatePair(leftCoord, rightCoord));
+        }
     }
 
     private Coordinate leftCoordinate (int y, int x)
     {
         Intcode computer = new Intcode(_input, _debug);
 
+        System.out.println("leftPointer "+x+" "+y);
+
         computer.setInputs(""+x, ""+y);
         computer.executeProgram();
 
         Vector<String> outputs = computer.getOutputs();
         Enumeration<String> iter = outputs.elements();
+        boolean beingPulled = false;
 
-        while (iter.hasMoreElements())
+        while (iter.hasMoreElements() && !beingPulled)
         {
             String value = iter.nextElement();
 
-            if (DroneStatus.STATIONARY.equals(value))
-            {
-                x++;
-
-                computer = new Intcode(_input, _debug);
-
-                computer.setInputs(""+x, ""+y);
-                computer.executeProgram();
-            }
+            if (DroneStatus.BEING_PULLED.equals(value))
+                beingPulled = true;
         }
 
-        return new Coordinate(x, y);
+        if (!beingPulled)  // keep going until we start being pulled
+            return leftCoordinate(y, x +1);
+        else
+            return new Coordinate(x, y);
     }
 
     private Coordinate rightCoordinate (int y, int x)
     {
         Intcode computer = new Intcode(_input, _debug);
 
+        System.out.println("rightPointer "+x+" "+y);
+
         computer.setInputs(""+x, ""+y);
         computer.executeProgram();
 
         Vector<String> outputs = computer.getOutputs();
         Enumeration<String> iter = outputs.elements();
+        boolean beingPulled = true;
 
-        while (iter.hasMoreElements())
+        while (iter.hasMoreElements() && beingPulled)
         {
             String value = iter.nextElement();
 
-            if (DroneStatus.BEING_PULLED.equals(value))
-            {
-                x++;
-
-                computer = new Intcode(_input, _debug);
-
-                computer.setInputs(""+x, ""+y);
-                computer.executeProgram();
-            }
+            if (DroneStatus.STATIONARY.equals(value))
+                beingPulled = false;
         }
 
-        return new Coordinate(x -1, y);
+        if (beingPulled)  // keep going until we stop being pulled
+            return rightCoordinate(y, x +1);
+        else
+            return new Coordinate(x -1, y);
     }
 
     private Vector<String> _input;
+    private Vector<CoordinatePair> _theGrid;
     private boolean _debug;
 }
