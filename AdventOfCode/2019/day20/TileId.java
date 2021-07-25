1b8cef016ba5dc376f0417658bfde6d7ab4284cb
Mark Little <mlittle@redhat.com>
2021-07-25 16:46:55 +0100

    initial Maze code.

---
+++ AdventOfCode/2019/day20/TileId.java
@@ -0,0 +1,13 @@
+public class TileId
+{
+    public static final String WALL = "#";
+    public static final String UNEXPLORED = " ";
+    public static final String TRAVERSE = ".";
+    public static final String DROID = "D";
+    public static final String OXYGEN_STATION = "O";
+    public static final String STARTING_POINT = "S";
+
+    private TileId ()  // prevent instantiation!
+    {
+    }
+}
