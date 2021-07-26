800eb80230f4e3895d3a89a786ac5a9d6bfed63d
Mark Little <mlittle@redhat.com>
2021-07-26 13:48:32 +0100

    Update TileId.java

--- AdventOfCode/2019/day20/TileId.java
+++ AdventOfCode/2019/day20/TileId.java
@@ -3,7 +3,7 @@ public class TileId
     public static final char WALL = '#';
     public static final char SPACE = ' ';
     public static final char PASSAGE = '.';
-    public static final char DOOR = '%';
+    public static final char PORTAL = '%';
 
     private TileId ()  // prevent instantiation!
     {
