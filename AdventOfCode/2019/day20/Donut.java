be3d3f7d0326a54c113ee5eed74a82ea38fe8b77
Mark Little <mlittle@redhat.com>
2021-07-26 12:13:12 +0100

    Create Donut.java

---
+++ AdventOfCode/2019/day20/Donut.java
@@ -0,0 +1,25 @@
+public class Donut
+{
+    public static final String DATA = "maze.txt";
+
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
