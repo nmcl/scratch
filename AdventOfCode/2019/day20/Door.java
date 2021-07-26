c54a5382ae16902d123388fb19162a3f227595af
Mark Little <mlittle@redhat.com>
2021-07-26 13:38:11 +0100

    Update Door.java

--- AdventOfCode/2019/day20/Door.java
+++ AdventOfCode/2019/day20/Door.java
@@ -1,10 +1,12 @@
-public class Door
+public class Door extends Tile
 {
     public static final String START = "AA";
     public static final String EXIT = "ZZ";
-    
+
     public Door (Coordinate position, boolean horizontalRepresentation)
     {
+        super(position, TileId.DOOR);
+        
         _location = position;
         _horizontal = horizontalRepresentation;
     }
