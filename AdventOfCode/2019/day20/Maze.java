56aff11ce31b61cb717da1be04bd7c5a7065ac96
Mark Little <mlittle@redhat.com>
2021-07-26 12:13:14 +0100

    Update Maze.java

--- AdventOfCode/2019/day20/Maze.java
+++ AdventOfCode/2019/day20/Maze.java
@@ -1,9 +1,68 @@
 import java.util.*;
+import java.io.*;
 
 public class Maze
 {
-    public Maze ()
+    public Maze (String data)
     {
-        
+        if (!loadData(data))
+            System.out.println("Error in loading data file: "+data);
     }
+
+    private final boolean loadData (String file)
+    {
+        BufferedReader reader = null;
+        boolean valid = true;
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(file));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                char[] asChar = line.toCharArray();  // all lines are the same length
+                
+                if (_width == 0)
+                    _width = asChar.length;
+
+                for (int i = 0; i < _width; i++)
+                {
+                    switch (asChar[i])
+                    {
+                        case TileId.WALL:
+                        case TileId.SPACE:
+                        case TileId.PASSAGE:
+                            _theMaze.add(new Tile(new Coordinate(i, _height), asChar[i]));
+                        default:
+                            _theMaze.add(new Tile(new Coordinate(i, _height), TileId.SPACE));
+                    }
+                }
+
+                _height++;
+            }
+        }
+        catch (Throwable ex)
+        {
+            valid = false;
+
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return valid;
+    }
+
+    private Vector<Tile> _theMaze;
+    private int _height = 0;
+    private int _width = 0;
 }
