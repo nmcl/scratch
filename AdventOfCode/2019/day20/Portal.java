843c846ab83c6eae338b54b1cd50396e1d393003
Mark Little <mlittle@redhat.com>
2021-08-01 12:57:34 +0100

    Update Portal.java

--- AdventOfCode/2019/day20/Portal.java
+++ AdventOfCode/2019/day20/Portal.java
@@ -30,6 +30,29 @@ public class Portal extends Tile
     {
         return _portalName;
     }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (this == obj)
+            return true;
+        
+        // only check location not content
+
+        if (getClass() == obj.getClass())
+        {
+            Portal temp = (Portal) obj;
+
+            System.out.println("comparing "+_position+" and "+temp._position);
+
+            return _position.equals(temp._position);  // only compare position not type.
+        }
+
+        return false;
+    }
     
     private char _portalId;
     private String _portalName;
