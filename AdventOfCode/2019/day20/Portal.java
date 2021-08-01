41815ef9cdf96858d5af39d030d67b946a25c842
Mark Little <mlittle@redhat.com>
2021-08-01 11:29:05 +0100

    Update Portal.java

--- AdventOfCode/2019/day20/Portal.java
+++ AdventOfCode/2019/day20/Portal.java
@@ -1,6 +1,11 @@
 /*
  * A Portal is two letters but as we scan in the information about
  * the Donut and Portals we only get to read one letter at a time.
+ * 
+ * Assume there are always 3 spaces around the first letter in the Portal
+ * name. Assume there are always 2 spaces around the second letter, then
+ * the first letter and a passage which represents the real location of the
+ * Portal.
  */
 
 public class Portal extends Tile
