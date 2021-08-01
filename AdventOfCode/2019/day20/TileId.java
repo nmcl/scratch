f7b61fe44acea693cbd1aeaac464a5bca8882b25
Mark Little <mlittle@redhat.com>
2021-08-01 15:09:08 +0100

    Update TileId.java

--- AdventOfCode/2019/day20/TileId.java
+++ AdventOfCode/2019/day20/TileId.java
@@ -5,6 +5,28 @@ public class TileId
     public static final char PASSAGE = '.';
     public static final char PORTAL = '%';
 
+    public static final int UP = 0;
+    public static final int DOWN = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
+
+    public static String position (int i)
+    {
+        switch (i)
+        {
+            case UP:
+                return "UP";
+            case DOWN:
+                return "DOWN";
+            case LEFT:
+                return "LEFT";
+            case RIGHT:
+                return "RIGHT";
+            default:
+                return "UNKNOWN";
+        }
+    }
+    
     private TileId ()  // prevent instantiation!
     {
     }
