708dd8f23aadcc58ee3138a2ecd890446d360968
Mark Little <mlittle@redhat.com>
2021-08-01 11:25:31 +0100

    Update Portal.java

--- AdventOfCode/2019/day20/Portal.java
+++ AdventOfCode/2019/day20/Portal.java
@@ -1,3 +1,8 @@
+/*
+ * A Portal is two letters but as we scan in the information about
+ * the Donut and Portals we only get to read one letter at a time.
+ */
+
 public class Portal extends Tile
 {
     public static final String START = "AA";
@@ -8,12 +13,19 @@ public class Portal extends Tile
         super(position, TileId.PORTAL);
 
         _portalId = portalId;
+        _portalName = "*"+portalId+"*";  // a default name initially.
     }
 
     public final char getId ()
     {
         return _portalId;
     }
+
+    public final String getName ()
+    {
+        return _portalName;
+    }
     
     private char _portalId;
+    private String _portalName;
 }
