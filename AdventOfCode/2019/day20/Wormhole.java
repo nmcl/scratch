268f398a10505a0f1c9972a1a86602b8480b7ee2
Mark Little <mlittle@redhat.com>
2021-08-07 14:22:22 +0100

    Create Wormhole.java

---
+++ AdventOfCode/2019/day20/Wormhole.java
@@ -0,0 +1,11 @@
+public class Wormhole
+{
+    public Wormhole (String name, Coordinate location)
+    {
+        _name = name;
+        _position = location;
+    }
+
+    private String _name;
+    private Coordinate _position;
+}
