b6ae4c56878911f45b482289851ef6330c1e5e50
Mark Little <mlittle@redhat.com>
2021-08-13 09:30:01 +0100

    Update Journey.java

---
+++ AdventOfCode/2019/day20/Journey.java
@@ -0,0 +1,11 @@
+public class Journey
+{
+    public Journey (Coordinate coord, int steps)
+    {
+        _coord = coord;
+        _steps = steps;
+    }
+
+    private Coordinate _coord;
+    private int _steps;
+}
