a29bca86ab5c1ce11b210ac0eefa602b65c0c33b
Mark Little <mlittle@redhat.com>
2021-08-13 11:23:05 +0100

    Update Traveller.java

--- AdventOfCode/2019/day20/Traveller.java
+++ AdventOfCode/2019/day20/Traveller.java
@@ -18,6 +18,33 @@ public class Traveller
         return -1;
     }
 
+    private HashMap<Coordinate, List<Route>> findRoutes (Vector<Wormhole> outerWormholes, Vector<Wormhole> innerWormholes)
+    {
+        HashMap<Coordinate, List<Route>> routesForEachCoordinate = new HashMap<Coordinate, List<Route>>();
+        Set<Wormhole> outerSet = new HashSet<Wormhole>(outerWormholes);
+        Iterator<Wormhole> iter = outerSet.iterator();
+
+        while (iter.hasNext())
+        {
+            Wormhole toCheck = iter.next();
+            int index = innerWormholes.indexOf(toCheck);
+
+            if (index != -1)
+            {
+                Coordinate innerLocation = innerWormholes.elementAt(index).getLocation();
+                List<Route> outerRoutes = routesForEachCoordinate.computeIfAbsent(toCheck.getLocation(), (k) -> new ArrayList<>());
+
+                outerRoutes.add(new Route(toCheck.getLocation(), innerLocation));
+
+                List<Route> innerRoutes = routesForEachCoordinate.computeIfAbsent(innerLocation, (k) -> new ArrayList<>());
+
+                innerRoutes.add(new Route(innerLocation, toCheck.getLocation()));
+            }
+        }
+
+        return routesForEachCoordinate;
+    }
+
     private Maze _theMaze;
     private boolean _debug;
 }
