9b9c153b4b4d32fdb5265be41498cdca31ac6f7c
Mark Little <mlittle@redhat.com>
2021-08-17 16:35:28 +0100

    Update Verifier.java

--- AdventOfCode/2019/day20/Verifier.java
+++ AdventOfCode/2019/day20/Verifier.java
@@ -24,9 +24,29 @@ public class Verifier
 
         int numberOfSteps = theTraveller.findAllKeys();
 
-        System.out.println("got "+numberOfSteps);
+        if (numberOfSteps == EXAMPLE_1_STEPS)
+        {
+            theMaze = new Maze(EXAMPLE_2, _debug);
+            theTraveller = new Traveller(theMaze, _debug);
 
-        return true;
+            if (_debug)
+            {
+                System.out.println(theMaze);
+
+                System.out.println(theMaze.printWithPortals());
+            }
+    
+            numberOfSteps = theTraveller.findAllKeys();
+    
+            if (numberOfSteps == EXAMPLE_2_STEPS)
+                return true;
+            else
+                System.out.println("Failed to verify "+EXAMPLE_2);
+        }
+        else
+            System.out.println("Failed to verify "+EXAMPLE_1);
+
+        return false;
     }
 
     private boolean _debug;
