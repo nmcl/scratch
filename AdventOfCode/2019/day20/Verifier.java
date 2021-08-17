0bba40371d44b161c78f678bfd1884d4f26611e5
Mark Little <mlittle@redhat.com>
2021-08-17 12:30:49 +0100

    Update Verifier.java

--- AdventOfCode/2019/day20/Verifier.java
+++ AdventOfCode/2019/day20/Verifier.java
@@ -1,7 +1,7 @@
 public class Verifier
 {
     public static final String EXAMPLE_1 = "example1.txt";
-    public static final int EXAMPLE_1_STEPS = 23;
+    public static final int EXAMPLE_1_STEPS = 26;  // problem statement doesn't ask for shortest path
     public static final String EXAMPLE_2 = "example2.txt";
     public static final int EXAMPLE_2_STEPS = 58;
 
