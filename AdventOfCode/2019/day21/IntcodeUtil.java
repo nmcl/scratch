75c130b329e5bd8d4aa797e5b77d7b0356e2d36c
Mark Little <mlittle@redhat.com>
2021-08-19 11:06:26 +0100

    Moving IntCode into the dir.

---
+++ AdventOfCode/2019/day21/IntcodeUtil.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class IntcodeUtil
+{
+    public static final Vector<String> readValues (String inputFile)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        String[] values = null;
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                values = line.split(Intcode.DELIMITER);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        Vector<String> instructions = new Vector<String>();
+
+        instructions.addAll(Arrays.asList(values));
+
+        return instructions;
+    }
+
+    private IntcodeUtil ()
+    {
+    }
+}
