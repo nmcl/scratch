a58c5a1f52c2dc3caf578ff17466f40ffefd534a
Mark Little <mlittle@redhat.com>
2021-08-19 20:39:54 +0100

    Update SpringDroid.java

--- AdventOfCode/2019/day21/SpringDroid.java
+++ AdventOfCode/2019/day21/SpringDroid.java
@@ -4,6 +4,17 @@ public class SpringDroid
 {
     public static final int MAX_INSTRUCTIONS = 15;
 
+    /*
+     * Jump if there is a hole on A or on B or on C and there is no hole on D.
+     *
+     * 'OR A J', //  J = A
+     * 'AND B J', // J = A AND B
+     * 'AND C J', // J = A AND B AND C
+     * 'NOT T J', // J = !A OR !B OR !C
+     * 'AND D J', // J = (!A OR !B OR !C) AND D
+     * 'WALK'
+     */
+
     public SpringDroid (Vector<String> instructions, boolean debug)
     {
         _debug = debug;
