0c18c9d0d0e6961bacd31462e9c33952b76931d9
Mark Little <mlittle@redhat.com>
2021-08-19 20:16:18 +0100

    Update SpringDroid.java

--- AdventOfCode/2019/day21/SpringDroid.java
+++ AdventOfCode/2019/day21/SpringDroid.java
@@ -1,11 +1,14 @@
+import java.util.*;
+
 public class SpringDroid
 {
     public static final int MAX_INSTRUCTIONS = 15;
-    
-    public SpringDroid (boolean debug)
+
+    public SpringDroid (Vector<String> instructions, boolean debug)
     {
         _debug = debug;
     }
 
+    private Intcode _computer;
     private boolean _debug;
 }
