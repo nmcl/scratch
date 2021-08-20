b67ec57e173938b08d4a47e007615ff718de5892
Mark Little <mlittle@redhat.com>
2021-08-20 15:46:43 +0100

    started Verifier.

---
+++ AdventOfCode/2019/day22/Verifier.java
@@ -0,0 +1,11 @@
+public class Verifier
+{
+    public static final int[] EXAMPLE_1_RESULT = {0, 3, 6, 9, 2, 5, 8, 1, 4, 7};
+    public static final int[] EXAMPLE_2_RESULT = {3, 0, 7, 4, 1, 8, 5, 2, 9, 6};
+    public static final int[] EXAMPLE_3_RESULT = {6, 3, 0, 7, 4, 1, 8, 5, 2, 9};
+
+    public Verifier ()
+    {
+
+    }
+}
