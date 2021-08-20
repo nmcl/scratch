a69f82483644489078b9f3064c917159d140f580
Mark Little <mlittle@redhat.com>
2021-08-20 15:53:51 +0100

    Create SlamShuffle.java

---
+++ AdventOfCode/2019/day22/SlamShuffle.java
@@ -0,0 +1,20 @@
+public class SlamShuffle
+{
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
+    }
+}
