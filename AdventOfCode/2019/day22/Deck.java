81df4bb42e15c6c8b697bb351d63c1ee1a6179f0
Mark Little <mlittle@redhat.com>
2021-08-20 16:05:02 +0100

    Update Deck.java

--- AdventOfCode/2019/day22/Deck.java
+++ AdventOfCode/2019/day22/Deck.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 public class Deck
 {
     public static final int SIZE_OF_DECK = 10007;
 
     public Deck (boolean debug)
     {
+        _theDeck = new Vector<Integer>(SIZE_OF_DECK);
         _debug = debug;
     }
 
+    private Vector<Integer> _theDeck;
     private boolean _debug;
 }
