aef96715e497e7a0385a9a4266a99a509637db2e
Mark Little <mlittle@redhat.com>
2021-08-20 16:21:51 +0100

    Update Deck.java

--- AdventOfCode/2019/day22/Deck.java
+++ AdventOfCode/2019/day22/Deck.java
@@ -6,8 +6,22 @@ public class Deck
 
     public Deck (boolean debug)
     {
+        this(debug, false);
+    }
+
+    public Deck (boolean debug, boolean populateDeck)
+    {
         _theDeck = new Vector<Integer>(SIZE_OF_DECK);
         _debug = debug;
+
+        if (populateDeck)
+            initialise();
+    }
+
+    private void initialise ()
+    {
+        for (int i = 0; i < SIZE_OF_DECK; i++)
+            _theDeck.add(i, i);
     }
 
     private Vector<Integer> _theDeck;
