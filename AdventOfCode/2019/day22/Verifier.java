93ac467eca47c91e822d1c2b3708fc95eaad23aa
Mark Little <mlittle@redhat.com>
2021-08-20 15:56:19 +0100

    Update Verifier.java

--- AdventOfCode/2019/day22/Verifier.java
+++ AdventOfCode/2019/day22/Verifier.java
@@ -1,7 +1,10 @@
 public class Verifier
 {
+    public static final String EXAMPLE_1 = "example1.txt";
     public static final int[] EXAMPLE_1_RESULT = {0, 3, 6, 9, 2, 5, 8, 1, 4, 7};
+    public static final String EXAMPLE_2 = "example2.txt";
     public static final int[] EXAMPLE_2_RESULT = {3, 0, 7, 4, 1, 8, 5, 2, 9, 6};
+    public static final String EXAMPLE_3 = "example3.txt";
     public static final int[] EXAMPLE_3_RESULT = {6, 3, 0, 7, 4, 1, 8, 5, 2, 9};
 
     public Verifier ()
