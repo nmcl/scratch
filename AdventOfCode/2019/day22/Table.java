f796a515071189f4d2a4dbe8d1399cbc772dcfcd
Mark Little <mlittle@redhat.com>
2021-08-21 18:14:15 +0100

    Create Table.java

---
+++ AdventOfCode/2019/day22/Table.java
@@ -0,0 +1,13 @@
+/*
+ * Represents the table onto which we can deal cards.
+ */
+
+public class Table
+{
+    public Table (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private boolean _debug;
+}
