6335656778d6cdd8013f03affbe9f3b5df99f0c8
Mark Little <mlittle@redhat.com>
2021-08-22 14:41:51 +0100

    Update Commands.java

--- AdventOfCode/2019/day22/Commands.java
+++ AdventOfCode/2019/day22/Commands.java
@@ -3,4 +3,8 @@ public class Commands
     public static final String DEAL_WITH_INCREMENT = "deal with increment";
     public static final String DEAL_INTO = "deal into new stack";
     public static final String CUT = "cut";
+
+    private Commands ()
+    {
+    }
 }
