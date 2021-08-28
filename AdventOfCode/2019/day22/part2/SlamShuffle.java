675ffb1702c79963849cceb54699dc0e2a5f35f4
Mark Little <mlittle@redhat.com>
2021-08-28 14:35:58 +0100

    Update SlamShuffle.java

--- AdventOfCode/2019/day22/part2/SlamShuffle.java
+++ AdventOfCode/2019/day22/part2/SlamShuffle.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 import java.math.*;
 
 public class SlamShuffle
@@ -5,7 +7,7 @@ public class SlamShuffle
     public static final String DATA_FILE = "data.txt";
     public static final BigInteger SIZE_OF_DECK = BigInteger.valueOf(119315717514047L);;
     public static final BigInteger SHUFFLE_TIMES = BigInteger.valueOf(101741582076661L);
-    public static final int CARD = 2020;
+    public static final BigInteger CARD_POSITION = BigInteger.valueOf(2020);
     
     // https://codeforces.com/blog/entry/72593
 
@@ -24,9 +26,11 @@ public class SlamShuffle
 
             if ("-debug".equals(args[i]))
                 debug = true;
-
-            if ("-verify".equals(args[i]))
-                verify = true;
         }
+
+        Dealer theDealer = new Dealer(DATA_FILE, debug);
+        BigInteger result = theDealer.dealCards(SIZE_OF_DECK, SHUFFLE_TIMES, CARD_POSITION);
+
+        System.out.println("Card at position "+CARD_POSITION+" is "+result);
     }
 }
