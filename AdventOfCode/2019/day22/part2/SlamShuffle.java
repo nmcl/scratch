9603e7453a3a0e61bb13e54e730c551b5304cc82
Mark Little <mlittle@redhat.com>
2021-08-28 14:01:55 +0100

    Update SlamShuffle.java

--- AdventOfCode/2019/day22/part2/SlamShuffle.java
+++ AdventOfCode/2019/day22/part2/SlamShuffle.java
@@ -12,13 +12,12 @@ public class SlamShuffle
     public static void main (String[] args)
     {
         boolean debug = false;
-        boolean verify = false;
 
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-verify] [-debug] [-help]");
+                System.out.println("Usage: [-debug] [-help]");
 
                 System.exit(0);
             }
@@ -29,23 +28,5 @@ public class SlamShuffle
             if ("-verify".equals(args[i]))
                 verify = true;
         }
-
-        if (verify)
-        {
-            Verifier theVerifier = new Verifier(debug);
-
-            if (theVerifier.verify())
-                System.out.println("\nVerified ok!");
-            else
-                System.out.println("\nVerify failed!");
-
-            System.exit(0);
-        }
-
-        Dealer theDealer = new Dealer(DATA_FILE, debug);
-        Deck theDeck = theDealer.dealCards(SIZE_OF_DECK);
-        int position = theDeck.positionOfCard(10);
-
-        System.out.println("Position of card "+10+" is "+position);
     }
 }
