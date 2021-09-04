d85fc7235e2ef22b97cd1fdc6925cb4a68df55bf
Mark Little <mlittle@redhat.com>
2021-09-04 12:53:07 +0100

    Update Packet.java

--- AdventOfCode/2019/day23/Packet.java
+++ AdventOfCode/2019/day23/Packet.java
@@ -1,30 +1,45 @@
+import java.util.*;
+
 public class Packet
 {
-    public static final int EMPTY_PACKET = -1;
-    
-    public Packet (int address, long x, long y)
+    public static final String EMPTY_PACKET = "-1";
+
+    public Packet (LinkedList<String> outputs)
+    {
+        _address = Long.valueOf(outputs.poll());
+        _x = Long.valueOf(outputs.poll());
+        _y = Long.valueOf(outputs.poll());
+    }
+
+    public Packet (long address, long x, long y)
     {
         _address = address;
         _x = x;
         _y = y;
     }
 
-    public final int getAddress ()
+    public final long getAddress ()
     {
         return _address;
     }
 
-    private final long getX ()
+    public final long getX ()
     {
         return _x;
     }
 
-    private final long getY ()
+    public final long getY ()
     {
         return _y;
     }
 
-    private int _address;
+    @Override
+    public String toString ()
+    {
+        return "< "+_address+", "+_x+", "+_y+" >";
+    }
+
+    private long _address;
     private long _x;
     private long _y;
 }
