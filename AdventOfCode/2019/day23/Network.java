4cc1f522a65e09a0a3952b9f3f808e62103e51fe
Mark Little <mlittle@redhat.com>
2021-09-04 12:53:05 +0100

    Update Network.java

--- AdventOfCode/2019/day23/Network.java
+++ AdventOfCode/2019/day23/Network.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // network of machines.
 
 public class Network
@@ -10,7 +12,46 @@ public class Network
         _debug = debug;
 
         for (int i = 0; i < NETWORK_SIZE; i++)
-            _theNetwork[i] = new Intcode(IntcodeUtil.readValues(fileName), debug);
+            _theNetwork[i] = new Intcode(IntcodeUtil.readValues(fileName), ""+i, debug);
+    }
+
+    public final Packet getFirstPacket (int destination)
+    {
+        Packet toReturn = null;
+
+        do
+        {
+            for (int i = 0; (i < NETWORK_SIZE) && (toReturn == null); i++)
+            {
+                _theNetwork[i].executeUntilInput();
+
+                if (_theNetwork[i].waitingForInput())
+                {
+                    _theNetwork[i].setInput(Packet.EMPTY_PACKET);
+                    _theNetwork[i].executeUntilInput();
+                }
+            
+                LinkedList<String> outputs = new LinkedList<String>(_theNetwork[i].getOutputs());
+
+                while (!outputs.isEmpty())
+                {
+                    Packet thePacket = new Packet(outputs);
+                
+                    if (_debug)
+                        System.out.println("Packet received from computer "+i+" is "+thePacket);
+
+                    if (thePacket.getAddress() == destination)
+                        toReturn = thePacket;
+                    else
+                    {
+                        if (thePacket.getAddress() < NETWORK_SIZE)
+                            _theNetwork[(int) thePacket.getAddress()].setInputs(""+thePacket.getX(), ""+thePacket.getY());
+                    }
+                }
+            }
+        } while (toReturn == null);
+
+        return toReturn;
     }
 
     private Intcode[] _theNetwork;
