8a6f0d145fff728aae34240ccdd3387314e0647c
Mark Little <mlittle@redhat.com>
2021-09-04 11:42:37 +0100

    Update NIC.java

--- AdventOfCode/2019/day23/NIC.java
+++ AdventOfCode/2019/day23/NIC.java
@@ -1,9 +1,20 @@
-// interface controller
-
 public class NIC
 {
-    public NIC ()
+    public static void main (String[] args)
     {
+        boolean debug = false;
 
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
     }
 }
