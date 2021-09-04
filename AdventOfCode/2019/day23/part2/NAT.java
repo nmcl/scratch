efee0320de9d8462b8bc6a0c5471dabbfc1a6ee4
Mark Little <mlittle@redhat.com>
2021-09-04 13:19:27 +0100

    Update NAT.java

---
+++ AdventOfCode/2019/day23/part2/NAT.java
@@ -0,0 +1,21 @@
+public class NAT
+{
+    public static final long ADDRESS = 255;
+
+    public NAT ()
+    {
+        _packet = null;
+    }
+
+    public final void setPacket (Packet p)
+    {
+        _packet = p
+    }
+
+    public final Packet getPacket ()
+    {
+        return _packet;
+    }
+
+    private Packet _packet;
+}
