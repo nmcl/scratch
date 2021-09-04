68deb015551b7acb64f15ebc51d593ee7eda859f
Mark Little <mlittle@redhat.com>
2021-09-04 11:42:35 +0100

    Update Network.java

--- AdventOfCode/2019/day23/Network.java
+++ AdventOfCode/2019/day23/Network.java
@@ -2,8 +2,12 @@
 
 public class Network
 {
-    public Network ()
+    public static final int NETWORK_SIZE = 50;
+
+    public Network (boolean debug)
     {
         
     }
+
+    private Intcode[] _theNetwork;
 }
