968c99cec97ce12c9d6c9604b16b9607587f9e12
Mark Little <mlittle@redhat.com>
2021-09-04 11:47:47 +0100

    Update Packet.java

--- AdventOfCode/2019/day23/Packet.java
+++ AdventOfCode/2019/day23/Packet.java
@@ -1,7 +1,28 @@
 public class Packet
 {
-    public Packet ()
+    public Packet (int address, long x, long y)
     {
-        
+        _address = address;
+        _x = x;
+        _y = y;
     }
+
+    public final int getAddress ()
+    {
+        return _address;
+    }
+
+    private final long getX ()
+    {
+        return _x;
+    }
+
+    private final long getY ()
+    {
+        return _y;
+    }
+
+    private int _address;
+    private long _x;
+    private long _y;
 }
