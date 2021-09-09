7e0c82481dd9ad71ffb0ac7d00eebe66e878b79d
Mark Little <mlittle@redhat.com>
2021-09-09 12:02:09 +0100

    Update Eris.java

--- AdventOfCode/2019/day24/Eris.java
+++ AdventOfCode/2019/day24/Eris.java
@@ -2,6 +2,23 @@ public class Eris
 {
     public static void main (String[] args)
     {
-        
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-debug] [-help");
+
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
     }
 }
