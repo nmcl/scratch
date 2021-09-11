2249a2d3bfdfc1643621289ed694deece7e77f6f
Mark Little <mlittle@redhat.com>
2021-09-11 11:55:14 +0100

    Update Grid.java

--- AdventOfCode/2019/day24/Grid.java
+++ AdventOfCode/2019/day24/Grid.java
@@ -3,17 +3,25 @@ public class Grid
     public static final int DEFAULT_WIDTH = 5;
     public static final int DEFAULT_HEIGHT = 5;
 
-    public Grid ()
+    public Grid (String fileName)
     {
-        this(DEFAULT_HEIGHT, DEFAULT_WIDTH);
+        this(DEFAULT_HEIGHT, DEFAULT_WIDTH, fileName);
     }
 
-    public Grid (int height, int width)
+    public Grid (int height, int width, String fileName)
     {
         _height = height;
         _width = width;
+
     }
 
+    private boolean loadWorld (String fileName)
+    {
+        boolean loaded = false;
+
+        return loaded;
+    }
+    
     private Tile[][] _theWorld;
     private int _height;
     private int _width;
