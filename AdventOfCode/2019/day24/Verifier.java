a5d1a054f9f0e46c00f19d9bca4f35d616c1b102
Mark Little <mlittle@redhat.com>
2021-09-11 13:37:47 +0100

    Update Verifier.java

--- AdventOfCode/2019/day24/Verifier.java
+++ AdventOfCode/2019/day24/Verifier.java
@@ -1,6 +1,7 @@
 public class Verifier
 {
     public static final String EXAMPLE = "example.txt";
+    public static final String MINUTE_BASE = "minute";
 
     public Verifier (boolean debug)
     {
@@ -10,19 +11,24 @@ public class Verifier
     public final boolean verify ()
     {
         Grid theWorld = new Grid(EXAMPLE, _debug);
+        Grid[] evolution = new Grid[4];
 
         if (_debug)
             System.out.println(theWorld);
 
         for (int i = 0; i < 4; i++)
         {
+            evolution[i] = new Grid(MINUTE_BASE+""+(i+1)+".txt", _debug);
             theWorld.evolve();
 
             System.out.println("After "+(i+1)+" minute:");
             System.out.println(theWorld);
+
+            if (!evolution[i].equals(theWorld))
+                return false;
         }
 
-        return false;
+        return true;
     }
 
     private boolean _debug;
