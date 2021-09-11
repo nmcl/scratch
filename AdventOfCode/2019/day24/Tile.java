ef0f97968c68c124beff2aa9412d19188fefdd96
Mark Little <mlittle@redhat.com>
2021-09-11 11:55:16 +0100

    Update Tile.java

--- AdventOfCode/2019/day24/Tile.java
+++ AdventOfCode/2019/day24/Tile.java
@@ -1,4 +1,20 @@
 public class Tile
 {
-    
+    public Tile (char type)
+    {
+        _type = type;
+    }
+
+    public boolean isBug ()
+    {
+        return TileId.BUG == _type;
+    }
+
+    @Override
+    public String toString ()
+    {
+        return Character.toString(_type);
+    }
+
+    private char _type;
 }
