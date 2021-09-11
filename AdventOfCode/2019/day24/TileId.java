adfc160dc5c1bded5b09a812b65a763e5b475397
Mark Little <mlittle@redhat.com>
2021-09-11 12:08:39 +0100

    Update TileId.java

--- AdventOfCode/2019/day24/TileId.java
+++ AdventOfCode/2019/day24/TileId.java
@@ -3,6 +3,14 @@ public class TileId
     public static final char BUG = '#';
     public static final char EMPTY_SPACE = '.';
 
+    public static boolean valid (char check)
+    {
+        if ((check == BUG) || (check == EMPTY_SPACE))
+            return true;
+        else
+            return false;
+    }
+
     private TileId ()
     {
     }
