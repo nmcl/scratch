d5a712906ec2d7e993b1991b13103a02391fdd8a
Mark Little <mlittle@redhat.com>
2021-09-14 11:30:57 +0100

    Update Verifier.java

--- AdventOfCode/2019/day24/part2/Verifier.java
+++ AdventOfCode/2019/day24/part2/Verifier.java
@@ -14,46 +14,6 @@ public class Verifier
 
     public final boolean verify ()
     {
-        Grid theWorld = new Grid(EXAMPLE, _debug);
-        Grid[] evolution = new Grid[4];
-
-        if (_debug)
-            System.out.println(theWorld);
-
-        for (int i = 0; i < 4; i++)
-        {
-            evolution[i] = new Grid(MINUTE_BASE+""+(i+1)+".txt", _debug);
-            theWorld.evolve();
-
-            System.out.println("After "+(i+1)+" minute:");
-            System.out.println(theWorld);
-
-            if (!evolution[i].equals(theWorld))
-                return false;
-        }
-
-        boolean found = false;
-        Grid duplicate = new Grid(DUPLICATE_LAYOUT, _debug);
-        Vector<Grid> previous = new Vector<Grid>();
-
-        previous.add(theWorld.snapshot());
-
-        while (!found)
-        {
-            theWorld.evolve();
-
-            if (previous.contains(theWorld))
-                found = true;
-            else
-                previous.add(theWorld.snapshot());
-        }
-
-        if (theWorld.equals(duplicate))
-        {
-            if (theWorld.biodiversityRating() == BIODIVERSITY_RATING)
-                return true;
-        }
-
         return false;
     }
 
