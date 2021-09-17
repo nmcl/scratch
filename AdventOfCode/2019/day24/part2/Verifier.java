010ed29161d2d2b517824dbb4b5c68ffac548255
Mark Little <mlittle@redhat.com>
2021-09-17 11:43:18 +0100

    Update Verifier.java

--- AdventOfCode/2019/day24/part2/Verifier.java
+++ AdventOfCode/2019/day24/part2/Verifier.java
@@ -5,7 +5,7 @@ public class Verifier
     public static final String EXAMPLE = "example.txt";
     public static final String DEPTH_BASE = "depth";
 
-    public static final int ITERATIONS = 10;  // number of minutes to run`
+    public static final int ITERATIONS = 10;  // number of minutes to run
     public static final long BUGS_PRESENT = 99;
 
     public Verifier (boolean debug)
