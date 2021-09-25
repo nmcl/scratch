f6a49b2c7ab572f72b881a9a422f80058224be31
Mark Little <mlittle@redhat.com>
2021-09-25 19:50:41 +0100

    Update Level.java

--- AdventOfCode/2019/day24/part2/Level.java
+++ AdventOfCode/2019/day24/part2/Level.java
@@ -2,15 +2,49 @@ import java.util.*;
 
 public class Level
 {
-    public Level (boolean debug)
+    public static final int CENTRE_HEIGHT = 2;  // TODO only makes sense for 5x5 so need to fix this!!
+    public static final int CENTRE_WIDTH = 2;
+
+    public Level (int layer, int height, int width, boolean debug)
     {
         _level = new HashSet<ThreeDPoint>();
+        _layer = layer;
+        _height = height;
+        _width = width;
         _debug = debug;
     }
 
     public void addBug (ThreeDPoint location)
     {
         _level.add(location);
+    }
+
+    @Override
+    public String toString ()
+    {
+        String str = "";
+        
+        for (int i = 0; i < _height; i++)
+        {
+            for (int j = 0; j < _width; j++)
+            {
+                if ((i == CENTRE_HEIGHT) && (j == CENTRE_WIDTH))
+                    str += TileId.NESTED_GRID;
+                else
+                {
+                    ThreeDPoint point = new ThreeDPoint(i, j, _layer);
+
+                    if (_level.contains(point))
+                        str += TileId.BUG;
+                    else
+                        str += TileId.EMPTY_SPACE;
+                }
+            }
+
+            str += "\n";
+        }
+
+        return str;
     }
 
     @Override
@@ -44,5 +78,8 @@ public class Level
     }
 
     private HashSet<ThreeDPoint> _level;
+    private int _layer;
+    private int _height;
+    private int _width;
     private boolean _debug;
 }
