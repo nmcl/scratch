7aa5a3a5a2490311159c2293ad4fd1ce8302f33a
Mark Little <mlittle@redhat.com>
2021-09-25 19:40:40 +0100

    Update Level.java

--- AdventOfCode/2019/day24/part2/Level.java
+++ AdventOfCode/2019/day24/part2/Level.java
@@ -1,99 +1,16 @@
+import java.util.*;
+
 public class Level
 {
-    public static final int CENTRE_HEIGHT = 2;  // TODO only makes sense for 5x5 so need to fix this!!
-    public static final int CENTRE_WIDTH = 2;
-
-    public Level (int height, int width, int level, boolean debug)
+    public Level (boolean debug)
     {
-        _level = level;
-        _height = height;
-        _width = width;
-        _debug = debug;
-
-        _theWorld = new Tile[_height][_width];
-
-        for (int i = 0; i < _height; i++)
-        {
-            for (int j = 0; j < _width; j++)
-            {
-                _theWorld[i][j] = new Tile(TileId.EMPTY_SPACE);
-            }
-        }
-
-        _theWorld[CENTRE_HEIGHT][CENTRE_WIDTH] = new Tile(TileId.NESTED_GRID);
-    }
-
-    public Level (Tile[][] theWorld, int level, boolean debug)
-    {
-        _theWorld = theWorld;
-        _height = _theWorld.length;
-        _width = _theWorld[0].length;
-        _level = level;
+        _level = new HashSet<ThreeDPoint>();
         _debug = debug;
     }
 
-    public final int getLevel ()
+    public void addBug (ThreeDPoint location)
     {
-        return _level;
-    }
-
-    public final void makeBug (int i, int j)
-    {
-        if ((i != CENTRE_HEIGHT) && (j != CENTRE_WIDTH))
-            _theWorld[i][j] = new Tile(TileId.BUG);
-    }
-
-    public final void makeSpace (int i, int j)
-    {
-        if ((i != CENTRE_HEIGHT) && (j != CENTRE_WIDTH))
-            _theWorld[i][j] = new Tile(TileId.EMPTY_SPACE);
-    }
-
-    public final boolean containsBug (int i, int j) throws IndexOutOfBoundsException
-    {
-        if (_debug)
-            System.out.println("Checking < "+i+", "+j+" >");
-
-        if (_theWorld[i][j].isBug())
-            return true;
-        else
-            return false;
-    }
-
-    public final boolean nestedLevel (int i, int j)
-    {
-        try
-        {
-            if (_theWorld[i][j].isBug())
-                return true;
-            else
-                return false;
-        }
-        catch (IndexOutOfBoundsException ex)
-        {
-            return false;
-        }
-    }
-
-    @Override
-    public String toString ()
-    {
-        String str = "";
-
-        for (int i = 0; i < _height; i++)
-        {
-            for (int j = 0; j < _width; j++)
-            {
-                if ((i == CENTRE_HEIGHT) && (j == CENTRE_WIDTH))
-                    str += TileId.NESTED_GRID;
-                else
-                    str += _theWorld[i][j];
-            }
-            
-            str += "\n";
-        }
-
-        return str;
+        _level.add(location);
     }
 
     @Override
@@ -109,15 +26,14 @@ public class Level
         {
             Level temp = (Level) obj;
 
-            if ((temp._height == _height) && (temp._width == _width))
+            if (temp._level.size() == _level.size())
             {
-                for (int i = 0; i < _height; i++)
+                Iterator<ThreeDPoint> iter = _level.iterator();
+
+                while (iter.hasNext())
                 {
-                    for (int j = 0; j < _width; j++)
-                    {
-                        if (temp._theWorld[i][j].type() != _theWorld[i][j].type())
-                            return false;
-                    }
+                    if (!temp._level.contains(iter.next()))
+                        return false;
                 }
 
                 return true;
@@ -127,26 +43,6 @@ public class Level
         return false;
     }
 
-    protected Level (Level theGrid)
-    {
-        _height = theGrid._height;
-        _width = theGrid._width;
-        _debug = theGrid._debug;
-
-        _theWorld = new Tile[_height][_width];
-
-        for (int i = 0; i < _height; i++)
-        {
-            for (int j = 0; j < _width; j++)
-            {
-                _theWorld[i][j] = new Tile(theGrid._theWorld[i][j].type());
-            }
-        }
-    }
-
-    private Tile[][] _theWorld;
-    private int _level;
-    private int _height;
-    private int _width;
+    private HashSet<ThreeDPoint> _level;
     private boolean _debug;
 }
