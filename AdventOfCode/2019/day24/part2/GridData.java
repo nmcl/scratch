6515aead7d6ddd4bcd1b6ac1dfb533ec011006a2
Mark Little <mlittle@redhat.com>
2021-09-26 15:32:49 +0100

    Update GridData.java

--- AdventOfCode/2019/day24/part2/GridData.java
+++ AdventOfCode/2019/day24/part2/GridData.java
@@ -12,6 +12,26 @@ public class GridData
     public static final int DEFAULT_HEIGHT = 5;
     public static final int DEFAULT_LEVELS = 5;
 
+    public static final ThreeDPoint topOuterEdge (ThreeDPoint current)
+    {
+        return new ThreeeDPoint(2, 1, current.getZ() - 1);
+    }
+
+    public static final ThreeDPoint bottomOuterEdge (ThreeDPoint current)
+    {
+        return new ThreeeDPoint(2, 3, current.getZ() - 1);
+    }
+
+    public static final ThreeDPoint leftOuterEdge (ThreeDPoint current)
+    {
+        return new ThreeeDPoint(2, 3, current.getZ() - 1);
+    }
+
+    public static final ThreeDPoint rightOuterEdge (ThreeDPoint current)
+    {
+        return new ThreeeDPoint(3, 2, current.getZ() - 1);
+    }
+
     private GridData ()
     {
     }
