7737703baa2f5a94d865da093d4b45771e2aece2
Mark Little <mlittle@redhat.com>
2021-09-26 15:41:25 +0100

    Update GridData.java

--- AdventOfCode/2019/day24/part2/GridData.java
+++ AdventOfCode/2019/day24/part2/GridData.java
@@ -12,6 +12,12 @@ public class GridData
     public static final int DEFAULT_HEIGHT = 5;
     public static final int DEFAULT_LEVELS = 5;
 
+    public static final int CENTRE_X = 2;
+    public static final int CENTRE_Y = 2;
+
+    public static final int TOP_INNER_EDGE_Y = 1;
+    public static final int BOTTOM_INNER_EDGE_Y = 3;
+
     public static final ThreeDPoint topOuterEdge (ThreeDPoint current)
     {
         return new ThreeeDPoint(2, 1, current.getZ() - 1);
@@ -30,6 +36,24 @@ public class GridData
     public static final ThreeDPoint rightOuterEdge (ThreeDPoint current)
     {
         return new ThreeeDPoint(3, 2, current.getZ() - 1);
+    }
+
+    public static final void bottomInnerEdge (ThreeDPoint current, HashSet<ThredDPoint> tiles)
+    {
+        tiles.add(new ThreeeDPoint(0, 4, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(1, 4, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(2, 4, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(3, 4, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(4, 4, current.getZ() + 1));
+    }
+
+    public static final void topInnerEdge (ThreeDPoint current, HashSet<ThreeDPoint> tiles)
+    {
+        tiles.add(new ThreeeDPoint(0, 0, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(1, 0, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(2, 0, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(3, 0, current.getZ() + 1));
+        tiles.add(new ThreeeDPoint(4, 0, current.getZ() + 1));
     }
 
     private GridData ()
