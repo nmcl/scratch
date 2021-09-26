3d5f5e14ba57f545a52c2e91900055a352004788
Mark Little <mlittle@redhat.com>
2021-09-26 15:06:22 +0100

    Create GridData.java

---
+++ AdventOfCode/2019/day24/part2/GridData.java
@@ -0,0 +1,12 @@
+// default values assuming 5x5
+
+public class GridData
+{
+    public static final int DEFAULT_WIDTH = 5;
+    public static final int DEFAULT_HEIGHT = 5;
+    public static final int DEFAULT_LEVELS = 5;
+
+    private GridData ()
+    {
+    }
+}
