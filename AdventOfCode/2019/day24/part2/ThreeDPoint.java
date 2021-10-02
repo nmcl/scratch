741c7faf1c76cc5ad8103868fb078bfc8face16c
Mark Little <mlittle@redhat.com>
2021-10-02 18:10:45 +0100

    Update ThreeDPoint.java

--- AdventOfCode/2019/day24/part2/ThreeDPoint.java
+++ AdventOfCode/2019/day24/part2/ThreeDPoint.java
@@ -34,6 +34,12 @@ public class ThreeDPoint
     {
         return _z;
     }
+
+    @Override
+    public String toString ()
+    {
+        return "< "+_x+", "+_y+", "+_z+" >";
+    }
     
     @Override
     public int hashCode ()
