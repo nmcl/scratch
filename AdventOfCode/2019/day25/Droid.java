4c4fee3f8376b959d17a84b776f37fef5c53746f
Mark Little <mlittle@redhat.com>
2021-10-03 14:43:19 +0100

    Update Droid.java

--- AdventOfCode/2019/day25/Droid.java
+++ AdventOfCode/2019/day25/Droid.java
@@ -1,9 +1,11 @@
 public class Droid
 {
-    public Droid ()
+    public Droid (Vector<String> values, boolean debug)
     {
-
+        _theComputer = new Droid(values, debug);
+        _debug = debug;
     }
 
     private Intcode _theComputer;
+    private boolean _debug;
 }
