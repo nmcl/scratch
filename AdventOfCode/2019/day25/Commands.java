20bbdc9528572485363ea2cd5c1e342415f00bbd
Mark Little <mlittle@redhat.com>
2021-10-03 14:39:32 +0100

    Update Commands.java

---
+++ AdventOfCode/2019/day25/Commands.java
@@ -0,0 +1,15 @@
+public class Commands
+{
+    public static final String NORTH = "north";
+    public static final String SOUTH = "south";
+    public static final String EAST = "east";
+    public static final String WEST = "west";
+
+    public static final String TAKE = "take";
+    public static final String DROP = "drop";
+    public static final String INVENTORY = "inv";
+    
+    private Commands ()
+    {
+    }
+}
