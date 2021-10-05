3629bdf8b86fd30552774a41aa0d48d20875ec5c
Mark Little <mlittle@redhat.com>
2021-10-05 12:13:17 +0100

    Update Cryostasis.java

--- AdventOfCode/2019/day25/Cryostasis.java
+++ AdventOfCode/2019/day25/Cryostasis.java
@@ -1,5 +1,22 @@
 public class Cryostatis
 {
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+        }
+    }
+    
     private Cryostatis ()
     {
     }
