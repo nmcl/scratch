6d3c1a80974b0c7506df9635aa5b592c3fbf3f5c
Mark Little <mlittle@redhat.com>
2021-10-09 17:23:44 +0100

    Update Commands.java

--- AdventOfCode/2019/day25/Commands.java
+++ AdventOfCode/2019/day25/Commands.java
@@ -9,6 +9,13 @@ public class Commands
     public static final String DROP = "drop";
     public static final String INVENTORY = "inv";
 
+    public static final String QUIT = "quit";
+    
+    public static final String getCommands ()
+    {
+        return "1: "+NORTH+"\n2: "+SOUTH+"\n3: "+EAST+"\n4: "+WEST+"\n5: "+TAKE+"\n6: "+DROP+"\n7: "+INVENTORY+"\n8: "+QUIT;
+    }
+
     private Commands ()  // prevent instantiation.
     {
     }
