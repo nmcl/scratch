2521df3beeaa20e94386db8b4ff60b2a263a275b
Mark Little <mlittle@redhat.com>
2021-10-22 18:44:13 +0100

    Create ReportRepair.java

---
+++ AdventOfCode/2020/day1/ReportRepair.java
@@ -0,0 +1,14 @@
+public class ReportRepair
+{
+    public static void main (String[] args)
+    {
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-help]");
+                System.exit(0);
+            }
+        }
+    }
+}
