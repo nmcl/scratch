0cfb2add3ecc5d75e799c96e7bbeb00897067492
Mark Little <mlittle@redhat.com>
2021-10-23 11:05:34 +0100

    Update ReportRepair.java

--- AdventOfCode/2020/day1/ReportRepair.java
+++ AdventOfCode/2020/day1/ReportRepair.java
@@ -1,13 +1,25 @@
 public class ReportRepair
 {
+    public static final int TOTAL_TO_FIND = 2020;
+
     public static void main (String[] args)
     {
+        int number = TOTAL_TO_FIND;
+
         for (int i = 0; i < args.length; i++)
         {
             if ("-help".equals(args[i]))
             {
-                System.out.println("Usage: [-help]");
+                System.out.println("Usage: [-total <number>] [-help]");
                 System.exit(0);
+            }
+
+            if ("-total".equals(args[i]))
+            {
+                try
+                {
+                    number = Integer
+                }
             }
         }
     }
