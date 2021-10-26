d379325b87de80e208b0e9c38ad021ad2f6bf4b7
Mark Little <mlittle@redhat.com>
2021-10-26 11:27:43 +0100

    Update Verifier.java

--- AdventOfCode/2020/day1/Verifier.java
+++ AdventOfCode/2020/day1/Verifier.java
@@ -3,13 +3,16 @@ public class Verifier
     public static final int EXAMPLE_INT1 = 1721;
     public static final int EXAMPLE_INT2 = 299;
 
-    public Verifier ()
+    public Verifier (boolean debug)
     {
-
+        _debug = debug;
     }
 
     public boolean verify ()
     {
+        
         return false;
     }
+
+    private boolean _debug;
 }
