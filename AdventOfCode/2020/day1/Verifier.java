c77a270cc6dc1bac56f68d6cc16c9ad47cdc0a52
Mark Little <mlittle@redhat.com>
2021-10-31 07:51:33 +0000

    Update Verifier.java

--- AdventOfCode/2020/day1/Verifier.java
+++ AdventOfCode/2020/day1/Verifier.java
@@ -5,9 +5,11 @@ public class Verifier
     public static final int EXAMPLE_INT1 = 1721;
     public static final int EXAMPLE_INT2 = 299;
 
+    public static final int EXAMPLE_TOTAL = 2020;
+
     public static final Integer[] EXAMPLE_FIGURES = { 1721, 979, 366, 299, 675, 1456 };
 
-    public Verifier (boolean debug)
+    public Verifier (int total, boolean debug)
     {
         _figures = new Vector<Integer>();
         _totaliser = new Total(_figures);
@@ -18,6 +20,16 @@ public class Verifier
 
     public boolean verify ()
     {
+        Integer[] results = _totaliser.sum(EXAMPLE_TOTAL);
+
+        if (results != null)
+        {
+            if (results.length == 2)
+            {
+                
+            }
+        }
+
         return false;
     }
 
