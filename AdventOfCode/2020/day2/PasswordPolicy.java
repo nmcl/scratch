e5cbe9f72600eaa4a5a219f52a5214e2c866b1b6
Mark Little <mlittle@redhat.com>
2021-11-07 13:14:16 +0000

    Update PasswordPolicy.java

--- AdventOfCode/2020/day2/PasswordPolicy.java
+++ AdventOfCode/2020/day2/PasswordPolicy.java
@@ -22,6 +22,12 @@ public class PasswordPolicy
         return _letter;
     }
 
+    @Override
+    public String toString ()
+    {
+        return "PasswordPolicy: < "+_lower+", "+_upper+", "+_letter+" >";
+    }
+
     private int _lower;
     private int _upper;
     private char _letter;
