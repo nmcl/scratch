3edc456a9d8ceede548efce68de758a2b8ee6c2a
Mark Little <mlittle@redhat.com>
2021-11-07 12:06:39 +0000

    Update Util.java

--- AdventOfCode/2020/day2/Util.java
+++ AdventOfCode/2020/day2/Util.java
@@ -2,8 +2,40 @@ import java.util.*;
 
 public class Util
 {
-    public static Vector<PasswordData> loadData (String file)
+    public static Vector<PasswordData> loadData (String inputFile)
     {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<PasswordData> results = new Vector<PasswordData>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
         return null;
     }
     
