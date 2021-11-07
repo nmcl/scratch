51bd2988eb90a1bf02a05c12afc3380a8fcb31c6
Mark Little <mlittle@redhat.com>
2021-11-07 13:48:18 +0000

    Update Verifier.java

--- AdventOfCode/2020/day2/Verifier.java
+++ AdventOfCode/2020/day2/Verifier.java
@@ -15,6 +15,15 @@ public class Verifier
          * 2-9 c: ccccccccc
          */
 
+        PasswordData[] data = new PasswordData[3];
+
+        data[0] = new PasswordData(new PasswordPolicy(1, 3, 'a'), "abcde");
+        data[1] = new PasswordData(new PasswordPolicy(1, 3, 'b'), "cdefg");
+        data[2] = new PasswordData(new PasswordPolicy(2, 9, 'c'), "ccccccccc");
+
+        if (data[0].valid() && !data[1].valid() && data[2].valid())
+            return true;
+
         return false;
     }
 
