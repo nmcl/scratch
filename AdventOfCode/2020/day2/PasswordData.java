1279ea271c9c433860e11211159dd0ffac93952f
Mark Little <mlittle@redhat.com>
2021-11-07 13:14:14 +0000

    Update PasswordData.java

--- AdventOfCode/2020/day2/PasswordData.java
+++ AdventOfCode/2020/day2/PasswordData.java
@@ -1,7 +1,17 @@
 public class PasswordData
 {
-    public PasswordData ()
+    public PasswordData (PasswordPolicy policy, String password)
     {
-
+        _policy = policy;
+        _pwd = password;
     }
+
+    @Override
+    public String toString ()
+    {
+        return _policy.toString()+"\n"+"Password: "+_pwd;
+    }
+
+    private PasswordPolicy _policy;
+    private String _pwd;
 }
