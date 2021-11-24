a29cc0ea1e19df15619baff985a684f09b0288f2
Mark Little <mlittle@redhat.com>
2021-11-24 18:43:20 +0000

    Update Verifier.java

--- AdventOfCode/2020/day3/Verifier.java
+++ AdventOfCode/2020/day3/Verifier.java
@@ -1,16 +1,20 @@
 public class Verifier
 {
     public static final String EXAMPLE_FILE = "example.txt";
-    
+
     public Verifier (boolean debug)
     {
+        _theMap = new Map(EXAMPLE_FILE, debug);
         _debug = debug;
     }
 
     public boolean verify ()
     {
+        System.out.println("loaded "+_theMap);
+        
         return false;
     }
 
+    private Map _theMap;
     private boolean _debug;
 }
