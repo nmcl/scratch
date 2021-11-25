582e31b9662f77934de3fc720c8b0fec31fee3dd
Mark Little <mlittle@redhat.com>
2021-11-25 18:47:25 +0000

    Update Taboggan.java

--- AdventOfCode/2020/day3/Taboggan.java
+++ AdventOfCode/2020/day3/Taboggan.java
@@ -6,12 +6,14 @@ public class Taboggan
     {
         _maps = new Map[MAP_COPIES];
         _maps[0] = original;
-        
+
+        _position = new Coordinate(0, 0);
         _mapIndex = 0;
         _debug = debug;
     }
 
     private Map[] _maps;
     private int _mapIndex;
+    private Coordinate _position;
     private boolean _debug;
 }
