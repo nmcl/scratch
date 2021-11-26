f0c0f51c3e2ff97576049ec3d9dde5a9db6a1444
Mark Little <mlittle@redhat.com>
2021-11-26 12:29:35 +0000

    Update Taboggan.java

--- AdventOfCode/2020/day3/Taboggan.java
+++ AdventOfCode/2020/day3/Taboggan.java
@@ -10,18 +10,39 @@ public class Taboggan
         _maps = new Map[MAP_COPIES];
         _maps[0] = original;
 
-        _position = new Coordinate(0, 0);
         _mapIndex = 0;
+        _numberOfTrees = 0;
+        _position = new Coordinate(0, 0);
         _debug = debug;
     }
 
     public void move ()
     {
-        
+        boolean finished = false;
+
+        while (!finished)
+        {
+            if (!_maps[_mapIndex].validPosition(_position))
+            {
+                _mapIndex++;
+
+                _maps[_mapIndex] = _maps[0];
+
+                _position = new Coordinate(_position.getX() + MOVE_X, _position.getY() + MOVE_Y);
+            }
+
+            if (!_maps[_mapIndex].finished(_position))
+            {
+
+            }
+            else
+                finished = true;
+        }
     }
 
     private Map[] _maps;
     private int _mapIndex;
+    private int _numberOfTrees;
     private Coordinate _position;
     private boolean _debug;
 }
