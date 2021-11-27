6fa727c31eb30770beedf91e3041cffe4f63dbf8
Mark Little <mlittle@redhat.com>
2021-11-27 19:12:40 +0000

    Update PassportFields.java

--- AdventOfCode/2020/day4/PassportFields.java
+++ AdventOfCode/2020/day4/PassportFields.java
@@ -1,5 +1,14 @@
 public class PassportFields
 {
+    public static final String BIRTH_YEAR = "byr";
+    public static final String ISSUE_YEAR = "iyr";
+    public static final String EXPORATION_YEAR = "eyr";
+    public static final String HEIGHT = "hgt";
+    public static final String HAIR_COLOUR = "hcl";
+    public static final String EYE_COLOUR = "ecl";
+    public static final String PASSPORT_ID = "pid";
+    public static final String COUNTRY_ID = "cid";
+
     private PassportFields ()
     {
     }
