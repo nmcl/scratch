8f747f5a367cf573333957e5bbca7f140a0cd289
Mark Little <mlittle@redhat.com>
2021-11-27 19:12:42 +0000

    Update PassportProcessing.java

--- AdventOfCode/2020/day4/PassportProcessing.java
+++ AdventOfCode/2020/day4/PassportProcessing.java
@@ -2,6 +2,22 @@ public class PassportProcessing
 {
     public static void main (String[] args)
     {
-        
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-debug] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
     }
 }
