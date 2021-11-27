90b6e31bdfb6fd5eeb0bd9b8d4c97c9a6117c914
Mark Little <mlittle@redhat.com>
2021-11-27 17:22:02 +0000

    Update Verifier.java

--- AdventOfCode/2020/day3/part2/Verifier.java
+++ AdventOfCode/2020/day3/part2/Verifier.java
@@ -2,6 +2,12 @@ public class Verifier
 {
     public static final String EXAMPLE_FILE = "example.txt";
 
+    public static final int TREES_1 = 2;
+    public static final int TREES_2 = 7;
+    public static final int TREES_3 = 3;
+    public static final int TREES_4 = 4;
+    public static final int TREES_5 = 2;
+    
     public static final int NUMBER_OF_TREES = 336;
 
     public Verifier (boolean debug)
@@ -21,25 +27,60 @@ public class Verifier
         if (_debug)
             System.out.println("Trees encountered: "+trees1);
         
+        if (trees1 != TREES_1)
+        {
+            System.out.println("Wrong number of trees1: "+trees1);
+
+            return false;
+        }
+
         int trees2 = _taboggan.move(Trajectory.JUMP_2);
 
         if (_debug)
             System.out.println("Trees encountered: "+trees2);
+
+        if (trees2 != TREES_2)
+        {
+            System.out.println("Wrong number of trees2: "+trees2);
+
+            return false;
+        }
 
         int trees3 = _taboggan.move(Trajectory.JUMP_3);
 
         if (_debug)
             System.out.println("Trees encountered: "+trees3);
 
+        if (trees3 != TREES_3)
+        {
+            System.out.println("Wrong number of trees3: "+trees3);
+
+            return false;
+        }
+
         int trees4 = _taboggan.move(Trajectory.JUMP_4);
 
         if (_debug)
             System.out.println("Trees encountered: "+trees4);
 
+        if (trees4 != TREES_4)
+        {
+            System.out.println("Wrong number of trees4: "+trees4);
+
+            return false;
+        }
+
         int trees5 = _taboggan.move(Trajectory.JUMP_5);
 
         if (_debug)
             System.out.println("Trees encountered: "+trees5);
+
+        if (trees5 != TREES_5)
+        {
+            System.out.println("Wrong number of trees5: "+trees5);
+
+            return false;
+        }
 
         int multTrees = trees1 * trees2 * trees3 * trees4 * trees5;
 
