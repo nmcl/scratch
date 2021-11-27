516abd858271830da2199482226470ac2a17af9f
Mark Little <mlittle@redhat.com>
2021-11-27 17:14:00 +0000

    Update Verifier.java

--- AdventOfCode/2020/day3/part2/Verifier.java
+++ AdventOfCode/2020/day3/part2/Verifier.java
@@ -16,12 +16,34 @@ public class Verifier
         if (_debug)
             System.out.println("Verified loaded\n"+_theMap);
 
-        int trees = _taboggan.move();
+        int trees1 = _taboggan.move(Trajectory.JUMP_1);
 
         if (_debug)
-            System.out.println("Trees encountered: "+trees);
+            System.out.println("Trees encountered: "+trees1);
         
-        if (trees == NUMBER_OF_TREES)
+        int trees2 = _taboggan.move(Trajectory.JUMP_2);
+
+        if (_debug)
+            System.out.println("Trees encountered: "+trees2);
+
+        int trees3 = _taboggan.move(Trajectory.JUMP_3);
+
+        if (_debug)
+            System.out.println("Trees encountered: "+trees3);
+
+        int trees4 = _taboggan.move(Trajectory.JUMP_4);
+
+        if (_debug)
+            System.out.println("Trees encountered: "+trees4);
+
+        int trees5 = _taboggan.move(Trajectory.JUMP_5);
+
+        if (_debug)
+            System.out.println("Trees encountered: "+trees5);
+
+        int multTrees = trees1 * trees2 * trees3 * trees4 * trees5;
+
+        if (multTrees == NUMBER_OF_TREES)
             return true;
         else
             return false;
