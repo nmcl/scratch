01c151f2014504c74b4e82c3052c51d6702285b3
Mark Little <mlittle@redhat.com>
2021-11-28 16:45:44 +0000

    Create Passport.java

---
+++ AdventOfCode/2020/day4/Passport.java
@@ -0,0 +1,15 @@
+public class Passport
+{
+    public Passport ()
+    {
+    }
+
+    private int _byr;
+    private int _iry;
+    private int _eyr;
+    private int _hgt;
+    private String _hcl;
+    private String _ecl;
+    private int _pid;
+    private int _cid;
+}
