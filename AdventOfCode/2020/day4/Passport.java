e46bc561d5454eaf69a388422a29b82ce31353db
Mark Little <mlittle@redhat.com>
2021-11-29 14:20:37 +0000

    Update Passport.java

--- AdventOfCode/2020/day4/Passport.java
+++ AdventOfCode/2020/day4/Passport.java
@@ -8,32 +8,32 @@ public class Passport
     {
     }
 
-    public final void setBirthYear (int byr)
+    public final void setBirthYear (String byr)
     {
         _byr = byr;
     }
 
-    public final int getBirthYear ()
+    public final String getBirthYear ()
     {
         return _byr;
     }
 
-    public final void setIssueYear (int iyr)
+    public final void setIssueYear (String iyr)
     {
         _iyr = iyr;
     }
 
-    public final int getIssueYear ()
+    public final String getIssueYear ()
     {
         return _iyr;
     }
 
-    public final void setExpirationYear (int eyr)
+    public final void setExpirationYear (String eyr)
     {
         _eyr = eyr;
     }
 
-    public final int getExpirationYear ()
+    public final String getExpirationYear ()
     {
         return _eyr;
     }
@@ -68,31 +68,31 @@ public class Passport
         return _ecl;
     }
 
-    public final void setPassportID (int pid)
+    public final void setPassportID (String pid)
     {
         _pid = pid;
     }
 
-    public final int getPassportUD ()
+    public final String getPassportUD ()
     {
         return _pid;
     }
 
-    public final void setCountryID (int cid)
+    public final void setCountryID (String cid)
     {
         _cid = cid;
     }
 
-    public final int getCountryID ()
+    public final String getCountryID ()
     {
         return _cid;
     }
 
     public final boolean isValid ()
     {
-        if ((_byr != UNASSIGNED_INT) && (_iyr != UNASSIGNED_INT))
+        if ((_byr != UNASSIGNED_STRING) && (_iyr != UNASSIGNED_STRING))
         {
-            if ((_eyr != UNASSIGNED_INT) && (_pid != UNASSIGNED_INT))
+            if ((_eyr != UNASSIGNED_STRING) && (_pid != UNASSIGNED_STRING))
             {
                 if (!UNASSIGNED_STRING.equals(_hgt) && !UNASSIGNED_STRING.equals(_hcl) && !UNASSIGNED_STRING.equals(_ecl))
                     return true;
@@ -108,12 +108,12 @@ public class Passport
         return "Passport < byr:"+_byr+", iyr:"+_iyr+", eyr:"+_eyr+", hgt:"+_hgt+", hcl:"+_hcl+", ecl:"+_ecl+", pid:"+_pid+", cid:"+_cid+" >";
     }
 
-    private int _byr = UNASSIGNED_INT;
-    private int _iyr = UNASSIGNED_INT;
-    private int _eyr = UNASSIGNED_INT;
+    private String _byr = UNASSIGNED_STRING;
+    private String _iyr = UNASSIGNED_STRING;
+    private String _eyr = UNASSIGNED_STRING;
     private String _hgt = UNASSIGNED_STRING;
     private String _hcl = UNASSIGNED_STRING;
     private String _ecl = UNASSIGNED_STRING;
-    private int _pid = UNASSIGNED_INT;
-    private int _cid = UNASSIGNED_INT;
+    private String _pid = UNASSIGNED_STRING;
+    private String _cid = UNASSIGNED_STRING;
 }
