4994a995ea638995735b2109a30d36e2caf65abb
Mark Little <mlittle@redhat.com>
2021-12-06 15:42:12 +0000

    Update Barcode.java

--- AdventOfCode/2020/day5/Barcode.java
+++ AdventOfCode/2020/day5/Barcode.java
@@ -3,9 +3,21 @@ public class Barcode
     public Barcode (String entry, boolean debug)
     {
         _data = entry;
+        _theSeat = null;
         _debug = debug;
     }
 
+    public final Seat getSeat ()
+    {
+        if (_theSeat == null)
+        {
+
+        }
+
+        return _theSeat;
+    }
+    
     private String _data;
+    private Seat _theSeat;
     private boolean _debug;
 }
