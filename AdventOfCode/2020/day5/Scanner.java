ed06419ffcce4879b6d9f434cb8b96184e3f692c
Mark Little <mlittle@redhat.com>
2021-12-06 11:59:44 +0000

    Update Scanner.java

--- AdventOfCode/2020/day5/Scanner.java
+++ AdventOfCode/2020/day5/Scanner.java
@@ -1,4 +1,23 @@
 public class Scanner
 {
-    
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
 }
