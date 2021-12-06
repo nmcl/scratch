3a8ba10d4ee133defebaa65b6fbb3d8cc8d58818
Mark Little <mlittle@redhat.com>
2021-12-06 15:39:21 +0000

    Update Barcode.java

---
+++ AdventOfCode/2020/day5/Barcode.java
@@ -0,0 +1,11 @@
+public class Barcode
+{
+    public Barcode (String entry, boolean debug)
+    {
+        _data = entry;
+        _debug = debug;
+    }
+
+    private String _data;
+    private boolean _debug;
+}
