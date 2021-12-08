cd6e4c6ec05eff4a3a8a5e14b5c0d7f5acbea9a7
Mark Little <mlittle@redhat.com>
2021-12-08 11:22:31 +0000

    Update Plane.java

--- AdventOfCode/2020/day5/part2/Plane.java
+++ AdventOfCode/2020/day5/part2/Plane.java
@@ -1,9 +1,14 @@
 public class Plane
 {
+    public final int ROWS = 128;
+    public final int COLUMNS = 8;
+
     public Plane (boolean debug)
     {
+        _seats = new Seat[ROWS][COLUMNS];
         _debug = debug;
     }
 
+    private Seat[][] _seats;
     private boolean _debug;
 }
