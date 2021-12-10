8802b183110c3292cc05a4a2cedaf380b3756a58
Mark Little <mlittle@redhat.com>
2021-12-10 12:28:06 +0000

    Update Questions.java

--- AdventOfCode/2020/day6/Questions.java
+++ AdventOfCode/2020/day6/Questions.java
@@ -1,6 +1,7 @@
 public class Questions
 {
-    public final int NUMBER_OF_QUESTIONS = 26;
+    public static final int NUMBER_OF_QUESTIONS = 26;
+    public static final int ASCII_A = 97;
 
     public Questions ()
     {
@@ -10,6 +11,18 @@ public class Questions
         {
             _answers[i] = false;
         }
+    }
+
+    public final boolean answerQuestion (char number, boolean answer)
+    {
+        if ((number > 0) && (number <= NUMBER_OF_QUESTIONS))
+        {
+            _answers[number ASCII_A] = answer;
+
+            return true;
+        }
+
+        return false;
     }
 
     private boolean[] _answers;
