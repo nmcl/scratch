c2051636c044b7cd39689ddb64408b8e429f382b
Mark Little <mlittle@redhat.com>
2021-12-11 14:44:02 +0000

    Update DeclarationForm.java

--- AdventOfCode/2020/day6/DeclarationForm.java
+++ AdventOfCode/2020/day6/DeclarationForm.java
@@ -4,5 +4,20 @@ public class DeclarationForm
     {
         boolean debug = false;
         boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
     }
 }
