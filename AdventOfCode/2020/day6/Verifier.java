8b722353324c4fc98e523299647d1eed14cd9c0a
Mark Little <mlittle@redhat.com>
2021-12-13 15:26:49 +0000

    Update Verifier.java

--- AdventOfCode/2020/day6/Verifier.java
+++ AdventOfCode/2020/day6/Verifier.java
@@ -3,10 +3,14 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE1 = "example1.txt";
+    public static final int EXAMPLE1_PEOPLE = 3;
     public static final int EXAMPLE1_ANSWERS = 6;
+    public static final int EXAMPLE1_GROUPS = 1;
 
     public static final String EXAMPLE2 = "example2.txt";
+    public static final int EXAMPLE2_PEOPLE = 11;
     public static final int EXAMPLE2_ANSWERS = 11;
+    public static final int EXAMPLE2_GROUPS = 5;
     
     public Verifier (boolean debug)
     {
@@ -18,8 +22,20 @@ public class Verifier
         Parser p = new Parser(_debug);
         Vector<Answers> answers = p.work(EXAMPLE1);
 
-        System.out.println("Size: "+answers.size());
-        
+        if (answers.size() == EXAMPLE1_GROUPS)
+        {
+            Answers a = answers.elementAt(0);
+
+            if (a.numberOfPeopleInGroup() == EXAMPLE1_PEOPLE)
+            {
+                if (a.)
+            }
+            else
+                System.out.println("Wrong number of people in group in EXAMPLE1: "+a.numberOfPeopleInGroup());
+        }
+        else
+            System.out.println("Wrong number of groups in EXAMPLE1: "+answers.size());
+
         return false;
     }
 
