6b7a00ef2fde43708d9647b95dbfad5a9b2908c8
Mark Little <mlittle@redhat.com>
2021-12-13 12:17:07 +0000

    Update Verifier.java

--- AdventOfCode/2020/day6/Verifier.java
+++ AdventOfCode/2020/day6/Verifier.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Verifier
 {
+    public static final String EXAMPLE1 = "example1.txt";
+    public static final int EXAMPLE1_ANSWERS = 6;
+
+    public static final String EXAMPLE2 = "example2.txt";
+    public static final int EXAMPLE2_ANSWERS = 11;
+    
     public Verifier (boolean debug)
     {
         _debug = debug;
@@ -7,6 +15,11 @@ public class Verifier
 
     public final boolean verify ()
     {
+        Parser p = new Parser(_debug);
+        Vector<Answers> answers = p.work(EXAMPLE1);
+
+        System.out.println("Size: "+answers.size());
+        
         return false;
     }
 
