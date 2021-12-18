23a19ee8d6cae6ba0f480a20581bdd5a1b2e9f85
Mark Little <mlittle@redhat.com>
2021-12-18 18:23:38 +0000

    Update LuggageProcessing.java

---
+++ AdventOfCode/2020/day7/LuggageProcessing.java
@@ -0,0 +1,23 @@
+public class LuggageProcessing
+{
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verifier = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
