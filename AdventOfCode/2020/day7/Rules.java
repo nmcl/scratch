0d4b680b62d13a109e5723ad27ea4139d6e509c9
Mark Little <mlittle@redhat.com>
2021-12-18 18:27:35 +0000

    Update Rules.java

---
+++ AdventOfCode/2020/day7/Rules.java
@@ -0,0 +1,12 @@
+public class Rules
+{
+    public static final String BAGS = "bags";
+    public static final String BAGS_CONTAINS = "bags contain";
+    public static final String NO_BAGS = "no other bags";
+    public static final String SEPARATOR = ",";
+    public static final String TERMINATOR = ".";
+
+    private Rules ()
+    {
+    }
+}
