0a74cc4820f1a4edcecbc63d301aea3bf6ad7788
Mark Little <mlittle@redhat.com>
2021-12-19 16:29:31 +0000

    Update Rules.java

--- AdventOfCode/2020/day7/Rules.java
+++ AdventOfCode/2020/day7/Rules.java
@@ -18,15 +18,21 @@ public class Rules
         Vector<String> rules = Util.loadData(dataFile, _debug);
         Inventory inv = new Inventory(_debug);
         Enumeration<String> iter = rules.elements();
+        boolean error = false;
 
-        while (iter.hasMoreElements())
+        while (iter.hasMoreElements() && !error)
         {
             String line = iter.nextElement();
             int index = line.indexOf(BAGS_CONTAINS);  // name of bag
 
+            if (index != -1)
+            {
 
-
+            }
+            else
+                error = true;
         }
+
         return inv;
     }
 
