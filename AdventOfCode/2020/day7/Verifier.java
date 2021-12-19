c4bf67c7ce623579c6fe3192d64b4d02acbeebd9
Mark Little <mlittle@redhat.com>
2021-12-19 15:50:59 +0000

    Update Verifier.java

---
+++ AdventOfCode/2020/day7/Verifier.java
@@ -0,0 +1,11 @@
+public class Verifier
+{
+    public static final String EXAMPLE_FILE = "example.txt";
+    
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private boolean _debug;
+}
