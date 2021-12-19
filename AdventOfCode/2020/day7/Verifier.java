e2d78b3b99f42169bab890e098e7ed6c08a03fda
Mark Little <mlittle@redhat.com>
2021-12-19 20:45:10 +0000

    Update Verifier.java

--- AdventOfCode/2020/day7/Verifier.java
+++ AdventOfCode/2020/day7/Verifier.java
@@ -1,7 +1,6 @@
 public class Verifier
 {
     public static final String EXAMPLE_FILE = "example.txt";
-    public static final Bag BAG_TYPE = new Bag("shiny gold");
     public static final int NUMBER_OF_BAGS = 4;
 
     public Verifier (boolean debug)
@@ -17,7 +16,7 @@ public class Verifier
         if (_debug)
             System.out.println("Loaded rules:\n\n"+inv);
 
-        int count = inv.bagCount(BAG_TYPE);
+        int count = inv.bagCount(LuggageProcessing.BAG_TYPE);
 
         if (count == NUMBER_OF_BAGS)
             return true;
