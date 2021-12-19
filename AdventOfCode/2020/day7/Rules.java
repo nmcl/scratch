f83c7f4d40141e6b7fb3103989e6279386565c1e
Mark Little <mlittle@redhat.com>
2021-12-19 16:04:27 +0000

    Update Rules.java

--- AdventOfCode/2020/day7/Rules.java
+++ AdventOfCode/2020/day7/Rules.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Rules
 {
     public static final String BAGS = "bags";
@@ -6,7 +8,17 @@ public class Rules
     public static final String SEPARATOR = ",";
     public static final String TERMINATOR = ".";
 
-    private Rules ()
+    public Rules (boolean debug)
     {
+        _debug = debug;
     }
+
+    public Inventory parse (String dataFile)
+    {
+        Vector<String> rules = Util.loadData(dataFile, _debug);
+
+        return null;
+    }
+
+    private boolean _debug;
 }
