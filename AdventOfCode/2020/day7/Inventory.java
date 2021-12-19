859e07249a3d3960f92631afbf7340ffd5964d79
Mark Little <mlittle@redhat.com>
2021-12-19 16:29:29 +0000

    Update Inventory.java

--- AdventOfCode/2020/day7/Inventory.java
+++ AdventOfCode/2020/day7/Inventory.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
 public class Inventory
 {
     public Inventory (boolean debug)
     {
+        _bags = new Vector<Bag>();
         _debug = debug;
     }
 
+    public final void add (Bag b)
+    {
+        _bags.add(b);
+    }
+
+    private Vector<Bag> _bags;
     private boolean _debug;
 }
