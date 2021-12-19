e96937305cacc4bc4d9555eeb5fc01e62e64b211
Mark Little <mlittle@redhat.com>
2021-12-19 15:59:14 +0000

    Update Bag.java

--- AdventOfCode/2020/day7/Bag.java
+++ AdventOfCode/2020/day7/Bag.java
@@ -1,9 +1,43 @@
+import java.util.*;
+
 public class Bag
 {
     public Bag (String type)
     {
         _type = type;
     }
+
+    @Override
+    public String toString ()
+    {
+        return "Bag coloud: "+_type;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        return Objects.hash(_type);
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (this == obj)
+            return true;
+        
+        if (getClass() == obj.getClass())
+        {
+            Bag temp = (Bag) obj;
+
+            return tenp._type.equals(_tyoe);
+        }
+
+        return false;
+    }
+
 
     private String _type;
 }
