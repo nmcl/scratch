f097d9b14d61c52ce61285725bfc74cc491a4511
Mark Little <mlittle@redhat.com>
2021-12-21 16:51:35 +0000

    Create Accumulator.java

---
+++ AdventOfCode/2020/day8/Accumulator.java
@@ -0,0 +1,13 @@
+// increases or decreases a single global value called the accumulator by the value given in the argument.
+
+public class Accumlator extends OpCode
+{
+    public Accumulator (int amount)
+    {
+        super(OpCode.ACCUMULATOR);
+
+        _amount = amount;
+    }
+
+    private int _amount;
+}
