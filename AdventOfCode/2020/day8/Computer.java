c8acf17dc3a6261b6f1c38135a06a28696ce2adc
Mark Little <mlittle@redhat.com>
2021-12-21 17:50:34 +0000

    Update Computer.java

--- AdventOfCode/2020/day8/Computer.java
+++ AdventOfCode/2020/day8/Computer.java
@@ -1,9 +1,26 @@
+import java.util.*;
+
 public class Computer
 {
     public Computer (boolean debug)
     {
         _accumulator = 0;
         _debug = debug;
+    }
+
+    public int executeUntilInfiniteLoop (Vector<OpCode> instructions)
+    {
+        int index = 0;
+        OpCode op = instructions.elementAt(index);
+
+        _accumulator = 0;
+        
+        while (!op.visited())
+        {
+
+        }
+
+        return -1;
     }
 
     private int _accumulator;
