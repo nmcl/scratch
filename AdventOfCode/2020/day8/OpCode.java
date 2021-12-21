9b9a5af70b8b1b810e65764b48655d119a449b11
Mark Little <mlittle@redhat.com>
2021-12-21 16:51:45 +0000

    Update OpCode.java

--- AdventOfCode/2020/day8/OpCode.java
+++ AdventOfCode/2020/day8/OpCode.java
@@ -3,8 +3,11 @@ public class OpCode
     public static final String ACCUMULATOR = "acc";
     public static final String JUMP = "jmp";
     public static final String NOP = "nop";
-    
-    private OpCode ()
+
+    protected OpCode (String type)
     {
+        _type = type;
     }
+
+    protected String _type;
 }
