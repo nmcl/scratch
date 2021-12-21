57451158afc9c31b1cb67b402c1b7f45b8cc38fa
Mark Little <mlittle@redhat.com>
2021-12-21 16:35:04 +0000

    Create OpCode.java

---
+++ AdventOfCode/2020/day8/OpCode.java
@@ -0,0 +1,10 @@
+public class OpCode
+{
+    public static final String ACCUMULATOR = "acc";
+    public static final String JUMP = "jmp";
+    public static final String NOP = "nop";
+    
+    private OpCode ()
+    {
+    }
+}
