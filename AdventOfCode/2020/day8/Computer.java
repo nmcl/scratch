dd2b7d68ad14ade7296a7a8391a51492c822875c
Mark Little <mlittle@redhat.com>
2021-12-21 16:51:38 +0000

    Update Computer.java

--- AdventOfCode/2020/day8/Computer.java
+++ AdventOfCode/2020/day8/Computer.java
@@ -2,8 +2,10 @@ public class Computer
 {
     public Computer (boolean debug)
     {
+        _accumulator = 0;
         _debug = debug;
     }
 
+    private int _accumulator;
     private boolean _debug;
 }
