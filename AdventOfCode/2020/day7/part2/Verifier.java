605906056fc2c7827929d0924871c3522c6add66
Mark Little <mlittle@redhat.com>
2021-12-21 16:17:40 +0000

    Update Verifier.java

--- AdventOfCode/2020/day7/part2/Verifier.java
+++ AdventOfCode/2020/day7/part2/Verifier.java
@@ -3,6 +3,9 @@ public class Verifier
     public static final String EXAMPLE1_FILE = "example1.txt";
     public static final int EXAMPLE1_TOTAL_NUMBER_OF_BAGS = 32;
 
+    public static final String EXAMPLE2_FILE = "example2.txt";
+    public static final int EXAMPLE2_TOTAL_NUMBER_OF_BAGS = 126;
+
     public Verifier (boolean debug)
     {
         _debug = debug;
@@ -19,13 +22,23 @@ public class Verifier
         int count = inv.totalBagCount(LuggageProcessing.BAG_TYPE);
 
         if (count == EXAMPLE1_TOTAL_NUMBER_OF_BAGS)
-            return true;
-        else
         {
-            System.out.println("Invalid number of total bags: "+count);
+            inv = theRules.parse(EXAMPLE2_FILE);
 
-            return false;
+            if (_debug)
+                System.out.println("\nLoaded rules:\n\n"+inv);
+
+            count = inv.totalBagCount(LuggageProcessing.BAG_TYPE);
+
+            if (count == EXAMPLE2_TOTAL_NUMBER_OF_BAGS)
+                return true;
+            else
+                System.out.println("Invalid number of total bags: "+count+" for "+EXAMPLE2_FILE);
         }
+        else
+            System.out.println("Invalid number of total bags: "+count+" for "+EXAMPLE1_FILE);
+
+        return false;
     }
 
     private boolean _debug;
