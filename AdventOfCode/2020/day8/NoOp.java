233b140785daa4343689821ed9b13b5288040cff
Mark Little <mlittle@redhat.com>
2021-12-21 17:09:03 +0000

    renamed

---
+++ AdventOfCode/2020/day8/NoOp.java
@@ -0,0 +1,9 @@
+// No OPeration - it does nothing
+
+public class NoOp extends OpCode
+{
+    public NoOp ()
+    {
+        super(OpCode.NOOP);
+    }
+}
--- AdventOfCode/2020/day8/Nop.java
+++
@@ -1,9 +0,0 @@
-// No OPeration - it does nothing
-
-public class Nop extends OpCode
-{
-    public Nop ()
-    {
-        super(OpCode.NOP);
-    }
-}
