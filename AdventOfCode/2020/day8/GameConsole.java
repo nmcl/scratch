ae80e672118a334eaae62925d278f191ae935914
Mark Little <mlittle@redhat.com>
2021-12-21 16:35:02 +0000

    Create GameConsole.java

---
+++ AdventOfCode/2020/day8/GameConsole.java
@@ -0,0 +1,23 @@
+public class GameConsole
+{
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[0]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
