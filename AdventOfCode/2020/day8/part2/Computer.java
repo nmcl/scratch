0d97642905407e4e7e48a58e5fbe0ce7b0be6440
Mark Little <mlittle@redhat.com>
2021-12-21 18:48:52 +0000

    Update Computer.java

--- AdventOfCode/2020/day8/part2/Computer.java
+++ AdventOfCode/2020/day8/part2/Computer.java
@@ -7,6 +7,61 @@ public class Computer
         _debug = debug;
     }
 
+    public int executeUntilEnd (Vector<OpCode> instructions)
+    {
+        int index = 0;
+        OpCode op = instructions.elementAt(index);
+        int accumulator = 0;
+
+        if (_debug)
+        {
+            System.out.println("\nInstruction: "+index);
+            System.out.println("Accumulator: "+accumulator+"\n");
+        }
+
+        switch (op.type())
+        {
+            case OpCode.ACCUMULATOR:
+            {
+                int value = ((Accumulator) op).getValue();
+
+                if (_debug)
+                    System.out.println("acc: adding "+value+" to accumulator "+accumulator);
+
+                accumulator += value;
+                index++;
+            }
+            break;
+            case OpCode.JUMP:
+            {
+                int value = ((Jump) op).getStep();
+
+                if (_debug)
+                    System.out.println("jmp: adding "+value+" to index "+index);
+
+                index += ((Jump) op).getStep();
+            }
+            break;
+            case OpCode.NOOP:
+            default:
+            {
+                // no op!!
+
+                if (_debug)
+                    System.out.println("nop");
+
+                index++;
+            }
+            break;
+        }
+
+        op.visit();
+
+        op = instructions.elementAt(index);
+
+        return accumulator;
+    }
+
     public int executeUntilInfiniteLoop (Vector<OpCode> instructions)
     {
         int index = 0;
