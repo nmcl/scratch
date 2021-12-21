32d5fc769980b339fbc21ce7d5efdb698c1bf135
Mark Little <mlittle@redhat.com>
2021-12-21 17:56:36 +0000

    Update Verifier.java

--- AdventOfCode/2020/day8/Verifier.java
+++ AdventOfCode/2020/day8/Verifier.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
+    public static final int ACCUMULATOR = 5;
 
     public Verifier (boolean debug)
     {
@@ -12,6 +13,13 @@ public class Verifier
     public boolean verify ()
     {
         Vector<OpCode> instructions = Util.loadData(EXAMPLE_DATA, _debug);
+        Computer theComputer = new Computer(_debug);
+        int acc = theComputer.executeUntilInfiniteLoop(instructions);
+
+        if (acc == ACCUMULATOR)
+            return true;
+
+        System.out.println("Wrong accumulator value: "+acc+"\n");
         
         return false;
     }
