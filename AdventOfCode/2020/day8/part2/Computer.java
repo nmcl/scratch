3f6f9d09d3084ae76d5fc60e79547d5121ffc3c9
Mark Little <mlittle@redhat.com>
2021-12-22 16:26:54 +0000

    Update Computer.java

--- AdventOfCode/2020/day8/part2/Computer.java
+++ AdventOfCode/2020/day8/part2/Computer.java
@@ -13,6 +13,7 @@ public class Computer
 
         try
         {
+            Vector<OpCode> attempt = new Vector<OpCode>();
 
         }
         catch (ArrayIndexOutOfBoundsException ex)
@@ -21,61 +22,6 @@ public class Computer
         }
 
         return null;
-    }
-
-    public int executeUntilEnd (Vector<OpCode> instructions)
-    {
-        int index = 0;
-        OpCode op = instructions.elementAt(index);
-        int accumulator = 0;
-
-        if (_debug)
-        {
-            System.out.println("\nInstruction: "+index);
-            System.out.println("Accumulator: "+accumulator+"\n");
-        }
-
-        switch (op.type())
-        {
-            case OpCode.ACCUMULATOR:
-            {
-                int value = ((Accumulator) op).getValue();
-
-                if (_debug)
-                    System.out.println("acc: adding "+value+" to accumulator "+accumulator);
-
-                accumulator += value;
-                index++;
-            }
-            break;
-            case OpCode.JUMP:
-            {
-                int value = ((Jump) op).getStep();
-
-                if (_debug)
-                    System.out.println("jmp: adding "+value+" to index "+index);
-
-                index += ((Jump) op).getStep();
-            }
-            break;
-            case OpCode.NOOP:
-            default:
-            {
-                // no op!!
-
-                if (_debug)
-                    System.out.println("nop");
-
-                index++;
-            }
-            break;
-        }
-
-        op.visit();
-
-        op = instructions.elementAt(index);
-
-        return accumulator;
     }
 
     public int executeUntilInfiniteLoop (Vector<OpCode> instructions)
