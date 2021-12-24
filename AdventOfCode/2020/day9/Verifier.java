e56fd3c5aa2fa670a50a5c598fe183caa613d382
Mark Little <mlittle@redhat.com>
2021-12-24 16:02:03 +0000

    Update Verifier.java

--- AdventOfCode/2020/day9/Verifier.java
+++ AdventOfCode/2020/day9/Verifier.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Verifier
 {
+    public static final String EXAMPLE_INPUT = "example.txt";
+
     public static final int EXAMPLE_PREAMBLE = 5;
     public static final int INVALID_NUMBER = 127;
 
@@ -12,6 +14,20 @@ public class Verifier
 
     public boolean verify ()
     {
+        Vector<Integer> data = Util.loadData(EXAMPLE_INPUT, _debug);
+        XMAS parser = new XMAS(_debug);
+        Vector<Integer> results = parser.validate(data, EXAMPLE_PREAMBLE);
+
+        if (results.size() == 1)
+        {
+            if (results.elementAt(0) == INVALID_NUMBER)
+                return true;
+            else
+                System.out.println("Wrong invalid entry: "+results.elementAt(0));
+        }
+        else
+            System.out.println("Wrong number of invalid entries: "+results.size());
+
         return false;
     }
 
