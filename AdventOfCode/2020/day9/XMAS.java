c965f378845ce3ac8f642e72ed42f31ef5bf5626
Mark Little <mlittle@redhat.com>
2021-12-24 15:37:50 +0000

    Update XMAS.java

--- AdventOfCode/2020/day9/XMAS.java
+++ AdventOfCode/2020/day9/XMAS.java
@@ -1,8 +1,18 @@
+import java.util.*;
+
 public class XMAS
 {
     public XMAS (boolean debug)
     {
         _debug = debug;
+    }
+
+    public Vector<Integer> validate (Vector<Integer> input, int preamble)
+    {
+        Vector<Integer> invalid = new Vector<Integer>();
+        int index = 0;
+
+        return invalid;
     }
 
     private boolean _debug;
