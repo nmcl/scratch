9fdeeba4183602f0bc0547d41394e71a4f7a177c
Mark Little <mlittle@redhat.com>
2021-12-26 17:25:08 +0000

    Update AdapterArray.java

--- AdventOfCode/2020/day10/AdapterArray.java
+++ AdventOfCode/2020/day10/AdapterArray.java
@@ -1,32 +1,35 @@
 public class AdapterArray
 {
-    boolean debug = false;
-    boolean verify = false;
-
-    for (int i = 0; i < args.length; i++)
+    public static void main (String[] args)
     {
-        if ("-help".equals(args[i]))
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
         {
-            System.out.println("Usage: [-debug] [-verify] [-help]");
-            System.exit(0);
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
         }
 
-        if ("-debug".equals(args[i]))
-            debug = true;
+        if (verify)
+        {
+            Verifier v = new Verifier(debug);
 
-        if ("-verify".equals(args[i]))
-            verify = true;
-    }
+            if (v.verify())
+                System.out.println("Verified ok.");
+            else
+                System.out.println("Verify failed!");
 
-    if (verify)
-    {
-        Verifier v = new Verifier(debug);
-
-        if (v.verify())
-            System.out.println("Verified ok.");
-        else
-            System.out.println("Verify failed!");
-
-        System.exit(0);
+            System.exit(0);
+        }
     }
 }
