5355716701f80037ab30f2fa4e25acfca210a48b
Mark Little <mlittle@redhat.com>
2021-12-26 18:03:08 +0000

    Update Device.java

--- AdventOfCode/2020/day10/Device.java
+++ AdventOfCode/2020/day10/Device.java
@@ -1,9 +1,16 @@
 public class Device
 {
-    public Device (boolean debug)
+    public Device (JoltageAdapter builtin, boolean debug)
     {
+        _builtin = builtin;
         _debug = debug;
     }
 
+    public JoltageAdapter getAdapter ()
+    {
+        return _builtin;
+
+    }
+    private JoltageAdapter _builtin;
     private boolean _debug;
 }
