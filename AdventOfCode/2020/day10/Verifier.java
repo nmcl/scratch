865e9036666bf7942b8769aeab586a40ed5c398e
Mark Little <mlittle@redhat.com>
2021-12-27 15:31:33 +0000

    Update Verifier.java

--- AdventOfCode/2020/day10/Verifier.java
+++ AdventOfCode/2020/day10/Verifier.java
@@ -8,6 +8,9 @@ public class Verifier
     public static final int EXAMPLE1_DIFFERENCE3 = 5;
 
     public static final String EXAMPLE2_DATA = "example2.txt";
+    public static final int EXAMPLE2_DIFFERENCE1 = 22;
+    public static final int EXAMPLE2_DIFFERENCE2 = 0;
+    public static final int EXAMPLE2_DIFFERENCE3 = 10;
 
     public Verifier (boolean debug)
     {
@@ -16,7 +19,15 @@ public class Verifier
 
     public boolean verify ()
     {
-        Vector<JoltageAdapter> adapters = Util.loadData(EXAMPLE1_DATA, _debug);
+        if (check(EXAMPLE1_DATA, EXAMPLE1_DIFFERENCE1, EXAMPLE1_DIFFERENCE2, EXAMPLE1_DIFFERENCE3))
+            return check(EXAMPLE2_DATA, EXAMPLE2_DIFFERENCE1, EXAMPLE2_DIFFERENCE2, EXAMPLE2_DIFFERENCE3);
+        else
+            return false;
+    }
+
+    private boolean check (String file, int diff1, int diff2, int diff3)
+    {
+        Vector<JoltageAdapter> adapters = Util.loadData(file, _debug);
         Device device = Util.largest(adapters, _debug);
         Connector con = new Connector(_debug);
         Vector<JoltageAdapter> sorted = con.connect(adapters);
@@ -30,22 +41,22 @@ public class Verifier
 
         con.getDifferenceThree().add(device.getAdapter());
 
-        if (con.getDifferenceOne().size() == EXAMPLE1_DIFFERENCE1)
+        if (con.getDifferenceOne().size() == diff1)
         {
-            if (con.getDifferenceTwo().size() == EXAMPLE1_DIFFERENCE2)
+            if (con.getDifferenceTwo().size() == diff2)
             {
-                if (con.getDifferenceThree().size() == EXAMPLE1_DIFFERENCE3)
+                if (con.getDifferenceThree().size() == diff1)
                 {
                     return true;
                 }
                 else
-                    System.out.println("Wrong difference 3 for "+EXAMPLE1_DATA+": "+con.getDifferenceThree().size());
+                    System.out.println("Wrong difference 3 for "+file+": "+con.getDifferenceThree().size());
             }
             else
-                System.out.println("Wrong difference 2 for "+EXAMPLE1_DATA+": "+con.getDifferenceTwo().size());
+                System.out.println("Wrong difference 2 for "+file+": "+con.getDifferenceTwo().size());
         }
         else
-            System.out.println("Wrong difference 1 for "+EXAMPLE1_DATA+": "+con.getDifferenceOne().size());
+            System.out.println("Wrong difference 1 for "+file+": "+con.getDifferenceOne().size());
 
         return false;
     }
