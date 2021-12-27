c33110b7af4d6db0de75e9739de457803ff54498
Mark Little <mlittle@redhat.com>
2021-12-27 14:05:47 +0000

    Update Connector.java

--- AdventOfCode/2020/day10/Connector.java
+++ AdventOfCode/2020/day10/Connector.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Connector
 {
     public Connector (boolean debug)
     {
         _debug = debug;
+    }
+
+    public Vector<JoltageAdapter> connect (Vector<JoltageAdapter> adapters)
+    {
+        return null;
     }
 
     private boolean _debug;
