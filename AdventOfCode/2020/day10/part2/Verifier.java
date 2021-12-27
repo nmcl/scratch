cb669b46b643975c1c60c4751912d7ca3adb82ae
Mark Little <mlittle@redhat.com>
2021-12-27 16:53:09 +0000

    Update Verifier.java

--- AdventOfCode/2020/day10/part2/Verifier.java
+++ AdventOfCode/2020/day10/part2/Verifier.java
@@ -3,14 +3,10 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE1_DATA = "example1.txt";
-    public static final int EXAMPLE1_DIFFERENCE1 = 7;
-    public static final int EXAMPLE1_DIFFERENCE2 = 0;
-    public static final int EXAMPLE1_DIFFERENCE3 = 5;
+    public static final long EXAMPLE1_ARRANGEMENTS = 8;
 
     public static final String EXAMPLE2_DATA = "example2.txt";
-    public static final int EXAMPLE2_DIFFERENCE1 = 22;
-    public static final int EXAMPLE2_DIFFERENCE2 = 0;
-    public static final int EXAMPLE2_DIFFERENCE3 = 10;
+    public static final long EXAMPLE2_ARRANGEMENTS = 19208;
 
     public Verifier (boolean debug)
     {
@@ -19,13 +15,13 @@ public class Verifier
 
     public boolean verify ()
     {
-        if (check(EXAMPLE1_DATA, EXAMPLE1_DIFFERENCE1, EXAMPLE1_DIFFERENCE2, EXAMPLE1_DIFFERENCE3))
-            return check(EXAMPLE2_DATA, EXAMPLE2_DIFFERENCE1, EXAMPLE2_DIFFERENCE2, EXAMPLE2_DIFFERENCE3);
+        if (check(EXAMPLE1_DATA, EXAMPLE1_ARRANGEMENTS))
+            return check(EXAMPLE2_DATA, EXAMPLE2_ARRANGEMENTS);
         else
             return false;
     }
 
-    private boolean check (String file, int diff1, int diff2, int diff3)
+    private boolean check (String file, long combinations)
     {
         Vector<JoltageAdapter> adapters = Util.loadData(file, _debug);
         Device device = Util.largest(adapters, _debug);
@@ -42,24 +38,12 @@ public class Verifier
             }
         }
 
-        con.getDifferenceThree().add(device.getAdapter());
+        long value = con.combinations(sorted);
 
-        if (con.getDifferenceOne().size() == diff1)
-        {
-            if (con.getDifferenceTwo().size() == diff2)
-            {
-                if (con.getDifferenceThree().size() == diff3)
-                {
-                    return true;
-                }
-                else
-                    System.out.println("Wrong difference 3 for "+file+": "+con.getDifferenceThree().size());
-            }
-            else
-                System.out.println("Wrong difference 2 for "+file+": "+con.getDifferenceTwo().size());
-        }
+        if (value == combinations)
+            return true;
         else
-            System.out.println("Wrong difference 1 for "+file+": "+con.getDifferenceOne().size());
+            System.out.println("Wrong number of combinations for "+file+": "+combinations);
 
         return false;
     }
