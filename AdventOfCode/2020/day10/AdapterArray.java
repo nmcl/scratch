d37e19e7e3528c378e9d608790cc5bec3b933dd8
Mark Little <mlittle@redhat.com>
2021-12-27 16:04:14 +0000

    Update AdapterArray.java

--- AdventOfCode/2020/day10/AdapterArray.java
+++ AdventOfCode/2020/day10/AdapterArray.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class AdapterArray
 {
+    public static final String DATA_FILE = "data.txt";
+
     public static void main (String[] args)
     {
         boolean debug = false;
@@ -31,5 +35,17 @@ public class AdapterArray
 
             System.exit(0);
         }
+
+        Vector<JoltageAdapter> adapters = Util.loadData(DATA_FILE, debug);
+        Device device = Util.largest(adapters, debug);
+        Connector con = new Connector(debug);
+        Vector<JoltageAdapter> sorted = con.connect(adapters);
+
+        sorted.add(device.getAdapter());
+        con.getDifferenceThree().add(device.getAdapter());
+        
+        int result = con.getDifferenceOne().size() * con.getDifferenceThree().size();
+
+        System.out.println("Number of 1-jolt differences multiplied by the number of 3-jolt differences: "+result);
     }
 }
