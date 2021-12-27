a8413faacc1fe4e11049cf83a95b52133c9862b4
Mark Little <mlittle@redhat.com>
2021-12-27 14:20:44 +0000

    Update Connector.java

--- AdventOfCode/2020/day10/Connector.java
+++ AdventOfCode/2020/day10/Connector.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Connector
 {
+    public static final int SEAT_JOLTAGE = 0;
+    public static final int JOLTAGE_RANGE = 3;
+
     public Connector (boolean debug)
     {
         _debug = debug;
@@ -9,6 +12,20 @@ public class Connector
 
     public Vector<JoltageAdapter> connect (Vector<JoltageAdapter> adapters)
     {
+        Vector<JoltageAdapter> toReturn = new Vector<JoltageAdapter>();
+        int joltage = 0;
+
+        while (adapters.size() > 0)
+        {
+            boolean found = false;
+
+            for (int i = 0; (i < adapters.size()) && !found; i++)
+            {
+
+            }
+
+        }
+
         return null;
     }
 
