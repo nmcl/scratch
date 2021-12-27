be68f24561bfb22d10ca951c32d7d31cc9fa0a85
Mark Little <mlittle@redhat.com>
2021-12-27 14:59:05 +0000

    Update Verifier.java

--- AdventOfCode/2020/day10/Verifier.java
+++ AdventOfCode/2020/day10/Verifier.java
@@ -1,8 +1,10 @@
+import java.util.*;
+
 public class Verifier
 {
     public static final String EXAMPLE1_DATA = "example1.txt";
     public static final String EXAMPLE2_DATA = "example2.txt";
-    
+
     public Verifier (boolean debug)
     {
         _debug = debug;
@@ -10,6 +12,9 @@ public class Verifier
 
     public boolean verify ()
     {
+        Vector<JoltageAdapter> adapters = Util.loadData(EXAMPLE1_DATA, _debug);
+        Device device = Util.largest(adapters, _debug);
+        
         return false;
     }
 
