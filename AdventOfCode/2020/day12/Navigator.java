016c46ba273f10b629b3d60ca637b63936a8c5e8
Mark Little <mlittle@redhat.com>
2021-12-28 20:00:14 +0000

    Update Navigator.java

---
+++ AdventOfCode/2020/day12/Navigator.java
@@ -0,0 +1,38 @@
+public class Navigator
+{
+    public static final String DATA_FILE = "data.txt";
+
+    public static void main (String[] args)
+    {
+        boolean debug = false;
+        boolean verify = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-debug] [-help]");
+
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+
+        if (verify)
+        {
+            Verifier theVerifier = new Verifier(debug);
+
+            if (theVerifier.verify())
+                System.out.println("Verified ok!");
+            else
+                System.out.println("Verify failed!");
+
+            System.exit(0);
+        }
+    }
+}
