5710835a8058597f7ca40723fdb1705f3ab42a83
Mark Little <mlittle@redhat.com>
2021-12-28 12:10:29 +0000

    Update Cell.java

--- AdventOfCode/2020/day11/Cell.java
+++ AdventOfCode/2020/day11/Cell.java
@@ -1,6 +1,25 @@
-public class Cell
+public class Tile
 {
-    public Cell ()
+    public Tile (char type)
     {
+        _type = type;
     }
+
+    public char type ()
+    {
+        return _type;
+    }
+    
+    public boolean isBug ()
+    {
+        return TileId.BUG == _type;
+    }
+
+    @Override
+    public String toString ()
+    {
+        return Character.toString(_type);
+    }
+
+    private char _type;
 }
