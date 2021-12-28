2ad413f3dd3b690cef08eccbac9bdb3ff4d6ae3b
Mark Little <mlittle@redhat.com>
2021-12-28 12:16:11 +0000

    Update CellId.java

--- AdventOfCode/2020/day11/CellId.java
+++ AdventOfCode/2020/day11/CellId.java
@@ -1,17 +1,18 @@
-public class TileId
+public class CellId
 {
-    public static final char BUG = '#';
-    public static final char EMPTY_SPACE = '.';
+    public static final char EMPTY_SEAT = 'L';
+    public static final char FLOOR = '.';
+    public static final char OCCUPIED_SEAT = '#';
 
     public static boolean valid (char check)
     {
-        if ((check == BUG) || (check == EMPTY_SPACE))
+        if ((check == EMPTY_SEAT) || (check == FLOOR) || (check == OCCUPIED_SEAT))
             return true;
         else
             return false;
     }
 
-    private TileId ()
+    private CellId ()
     {
     }
 }
