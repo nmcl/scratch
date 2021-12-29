e91ba0fd8a5cbbc2a83b95227225e742f3909078
Mark Little <mlittle@redhat.com>
2021-12-29 11:47:48 +0000

    Update Ship.java

--- AdventOfCode/2020/day12/Ship.java
+++ AdventOfCode/2020/day12/Ship.java
@@ -3,9 +3,21 @@ public class Ship
     public Ship (boolean debug)
     {
         _facing = Direction.EAST;
+        _position = new Coordinate(0, 0);
         _debug = debug;
     }
 
+    public final char getFacing ()
+    {
+        return _facing;
+    }
+
+    public final Coordinate getPosition ()
+    {
+        return _position;
+    }
+
     private char _facing;
+    private Coordinate _position;
     private boolean _debug;
 }
