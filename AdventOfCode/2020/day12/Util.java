30e9802e85cfd1ff0e476939177edf3178707ed2
Mark Little <mlittle@redhat.com>
2021-12-29 12:10:25 +0000

    merged.

--- AdventOfCode/2020/day12/Util.java
+++ AdventOfCode/2020/day12/Util.java
@@ -3,6 +3,43 @@ import java.io.*;
 
 public class Util
 {
+    public static char getNextFacing (char current, char action)
+    {
+        // no real error checking!
+
+        switch (current)
+        {
+            case Direction.NORTH:
+            {
+                if (action == Action.LEFT)
+                    return Direction.EAST;
+                else
+                    return Direction.WEST;
+            }
+            case Direction.SOUTH:
+            {
+                if (action == Action.LEFT)
+                    return Direction.WEST;
+                else
+                    return Direction.EAST;
+            }
+            case Direction.EAST:
+            {
+                if (action == Action.LEFT)
+                    return Direction.SOUTH;
+                else
+                    return Direction.NORTH;
+            }
+            default:
+            {
+                if (action == Action.LEFT)
+                    return Direction.NORTH;
+                else
+                    return Direction.SOUTH;
+            }
+        }
+    }
+
     public static final Vector<Command> loadCommands (String inputFile, boolean debug)
     {
         /*
