60f74cdf4e11cba29372272ac4a3af9bcf6b6456
Mark Little <mlittle@redhat.com>
2021-12-29 11:25:04 +0000

    Update Command.java

--- AdventOfCode/2020/day12/Command.java
+++ AdventOfCode/2020/day12/Command.java
@@ -1,14 +1,21 @@
 public class Command
 {
-    public Command (char type)
+    public Command (char type, int distance)
     {
         _type = type;
+        _distance = distance;
     }
 
     public final char action ()
     {
         return _type;
     }
-    
+
+    public final int distance ()
+    {
+        return _distance;
+    }
+
     private char _type;
+    private int _distance;
 }
