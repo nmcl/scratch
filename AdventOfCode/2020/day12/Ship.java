877c1e3011ba6dd1eef57646fe5b7cfd18d937cc
Mark Little <mlittle@redhat.com>
2021-12-29 11:51:46 +0000

    Update Ship.java

--- AdventOfCode/2020/day12/Ship.java
+++ AdventOfCode/2020/day12/Ship.java
@@ -17,6 +17,59 @@ public class Ship
         return _position;
     }
 
+    public final void move (Vector<Command> commands)
+    {
+        Enumeration<Command> iter = commands.elements();
+
+        while (iter.hasMoreElements())
+        {
+            Command c = iter.nextElement();
+
+            switch (c.action())
+            {
+                case Action.EAST:
+                {
+
+                }
+                break;
+                case Action.WEST:
+                {
+
+                }
+                break;
+                case Action.NORTH:
+                {
+
+                }
+                break;
+                case Action.SOUTH:
+                {
+
+                }
+                break;
+                case Action.LEFT:
+                {
+
+                }
+                break;
+                case Action.RIGHT:
+                {
+
+                }
+                break;
+                case Action.FORWARD:
+                {
+                }
+                break;
+                default:
+                {
+                    System.out.println("Unknown action: "+action);
+                }
+                break;
+            }
+        }
+    }
+
     private char _facing;
     private Coordinate _position;
     private boolean _debug;
