2297ffe2f4750188ee4d7736408c5039bce5e4de
Mark Little <mlittle@redhat.com>
2021-12-29 11:44:55 +0000

    Create Direction.java

---
+++ AdventOfCode/2020/day12/Direction.java
@@ -0,0 +1,11 @@
+public class Direction
+{
+    public static final char NORTH = 'N';
+    public static final char SOUTH = 'S';
+    public static final char EAST = 'E';
+    public static final char WEST = 'W';
+    
+    private Direction ()
+    {
+    }
+}
