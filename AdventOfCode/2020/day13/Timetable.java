8cf31b5faeb79fc7ccc53a7925c2f3a062f1c26e
Mark Little <mlittle@redhat.com>
2021-12-30 15:32:54 +0000

    Update Timetable.java

--- AdventOfCode/2020/day13/Timetable.java
+++ AdventOfCode/2020/day13/Timetable.java
@@ -3,10 +3,49 @@ import java.io.*;
 
 public class Timetable
 {
+    public static String SEPARATOR = ",";
+
     public Timetable (String fileName, boolean debug)
     {
+        _buses = new Vector<Bus>();
         _debug = debug;
     }
 
+    private void loadData (String fileName)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<Command> values = new Vector<Command>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                String[] busData = line.split(SEPARATOR);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+    }
+
+    private Vector<Bus> _buses;
     private boolean _debug;
 }
