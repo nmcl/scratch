89cb807eec8ecfb39d45e1444efd2f1d89f721d4
Mark Little <mlittle@redhat.com>
2021-12-30 17:03:54 +0000

    Update Verifier.java

--- AdventOfCode/2020/day13/Verifier.java
+++ AdventOfCode/2020/day13/Verifier.java
@@ -1,6 +1,8 @@
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
+    public static final int EARLIEST_DEPARTURE = 944;
+    public static final int RESULT = 255;
 
     public Verifier (boolean debug)
     {
@@ -10,7 +12,20 @@ public class Verifier
     public boolean verify ()
     {
         Timetable tt = new Timetable(EXAMPLE_DATA, _debug);
-        
+        Bus bs = tt.busToCatch();
+
+        if (bs != null)
+        {
+            if (bs.nextDeparture() == EARLIEST_DEPARTURE)
+            {
+                return true;
+            }
+
+            System.out.println("Incorrect departure time: "+tt.earliestDeparture());
+        }
+        else
+            System.out.println("No bus found!");
+
         return false;
     }
 
