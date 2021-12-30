60e28dfb883c0c39e1d972a49770a0f539f9b5fc
Mark Little <mlittle@redhat.com>
2021-12-30 19:39:43 +0000

    Update Verifier.java

--- AdventOfCode/2020/day13/part2/Verifier.java
+++ AdventOfCode/2020/day13/part2/Verifier.java
@@ -12,25 +12,9 @@ public class Verifier
     public boolean verify ()
     {
         Timetable tt = new Timetable(EXAMPLE1_DATA, _debug);
-        Bus bs = tt.busToCatch();
 
-        if (bs != null)
-        {
-            if (bs.nextDeparture() == EARLIEST_DEPARTURE)
-            {
-                int diff = bs.nextDeparture() - tt.earliestDeparture();
-
-                if (diff * bs.getID() == RESULT)
-                    return true;
-                else
-                    System.out.println("Wrong difference "+diff+" or bus "+bs);
-            }
-            else
-                System.out.println("Incorrect departure time: "+bs.nextDeparture());
-        }
-        else
-            System.out.println("No bus found!");
-
+        tt.earliestTimestamp();
+        
         return false;
     }
 
