0efef1ac3a3c5166cd4af32a944424b4b72164dd
Mark Little <mlittle@redhat.com>
2021-12-30 14:49:01 +0000

    Update Timetable.java

---
+++ AdventOfCode/2020/day13/Timetable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Timetable
+{
+    public Timetable (String fileName, boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private boolean _debug;
+}
