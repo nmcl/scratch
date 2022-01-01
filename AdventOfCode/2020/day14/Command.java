852e67dde94240c1d388bcd70b952b490b1d2983
Mark Little <mlittle@redhat.com>
2022-01-01 16:22:00 +0000

    Update Command.java

--- AdventOfCode/2020/day14/Command.java
+++ AdventOfCode/2020/day14/Command.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Command
 {
-    public Command ()
+    public static final String MASK = "mask";
+    public static final String MEM = "mem";
+
+    public Command (String mask, Vector<String> cmds)
     {
-        
+
     }
 }
