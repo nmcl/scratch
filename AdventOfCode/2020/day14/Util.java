56bec94053ffe08520e1ee70ca5e1b1c15ad5ebf
Mark Little <mlittle@redhat.com>
2022-01-01 16:22:03 +0000

    Update Util.java

--- AdventOfCode/2020/day14/Util.java
+++ AdventOfCode/2020/day14/Util.java
@@ -10,15 +10,39 @@ public class Util
          */
 
         BufferedReader reader = null;
-        Vector<JoltageAdapter> values = new Vector<JoltageAdapter>();
+        Vector<Command> values = new Vector<Command>();
 
         try
         {
             reader = new BufferedReader(new FileReader(inputFile));
             String line = null;
+            Command cmd = null;
+            String mask = null;
+            Vector<String> cmds = null;
 
             while ((line = reader.readLine()) != null)
             {
+                if (line.indexOf(Command.MASK) != -1)
+                {
+                    if (mask != null) // new entry
+                    {
+                        Command cmd = new Command(mask, cmds);
+
+                        values.add(cmd);
+                        
+                        cmds = null;
+                    }
+
+                    nask = line;
+                }
+                else
+                {
+                    if (cmds == null)
+                        cmds = new Vector<String>();
+
+                    cmds.add(line);
+                }
+
                 values.add(new JoltageAdapter(Integer.parseInt(line), debug));
             }
         }
