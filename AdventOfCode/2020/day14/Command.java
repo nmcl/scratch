a1031a8217e395ca84afc3dbaf0bdc8ddbb353bf
Mark Little <mlittle@redhat.com>
2022-01-01 17:06:22 +0000

    Update Command.java

--- AdventOfCode/2020/day14/Command.java
+++ AdventOfCode/2020/day14/Command.java
@@ -7,6 +7,23 @@ public class Command
 
     public Command (String mask, Vector<String> cmds)
     {
-
+        _mask = maskl
+        _commands = cmds;
     }
+
+    @Override
+    public String toString ()
+    {
+        String str = _mask;
+
+        for (int i = 0; i < _commands.size(); i++)
+        {
+            str += "\n" + _commands.elementAt(i);
+        }
+
+        return str;
+    }
+
+    private String _mask;
+    private Vector<String> _commands;
 }
