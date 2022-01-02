2a5cd601804aa45205877241c52dc90d29d9c023
Mark Little <mlittle@redhat.com>
2022-01-02 14:57:21 +0000

    Update Command.java

--- AdventOfCode/2020/day14/Command.java
+++ AdventOfCode/2020/day14/Command.java
@@ -8,15 +8,36 @@ public class Command
     public static final String CLOSE_BRACE = "]";
     public static final String EQUALS = "=";
 
-    public Command (String mask, Vector<String> cmds)
+    public Command (String mask, Vector<String> cmds, boolean debug)
     {
         _mask = mask;
         _commands = cmds;
+        _debug = debug;
     }
 
     public void execute (HashMap<Long, Long> memory)
     {
         String theMask = _mask.substring(_mask.indexOf(EQUALS) +1).trim();
+
+        if (_debug)
+            System.out.println("Bitmask is: "+theMask);
+
+        for (int i = 0; i < _commands.size(); i++)
+        {
+            int startIndex = _commands.elementAt(i).indexOf(OPEN_BRACE);
+            int endIndex = _commands.elementAt(i).indexOf(CLOSE_BRACE);
+            String addr = _commands.elementAt(i).substring(startIndex +1, endIndex);
+
+            if (_debug)
+                System.out.println("Memory address: "+addr);
+
+            startIndex = _commands.elementAt(i).indexOf(EQUALS);
+
+            String value = _commands.elementAt(i).substring(startIndex +1).trim();
+
+            if (_debug)
+                System.out.println("Value: "+value);
+        }
     }
 
     @Override
@@ -34,4 +55,5 @@ public class Command
 
     private String _mask;
     private Vector<String> _commands;
+    private boolean _debug;
 }
