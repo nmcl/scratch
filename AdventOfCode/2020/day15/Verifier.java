9d250496ee8767a971734f404e66be9bd457129a
Mark Little <mlittle@redhat.com>
2022-01-03 15:55:31 +0000

    Update Verifier.java

--- AdventOfCode/2020/day15/Verifier.java
+++ AdventOfCode/2020/day15/Verifier.java
@@ -1,5 +1,20 @@
 public class Verifier
 {
+    public final int[] EXAMPLE_1 = { 0, 3, 6 };
+    public final int EXAMPLE_1_RESULT = 436;
+    public final int[] EXAMPLE_2 = { 1, 3, 2 };
+    public final int EXAMPLE_2_RESULT = 1;
+    public final int[] EXAMPLE_3 = { 2, 1, 3 };
+    public final int EXAMPLE_3_RESULT = 10;
+    public final int[] EXAMPLE_4 = { 1, 2, 3 };
+    public final int EXAMPLE_4_RESULT = 27;
+    public final int[] EXAMPLE_5 = { 2, 3, 1 };
+    public final int EXAMPLE_5_RESULT = 78;
+    public final int[] EXAMPLE_6 = { 3, 2, 1 };
+    public final int EXAMPLE_6_RESULT = 438;
+    public final int[] EXAMPLE_7 = { 3, 1, 2 };
+    public final int EXAMPLE_7_RESULT = 1836;
+
     public Verifier (boolean debug)
     {
         _debug = debug;
