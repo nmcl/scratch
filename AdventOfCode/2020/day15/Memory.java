b557e19e697951dd718194cc71fb5d0a17553bd3
Mark Little <mlittle@redhat.com>
2022-01-03 17:51:30 +0000

    Update Memory.java

---
+++ AdventOfCode/2020/day15/Memory.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Memory
+{
+    public Memory ()
+    {
+        _theMemory = new HashMap<Long, Long>();
+    }
+
+    public void addNumber (long numb)
+    {
+
+    }
+
+    public long numberPresent (long numb)
+    {
+        return -1;
+    }
+
+    private HashMap<Long, Long> _theMemory;
+}
