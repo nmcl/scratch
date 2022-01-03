5e20d496dcab22cfb548d32628e10da5714ce20a
Mark Little <mlittle@redhat.com>
2022-01-03 19:09:00 +0000

    Update Memory.java

--- AdventOfCode/2020/day15/Memory.java
+++ AdventOfCode/2020/day15/Memory.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/*
+ * They begin by taking turns reading from a list of starting numbers (your puzzle input).
+ * Then, each turn consists of considering the most recently spoken number:
+ * 
+ * If that was the first time the number has been spoken, the current player says 0.
+ * Otherwise, the number had been spoken before; the current player announces how many turns
+ * apart the number is from when it was previously spoken.
+ */
+
 public class Memory
 {
     public Memory ()
