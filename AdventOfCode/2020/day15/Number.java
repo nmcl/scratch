4f1dd98b827de6c17d56f13c64e9eb1190f1a35e
Mark Little <mlittle@redhat.com>
2022-01-04 14:33:54 +0000

    Update Number.java

--- AdventOfCode/2020/day15/Number.java
+++ AdventOfCode/2020/day15/Number.java
@@ -4,8 +4,20 @@ public class Number
     {
         _value = value;
         _turnLastSpoken = turn;
+        _numberOfTimesSpoken = 0;
+    }
+
+    public void spoken ()
+    {
+        _numberOfTimesSpoken++;
+    }
+
+    public Long spokenTimes ()
+    {
+        return _numberOfTimesSpoken;
     }
 
     private Long _value;
     private Long _turnLastSpoken;
+    private Long _numberOfTimesSpoken;
 }
