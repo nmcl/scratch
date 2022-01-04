e8c418d2f85176169fa7ec7a1d48b10f83c5f8da
Mark Little <mlittle@redhat.com>
2022-01-04 16:35:38 +0000

    Update Number.java

--- AdventOfCode/2020/day15/Number.java
+++ AdventOfCode/2020/day15/Number.java
@@ -10,11 +10,13 @@ public class Number
 
     public void spoken (Integer turn)
     {
-        System.out.println("Turn "+turn+" for "+_value);
+        System.out.println("On turn "+turn+" spoken "+this);
 
         _nextRecentTurn = _recentTurn;
         _recentTurn = turn;
         _numberOfTimesSpoken++;
+
+        System.out.println("Now on turn "+turn+" spoken "+this);
     }
 
     public Integer turnDifference ()
