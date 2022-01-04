25adb3d12da0952ae329fff3bc5b158d7c96a489
Mark Little <mlittle@redhat.com>
2022-01-04 20:37:00 +0000

    Create Util.java

---
+++ AdventOfCode/2020/day16/Util.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Util
+{
+    public static final Vector<Category> loadData (String inputFile, boolean debug)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<Command> values = new Vector<Command>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+
+            while ((line = reader.readLine()) != null)
+            {
+                if (line.indexOf(Command.MASK) != -1)
+                {
+                    if (mask != null) // new entry
+                    {
+                        values.add(new Command(mask, cmds, debug));
+                        cmds = null;
+                    }
+
+                    mask = line;
+                }
+                else
+                {
+                    if (cmds == null)
+                        cmds = new Vector<String>();
+
+                    cmds.add(line);
+                }
+            }
+
+            if (cmds != null)
+                values.add(new Command(mask, cmds, debug));
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return values;
+    }
+
+    private Util ()
+    {
+    }
+}
