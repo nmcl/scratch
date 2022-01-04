f288ff19bfdd3c96a903235c392ef6a187ba99ea
Mark Little <mlittle@redhat.com>
2022-01-04 18:52:09 +0000

    Update Verifier.java

--- AdventOfCode/2020/day15/part2/Verifier.java
+++ AdventOfCode/2020/day15/part2/Verifier.java
@@ -1,19 +1,19 @@
 public class Verifier
 {
     public final int[] EXAMPLE_1 = { 0, 3, 6 };
-    public final int EXAMPLE_1_RESULT = 436;
+    public final int EXAMPLE_1_RESULT = 175594;
     public final int[] EXAMPLE_2 = { 1, 3, 2 };
-    public final int EXAMPLE_2_RESULT = 1;
+    public final int EXAMPLE_2_RESULT = 2578;
     public final int[] EXAMPLE_3 = { 2, 1, 3 };
-    public final int EXAMPLE_3_RESULT = 10;
+    public final int EXAMPLE_3_RESULT = 3544142;
     public final int[] EXAMPLE_4 = { 1, 2, 3 };
-    public final int EXAMPLE_4_RESULT = 27;
+    public final int EXAMPLE_4_RESULT = 261214;
     public final int[] EXAMPLE_5 = { 2, 3, 1 };
-    public final int EXAMPLE_5_RESULT = 78;
+    public final int EXAMPLE_5_RESULT = 6895259;
     public final int[] EXAMPLE_6 = { 3, 2, 1 };
-    public final int EXAMPLE_6_RESULT = 438;
+    public final int EXAMPLE_6_RESULT = 18;
     public final int[] EXAMPLE_7 = { 3, 1, 2 };
-    public final int EXAMPLE_7_RESULT = 1836;
+    public final int EXAMPLE_7_RESULT = 362;
 
     public Verifier (boolean debug)
     {
@@ -51,7 +51,7 @@ public class Verifier
         Memory mem = new Memory(_debug);
         Integer lastNumberSpoken = speakInitial(initial, mem);
 
-        for (int i = 3; i < 2020; i++)
+        for (int i = 3; i < 30000000; i++)
         {
             if (_debug)
                 System.out.println("Turn number "+i+" spoke "+lastNumberSpoken);
