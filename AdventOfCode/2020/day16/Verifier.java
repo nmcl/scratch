8cddb44e7325c90fe1f358b1692c01c1e8e75599
Mark Little <mlittle@redhat.com>
2022-01-05 14:00:25 +0000

    Update Verifier.java

--- AdventOfCode/2020/day16/Verifier.java
+++ AdventOfCode/2020/day16/Verifier.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Verifier
 {
+    public static final String EXAMPLE_DATA = "example.txt";
+
     public Verifier (boolean debug)
     {
         _debug = debug;
@@ -7,6 +11,15 @@ public class Verifier
 
     public boolean verify ()
     {
+        Vector<Category> cats = Util.loadCategories(EXAMPLE_DATA, _debug);
+        Vector<Ticket> ticks = Util.loadTickets(EXAMPLE_DATA, _debug);
+
+        for (int i = 0; i < cats.size(); i++)
+            System.out.println(cats.elementAt(i));
+
+        for (int j = 0; j < ticks.size(); j++)
+            System.out.println(ticks.elementAt(j));
+
         return false;
     }
 
