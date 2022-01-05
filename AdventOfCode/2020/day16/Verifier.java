592a6e2fb011a45ab8468d76b5554526148c0dca
Mark Little <mlittle@redhat.com>
2022-01-05 16:19:23 +0000

    Update Verifier.java

--- AdventOfCode/2020/day16/Verifier.java
+++ AdventOfCode/2020/day16/Verifier.java
@@ -14,11 +14,23 @@ public class Verifier
         Vector<Category> cats = Util.loadCategories(EXAMPLE_DATA, _debug);
         Vector<Ticket> ticks = Util.loadTickets(EXAMPLE_DATA, _debug);
 
-        for (int i = 0; i < cats.size(); i++)
-            System.out.println(cats.elementAt(i));
+        if (_debug)
+        {
+            for (int i = 0; i < cats.size(); i++)
+                System.out.println(cats.elementAt(i));
 
-        for (int j = 0; j < ticks.size(); j++)
-            System.out.println(ticks.elementAt(j));
+            for (int j = 0; j < ticks.size(); j++)
+                System.out.println(ticks.elementAt(j));
+        }
+
+        for (int k = 0; k < ticks.size(); k++)
+        {
+            for (int l = 0; l < cats.size(); l++)
+            {
+                if (!cats.elementAt(l).valid(ticks.elementAt(k)))
+                    System.out.println(ticks.elementAt(k)+" is invalid.");
+            }
+        }
 
         return false;
     }
