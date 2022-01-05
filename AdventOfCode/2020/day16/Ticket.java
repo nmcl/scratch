ab892737e3f40566ebeb880392535af40c4bacdb
Mark Little <mlittle@redhat.com>
2022-01-05 13:49:45 +0000

    Update Ticket.java

--- AdventOfCode/2020/day16/Ticket.java
+++ AdventOfCode/2020/day16/Ticket.java
@@ -2,5 +2,18 @@ public class Ticket
 {
     public Ticket (String[] range)
     {
+        _values = new int[range.length];
+
+        for (int i = 0; i < range.length; i++)
+        {
+            _values[i] = Integer.parseInt(range[i]);
+        }
     }
+
+    public int[] values ()
+    {
+        return _values;
+    }
+
+    private int[] _values;
 }
