b72114caf6aa08824075c08702b9888eba2d9279
Mark Little <mlittle@redhat.com>
2022-01-05 20:43:58 +0000

    Update Verifier.java

--- AdventOfCode/2020/day16/Verifier.java
+++ AdventOfCode/2020/day16/Verifier.java
@@ -4,6 +4,7 @@ public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
 
+    public static final Ticket OWN_TICKET = new Ticket(new String[] { "7" ,"1" ,"14" });
     public static final Ticket TICKET_1 = new Ticket(new String[] { "40" ,"4" ,"50" });
     public static final Ticket TICKET_2 = new Ticket(new String[] { "55" ,"2" ,"20" });
     public static final Ticket TICKET_3 = new Ticket(new String[] { "38" ,"6" ,"12" });
@@ -27,12 +28,27 @@ public class Verifier
                 System.out.println(ticks.elementAt(j));
         }
 
+        boolean[] correct = new boolean[4];
+
+        for (int l = 0; l < 4; l++)
+            correct[l] = false;
+
         for (int k = 0; k < ticks.size(); k++)
         {
-            Util.checkAlCategories(cats, ticks.elementAt(k));
+            if (ticks.elementAt(k).equals(OWN_TICKET) && (Util.checkAlCategories(cats, OWN_TICKET)))
+                correct[0] = true;
+
+            if (ticks.elementAt(k).equals(TICKET_1) && (!Util.checkAlCategories(cats, TICKET_1)))
+                correct[1] = true;
+
+            if (ticks.elementAt(k).equals(TICKET_2) && (!Util.checkAlCategories(cats, TICKET_2)))
+                correct[2] = true;
+
+            if (ticks.elementAt(k).equals(TICKET_3) && (!Util.checkAlCategories(cats, TICKET_3)))
+                correct[3] = true;
         }
 
-        return false;
+        return correct[0] & correct[1] & correct[2] & correct[3];
     }
 
     private boolean _debug;
