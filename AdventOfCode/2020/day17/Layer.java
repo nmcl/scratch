21703e2decd7110b06ec80e5d04aa97c9e9a455b
Mark Little <mlittle@redhat.com>
2022-01-15 16:23:24 +0000

    Update Layer.java

--- AdventOfCode/2020/day17/Layer.java
+++ AdventOfCode/2020/day17/Layer.java
@@ -40,36 +40,6 @@ public class Layer
         return str;
     }
 
-    @Override
-    public boolean equals (Object obj)
-    {
-        if (obj == null)
-            return false;
-
-        if (this == obj)
-            return true;
-        
-        if (getClass() == obj.getClass())
-        {
-            Layer temp = (Layer) obj;
-
-            if (temp._level.size() == _level.size())
-            {
-                Iterator<ThreeDPoint> iter = _level.iterator();
-
-                while (iter.hasNext())
-                {
-                    if (!temp._level.contains(iter.next()))
-                        return false;
-                }
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     private char[][] _cubes;
     private int _layer;
     private boolean _debug;
