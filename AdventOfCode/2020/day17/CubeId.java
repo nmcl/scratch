760d82bef85feee48eb237398275ff0b710b139b
Mark Little <mlittle@redhat.com>
2022-01-17 14:01:09 +0000

    Update CubeId.java

--- AdventOfCode/2020/day17/CubeId.java
+++ AdventOfCode/2020/day17/CubeId.java
@@ -3,14 +3,6 @@ public class CubeId
     public static final char ACTIVE = '#';
     public static final char INACTIVE = '.';
 
-    public static boolean valid (char check)
-    {
-        if ((check == ACTIVE) || (check == INACTIVE))
-            return true;
-        else
-            return false;
-    }
-
     private CubeId ()
     {
     }
