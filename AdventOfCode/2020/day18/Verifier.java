0b02257bb8a76728a06a223fe10b88a0bcc69106
Mark Little <mlittle@redhat.com>
2022-01-23 17:24:28 +0000

    Update Verifier.java

--- AdventOfCode/2020/day18/Verifier.java
+++ AdventOfCode/2020/day18/Verifier.java
@@ -4,6 +4,8 @@ public class Verifier
 {
     public static final String EXAMPLE1 = "example1.txt";
     public static final long EXAMPLE1_RESULT = 71;
+    public static final String EXAMPLE2 = "example2.txt";
+    public static final long EXAMPLE2_RESULT = 51;
 
     public Verifier (boolean debug)
     {
@@ -17,13 +19,20 @@ public class Verifier
         Long result = p.parse(data);
 
         if (result == EXAMPLE1_RESULT)
-            return true;
-        else
         {
+            data = Util.loadData(EXAMPLE2);
+
+            result = p.parse(data);
+
+            if (result == EXAMPLE2_RESULT)
+                return true;
+            else
+                System.out.println("Incorrect value for "+EXAMPLE2+" "+result);
+        }
+        else
             System.out.println("Incorrect value for "+EXAMPLE1+" "+result);
 
-            return false;
-        }
+        return false;
     }
 
     private boolean _debug;
