4c704e871c03ab29c0e9e355a4c5092aeed7f8a9
Mark Little <mlittle@redhat.com>
2022-01-23 16:23:34 +0000

    Update MathsParser.java

--- AdventOfCode/2020/day18/MathsParser.java
+++ AdventOfCode/2020/day18/MathsParser.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class MathsParser
 {
     public MathsParser (boolean debug)
     {
         _debug = debug;
+    }
+
+    public int parse (Vector<String> data)
+    {
+        return -1;
     }
 
     private boolean _debug;
