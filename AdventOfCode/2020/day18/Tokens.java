f352548a84d59fd5309d91a20eda3b54735db604
Mark Little <mlittle@redhat.com>
2022-01-23 16:22:43 +0000

    Update Tokens.java

--- AdventOfCode/2020/day18/Tokens.java
+++ AdventOfCode/2020/day18/Tokens.java
@@ -1,12 +1,12 @@
-public class Token
+public class Tokens
 {
     public static final char SPACE = ' ';
     public static final char PLUS = '+';
     public static final char MULTIPLY = '*';
     public static final char OPEN_BRACE = '(';
     public static final char CLOSE_BRACE = ')';
-    
-    private Token ()
+
+    private Tokens ()
     {
     }
 }
