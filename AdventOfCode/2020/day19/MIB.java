14be8674c5ee752ddc61535ba37bd84dd38ecb1a
Mark Little <mlittle@redhat.com>
2022-01-30 14:22:52 +0000

    Update MIB.java

---
+++ AdventOfCode/2020/day19/MIB.java
@@ -0,0 +1,37 @@
+public class MIB
+{
+    public static final String MESSAGE_FILE = "messages.txt";
+
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+
+        if (verify)
+        {
+            Verifier v = new Verifier(debug);
+
+            if (v.verify())
+                System.out.println("Verified ok.");
+            else
+                System.out.println("Verify failed!");
+
+            System.exit(0);
+        }
+    }
+}
