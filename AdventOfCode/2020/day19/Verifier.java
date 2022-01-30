531c0f2782b98948793922470ad4807d4c53e6f0
Mark Little <mlittle@redhat.com>
2022-01-30 14:21:48 +0000

    Update Verifier.java

---
+++ AdventOfCode/2020/day19/Verifier.java
@@ -0,0 +1,14 @@
+public class Verify
+{
+    public Verify (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    public boolean verify ()
+    {
+        return false;
+    }
+
+    private boolean _debug;
+}
