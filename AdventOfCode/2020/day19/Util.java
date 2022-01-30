4cc454dc4c80c4795addf8b2afd07ff535e22deb
Mark Little <mlittle@redhat.com>
2022-01-30 14:33:31 +0000

    Update Util.java

---
+++ AdventOfCode/2020/day19/Util.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Util
+{
+    public static final Vector<Rule> loadRules (String inputFile, boolean debug)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<Command> values = new Vector<Command>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return values;
+    }
+
+    private Util ()
+    {
+    }
+}
