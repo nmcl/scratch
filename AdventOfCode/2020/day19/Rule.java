6c0edebf25a221d57c8a5a825d4d4503b5eea9e1
Mark Little <mlittle@redhat.com>
2022-02-05 13:39:02 +0000

    Update Rule.java

--- AdventOfCode/2020/day19/Rule.java
+++ AdventOfCode/2020/day19/Rule.java
@@ -1,20 +1,20 @@
 public class Rule
 {
-    public Rule (String number, char[] ruleOneList, char[] ruleTwoList)
+    public Rule (String number, String[] ruleOneList, String[] ruleTwoList)
     {
         _number = Integer.valueOf(number);
         
         _ruleOne = new int[ruleOneList.length];
 
         for (int i = 0; i < _ruleOne.length; i++)
-            _ruleOne[i] = Character.getNumericValue(ruleOneList[i]);
+            _ruleOne[i] = Integer.valueOf(ruleOneList[i]);
 
         if (ruleTwoList != null)
         {
             _ruleTwo = new int[ruleTwoList.length];
 
             for (int j = 0; j < _ruleOne.length; j++)
-                _ruleTwo[j] = Character.getNumericValue(ruleTwoList[j]);
+                _ruleTwo[j] = Integer.valueOf(ruleTwoList[j]);
         }
         else
             _ruleTwo = null;
