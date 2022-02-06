4d76ce0fea0d2ad7d19b6cb137ca9a8b659e0910
Mark Little <mlittle@redhat.com>
2022-02-06 15:48:12 +0000

    Update Matcher.java

--- AdventOfCode/2020/day19/Matcher.java
+++ AdventOfCode/2020/day19/Matcher.java
@@ -13,13 +13,33 @@ public class Matcher
         return null;
     }
 
-    private String getMatchingString (Rule theRule, String current)
+    private String getMatchingString (Rule theRule, String current, boolean checkFirst)
     {
         String str = current;
 
         if (theRule.getMatch() == Rule.NO_MATCH)
         {
+            if (checkFirst)
+            {
+                int[] firstRules = theRule.firstDependantRules();
 
+                for (int i = 0; i < firstRules.length; i++)
+                {
+                    str = getMatchingString(_rules[firstRules[i]], str, true);
+                }
+            }
+            else
+            {
+                int[] secondRules = theRule.secondDependantRules();
+
+                if (secondRules != null)
+                {
+                    for (int i = 0; i < secondRules.length; i++)
+                    {
+                        str = getMatchingString(_rules[secondRules[i]], str, false);
+                    }
+                }
+            }
         }
         else
             str += theRule.getMatch();
