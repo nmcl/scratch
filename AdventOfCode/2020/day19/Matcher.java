5a05f423a8ed518767cbba47d0e62484f2d05a86
Mark Little <mlittle@redhat.com>
2022-02-09 13:10:44 +0000

    Update Matcher.java

--- AdventOfCode/2020/day19/Matcher.java
+++ AdventOfCode/2020/day19/Matcher.java
@@ -16,22 +16,16 @@ public class Matcher
         //if (_debug)
             System.out.println("Trying to match rule: "+ruleToMatch);
 
-        String current = getMatchingString(ruleToMatch, "", true);
+        String current = getMatchingString(ruleToMatch, "");
 
         System.out.println("Match using first rules: "+current);
 
         toMatch.add(new Message(current));
 
-        current = getMatchingString(ruleToMatch, "", false);
-
-        toMatch.add(new Message(current));
-        
-        System.out.println("Match using second rules: "+current);
-
         return null;
     }
 
-    private String getMatchingString (Rule theRule, String current, boolean checkFirst)
+    private String getMatchingString (Rule theRule, String current)
     {
         String str = current;
 
@@ -39,32 +33,14 @@ public class Matcher
 
         if (!theRule.isCharacterRule())
         {
-            if (checkFirst)
+            int[] firstRules = theRule.firstDependantRules();
+
+            for (int i = 0; i < firstRules.length; i++)
             {
-                int[] firstRules = theRule.firstDependantRules();
+                str = getMatchingString(_rules[firstRules[i]], str);
 
-                for (int i = 0; i < firstRules.length; i++)
-                {
-                    str = getMatchingString(_rules[firstRules[i]], str, true);
-
-                    //if (_debug)
-                        System.out.println("First rules string: "+str);
-                }
-            }
-            else
-            {
-                int[] secondRules = theRule.secondDependantRules();
-
-                if (secondRules != null)
-                {
-                    for (int i = 0; i < secondRules.length; i++)
-                    {
-                        str = getMatchingString(_rules[secondRules[i]], str, false);
-
-                        //if (_debug)
-                            System.out.println("Second rules string: "+str);
-                    }
-                }
+                //if (_debug)
+                    System.out.println("First rules string: "+str);
             }
         }
         else
