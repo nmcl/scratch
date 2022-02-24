843f959233b6fe32648c4afe828fcc77a6d35cbf
Mark Little <mlittle@redhat.com>
2022-02-24 11:40:40 +0000

    Update Tile.java

---
+++ AdventOfCode/2020/day20/Tile.java
@@ -0,0 +1,13 @@
+public class Tile
+{
+    public Tile (int number, char[][] data, boolean debug)
+    {
+        _id = number;
+        _data = date;
+        _debug = debug;
+    }
+
+    private int _id;
+    private char[][] _data;
+    private boolean _debug;
+}
