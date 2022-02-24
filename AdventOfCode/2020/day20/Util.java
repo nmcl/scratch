920e9048107596706637e8151661d4ab8f5f1466
Mark Little <mlittle@redhat.com>
2022-02-24 11:41:56 +0000

    Create Util.java

---
+++ AdventOfCode/2020/day20/Util.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Util
+{
+    public static final Vector<Tile> loadData (String inputFile)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<String> data = new Vector<String>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+
+            while ((line = reader.readLine()) != null)
+            {
+                data.add(line);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return data;
+    }
+
+    private Util ()
+    {
+    }
+}
