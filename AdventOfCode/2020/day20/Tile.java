c7b503646741913a320be72a65acc2e483851d00
Mark Little <mlittle@redhat.com>
2022-02-26 17:56:48 +0000

    Update Tile.java

--- AdventOfCode/2020/day20/Tile.java
+++ AdventOfCode/2020/day20/Tile.java
@@ -12,6 +12,24 @@ public class Tile
         return _id;
     }
 
+    @Override
+    public String toString ()
+    {
+        String str = TileData.TILE_ID+_id+":\n";
+
+        for (int i = 0; i < _data.length; i++)
+        {
+            for (int j = 0; i < _data[0].length; j++)
+            {
+                str += _data[j][i];
+            }
+
+            str += "\n";
+        }
+
+        return str;
+    }
+
     private int _id;
     private char[][] _data;
     private boolean _debug;
