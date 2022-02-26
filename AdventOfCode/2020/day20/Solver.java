26f2738ce499d25b07085a2b5494bd599b14f8ea
Mark Little <mlittle@redhat.com>
2022-02-26 19:39:44 +0000

    Update Solver.java

--- AdventOfCode/2020/day20/Solver.java
+++ AdventOfCode/2020/day20/Solver.java
@@ -18,6 +18,8 @@ public class Solver
         for (int i = 0; i < tiles.size(); i++)
         {
             Tile t = tiles.elementAt(i);
+
+            numberOfMatchingEdges(t, tiles);
         }
 
         return toReturn;
@@ -25,22 +27,27 @@ public class Solver
 
     private int numberOfMatchingEdges (Tile toCheck, Vector<Tile> tiles)
     {
-        int number = 0;
-        
         for (int i = 0; i < tiles.size(); i++)
         {
             Tile t = tiles.elementAt(i);
+            int number = 0;
 
             if (!t.equals(toCheck))  // don't compare with ourself!
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (t.getEdges()[j].equals(toCheck.getEdges()[k]))
+                            number++;
+                    }
                 }
+
+                System.out.println("Tile "+t.getID()+" shares "+number+" common edges with tile "+toCheck.getID());
             }
         }
 
-        return number;
+        return 0;
     }
 
     private boolean _debug;
