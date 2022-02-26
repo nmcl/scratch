ce6234f03b61522044d9a0ba27c23c801e7473ca
Mark Little <mlittle@redhat.com>
2022-02-26 18:46:04 +0000

    Update Solver.java

--- AdventOfCode/2020/day20/Solver.java
+++ AdventOfCode/2020/day20/Solver.java
@@ -10,6 +10,10 @@ public class Solver
     Vector<Tile> arrangement (Vector<Tile> tiles)
     {
         Vector<Tile> toReturn = new Vector<Tile>();
+        double dimension = Math.sqrt((double) tiles.size());
+
+        if (_debug)
+            System.out.println("Dimensions "+dimension+" by "+dimension);
 
         return toReturn;
     }
