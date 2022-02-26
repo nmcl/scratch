c9356a59ac04fe0959ab312b4b1995a03d20803c
Mark Little <mlittle@redhat.com>
2022-02-26 17:56:53 +0000

    Update Util.java

--- AdventOfCode/2020/day20/Util.java
+++ AdventOfCode/2020/day20/Util.java
@@ -10,7 +10,7 @@ public class Util
          */
 
         BufferedReader reader = null;
-        Vector<String> data = new Vector<String>();
+        Vector<Tile> data = new Vector<Tile>();
 
         try
         {
