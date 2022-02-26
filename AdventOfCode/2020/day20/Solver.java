f248c9d5a76ec269f286716cd805ebc9225980b4
Mark Little <mlittle@redhat.com>
2022-02-26 19:25:20 +0000

    Update Solver.java

--- AdventOfCode/2020/day20/Solver.java
+++ AdventOfCode/2020/day20/Solver.java
@@ -7,7 +7,7 @@ public class Solver
         _debug = debug;
     }
 
-    Vector<Tile> arrangement (Vector<Tile> tiles)
+    public Vector<Tile> arrangement (Vector<Tile> tiles)
     {
         Vector<Tile> toReturn = new Vector<Tile>();
         int dimension = (int) Math.sqrt((double) tiles.size());
@@ -15,7 +15,32 @@ public class Solver
         if (_debug)
             System.out.println("Dimensions "+dimension+" by "+dimension);
 
+        for (int i = 0; i < tiles.size(); i++)
+        {
+            Tile t = tiles.elementAt(i);
+        }
+
         return toReturn;
+    }
+
+    private int numberOfMatchingEdges (Tile toCheck, Vector<Tile> tiles)
+    {
+        int number = 0;
+        
+        for (int i = 0; i < tiles.size(); i++)
+        {
+            Tile t = tiles.elementAt(i);
+
+            if (!t.equals(toCheck))
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                }
+            }
+        }
+
+        return number;
     }
 
     private boolean _debug;
