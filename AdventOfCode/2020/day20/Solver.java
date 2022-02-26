a32deb677c4d461c7304ce8ae8aa9bcfc4840301
Mark Little <mlittle@redhat.com>
2022-02-26 18:43:41 +0000

    Update Solver.java

---
+++ AdventOfCode/2020/day20/Solver.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solver
+{
+    public Solver (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    Vector<Tile> arrangement (Vector<Tile> tiles)
+    {
+        Vector<Tile> toReturn = new Vector<Tile>();
+
+        return toReturn;
+    }
+
+    private boolean _debug;
+}
