5c05201e7b079435a324644a640933c3643d0f4d
Mark Little <mlittle@redhat.com>
2022-03-14 11:55:59 +0000

    Update TileData.java

--- AdventOfCode/2020/day20/TileData.java
+++ AdventOfCode/2020/day20/TileData.java
@@ -1,7 +1,7 @@
 public class TileData
 {
     public static final char HASH = '#';
-    public static final char SPACE = '.';
+    public static final char SPACE = '.';  // unoccupied
     
     public static final String TILE_ID = "Tile ";
     
