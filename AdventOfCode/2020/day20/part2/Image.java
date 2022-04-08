25c867ad64899961c87476d4a5dc745cf5edd521
Mark Little <mlittle@redhat.com>
2022-04-08 11:39:58 +0100

    Update Image.java

--- AdventOfCode/2020/day20/part2/Image.java
+++ AdventOfCode/2020/day20/part2/Image.java
@@ -1,6 +1,18 @@
-public class Image {
-    public Image(int x, int y) {
+public class Image
+{
+    public Image(int x, int y)
+    {
         _theWorld = new Tile[x][y];
+    }
+
+    public void placeTile (Tile theTile, int x, int y)
+    {
+        _theWorld[x][y] = theTile;
+    }
+
+    public Tile getTile (int x, int y)
+    {
+        return _theWorld[x][y];
     }
 
     private Tile[][] _theWorld;
