07c8df84c6cbfed3b14c39e975d80b2221116619
Mark Little <mlittle@redhat.com>
2022-04-19 19:14:55 +0100

    Update Image.java

--- AdventOfCode/2020/day20/part2/Image.java
+++ AdventOfCode/2020/day20/part2/Image.java
@@ -1,7 +1,25 @@
 public class Image
 {
+    /*
+     * 144 tiles in the example. Hard wire
+     * this for now but could make it dynamic.
+     */
+
+    public static final int MAX_X = 12;
+    public static final int MAX_Y = 12;
+
     public Image ()
     {
-        
+        _theWorld = new Tile[MAX_Y][MAX_X];
+
+        for (int i = 0; i < MAX_Y; i++)
+        {
+            for (int j = 0; j < MAX_X; j++)
+            {
+                _theWorld[i][j] = null;
+            }
+        }
     }
+
+    private Tile[][] _theWorld;
 }
