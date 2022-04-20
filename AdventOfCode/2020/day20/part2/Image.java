f770e341cd419db95e614099a16d10223ce2d8bc
Mark Little <mlittle@redhat.com>
2022-04-20 20:58:18 +0100

    Update Image.java

--- AdventOfCode/2020/day20/part2/Image.java
+++ AdventOfCode/2020/day20/part2/Image.java
@@ -42,6 +42,31 @@ public class Image
     {
         String str = "";
 
+        for (int i = 0; i < _theWorld.length; i++)
+        {
+            for (int j = 0; j < _theWorld[0].length; j++)
+            {
+                Tile t = _theWorld[i][j];
+                int index = 0;
+
+                if (t != null)
+                {
+                    int maxIndex = t.numberOfLines();
+
+                    while (index < maxIndex)
+                    {
+                        str += t.line(index);
+
+                        index++;
+                    }
+                }
+                else
+                    str += NO_TILE;
+
+                str += "\n";
+            }
+        }
+
         return str;
     }
 
