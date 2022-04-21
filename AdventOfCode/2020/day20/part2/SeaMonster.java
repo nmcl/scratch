fafeb4cf1ba443db20d3422dda0a6ccb2d12e461
Mark Little <mlittle@redhat.com>
2022-04-21 16:26:22 +0100

    Update SeaMonster.java

--- AdventOfCode/2020/day20/part2/SeaMonster.java
+++ AdventOfCode/2020/day20/part2/SeaMonster.java
@@ -11,6 +11,8 @@ public class SeaMonster
     public static final int HEIGHT = 3;
     public static final int WIDTH = 20;
 
+    public static final int COUNT = 15;
+
     private SeaMonster ()
     {
     }
