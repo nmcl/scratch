8a9ef4e1e5b8cbebb91daad7c91071e6c796d295
Mark Little <mlittle@redhat.com>
2022-04-21 16:02:22 +0100

    Create SeaMonster.java

---
+++ AdventOfCode/2020/day20/part2/SeaMonster.java
@@ -0,0 +1,17 @@
+public class SeaMonster
+{
+    /*
+     * Check for this pattern ...
+     * 
+     *                   # 
+     * #    ##    ##    ###
+     *  #  #  #  #  #  #   
+     */
+
+    public static final int HEIGHT = 3;
+    public static final int WIDTH = 20;
+
+    private SeaMonster ()
+    {
+    }
+}
