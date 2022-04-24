cf48d6d41c312e15be840eafab3b505d0051322a
Mark Little <mlittle@redhat.com>
2022-04-24 15:27:46 +0100

    Update Util.java

--- AdventOfCode/2020/day21/Util.java
+++ AdventOfCode/2020/day21/Util.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Util
 {
-    public static final String ALLERGEN_DELIMITER = "contains";
+    public static final String ALLERGEN_DELIMITER = "(contains";
 
     public static final Food[] loadRules (String inputFile, boolean debug)
     {
@@ -13,7 +13,6 @@ public class Util
 
         BufferedReader reader = null;
         Vector<Food> values = new Vector<Food>();
-        int maxRule = 0;
 
         try
         {
@@ -21,61 +20,11 @@ public class Util
 
             reader = new BufferedReader(new FileReader(inputFile));
          
-            while (((line = reader.readLine()) != null) && (line.length() > 0))
+            while (line = reader.readLine()) != null)
             {
-                int nameDelim = line.indexOf(RULE_NAME_DELIMITER);
-                String name = line.substring(0, nameDelim);
-
-                if (debug)
-                    System.out.println("Rule name: "+name);
-
-                int matchDelim = line.indexOf(MATCH);
-
-                if (matchDelim == -1)
-                {
-                    int orDelim = line.indexOf(OR_DELIMITER);  // there's only ever a maximum of one of these in a rule
-                    String dataOne = line.substring(nameDelim +1, ((orDelim == -1) ? line.length() : orDelim));
-                    String dataTwo = ((orDelim == -1) ? null : line.substring(orDelim +1));
-
-                    if (debug)
-                    {
-                        System.out.println("Rule one: "+dataOne);
-                        System.out.println("Rule two: "+dataTwo);
-                    }
-
-                    String[] dataOneList = dataOne.trim().split(RULE_DELIMITER);
-                    String[] dataTwoList = (dataTwo == null ? null : dataTwo.trim().split(RULE_DELIMITER));
-
-                    if (debug && (dataOneList != null))
-                    {
-                        for (int i = 0; i < dataOneList.length; i++)
-                            System.out.println("Rule one list: "+dataOneList[i]);
-                    }
-
-                    if (debug && (dataTwoList != null))
-                    {
-                        for (int i = 0; i < dataTwoList.length; i++)
-                            System.out.println("Rule two list: "+dataTwoList[i]);
-                    }
-
-                    Rule r = new Rule(name, dataOneList, dataTwoList);
-
-                    if (r.getNumber() > maxRule)
-                        maxRule = r.getNumber();
-
-                    values.add(r);
-                }
-                else
-                {
-                    // match is always one character
-
-                    Rule r = new Rule(name, line.charAt(matchDelim +1));
-
-                    if (r.getNumber() > maxRule)
-                        maxRule = r.getNumber();
-
-                    values.add(r);
-                }
+                String[] foodData = line.split(" ");
+                
+                System.out.println()
             }
         }
         catch (Throwable ex)
@@ -93,59 +42,7 @@ public class Util
             }
         }
 
-        Rule[] ordered = new Rule[maxRule +1];
-
-        for (int i = 0; i < values.size(); i++)
-        {
-            Rule r = values.elementAt(i);
-            
-            ordered[r.getNumber()] = r;
-        }
-
-        return ordered;
-    }
-
-    public static final Message[] loadMessages (String inputFile, boolean debug)
-    {
-        /*
-         * Open the data file and read it in.
-         */
-
-        BufferedReader reader = null;
-        Vector<Message> values = new Vector<Message>();
-
-        try
-        {
-            String line = null;
-
-            reader = new BufferedReader(new FileReader(inputFile));
-            
-            while (((line = reader.readLine()) != null) && (line.length() > 0))
-            {
-                // loop until blank line.
-            }
-
-            while ((line = reader.readLine()) != null)
-            {
-                values.add(new Message(line));
-            }
-        }
-        catch (Throwable ex)
-        {
-            ex.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                reader.close();
-            }
-            catch (Throwable ex)
-            {
-            }
-        }
-
-        return values.toArray(new Message[values.size()]);
+        return values;
     }
 
     private Util ()
