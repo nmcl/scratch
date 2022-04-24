9cc2f25004a76b9052b803157708900835f20ad0
Mark Little <mlittle@redhat.com>
2022-04-24 16:04:26 +0100

    Update Food.java

--- AdventOfCode/2020/day21/Food.java
+++ AdventOfCode/2020/day21/Food.java
@@ -4,7 +4,26 @@ public class Food
 {
     public Food ()
     {
+        _allergens = new Vector<String>();
+        _ingredients = new Vector<String>();
+    }
 
+    public final void addIngredient (String data)
+    {
+        _ingredients.add(data);
+    }
+
+    public final void addAllergen (String data)
+    {
+        _allergens.add(data);
+    }
+
+    @Override
+    public String toString ()
+    {
+        String str = "";
+
+        return str;
     }
 
     Vector<String> _allergens;
