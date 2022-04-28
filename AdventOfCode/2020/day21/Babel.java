09f2ffac4c9077cbda03925324b126469900bc8c
Mark Little <mlittle@redhat.com>
2022-04-28 12:52:48 +0100

    Update Babel.java

--- AdventOfCode/2020/day21/Babel.java
+++ AdventOfCode/2020/day21/Babel.java
@@ -12,6 +12,25 @@ public class Translator
         for (int i = 0; i < foods.size(); i++)
         {
             Food toCheck = foods.elementAt(i);
+            Vector<String> ingredients = toCheck.getIngredients();
+
+            for (int j = 0; j < ingredients.size(); j++)
+            {
+                String ingredient = ingredients.elementAt(j);
+
+                for (int k = 0; k < foods.size(); k++)
+                {
+                    Food compare = foods.elementAt(k);
+
+                    if (!toCheck.equals(compare))
+                    {
+                        if (compare.getIngredients().contains(ingredient))
+                        {
+                            System.out.println("Ingredient "+ingredient+" found in\n"+compare);
+                        }
+                    }
+                }
+            }
         }
     }
     
