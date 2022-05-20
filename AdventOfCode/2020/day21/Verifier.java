d1a2394d0c1dac2a426087ca9e719a63d58acb76
Mark Little <mlittle@redhat.com>
2022-05-20 13:23:01 +0100

    Update Verifier.java

--- AdventOfCode/2020/day21/Verifier.java
+++ AdventOfCode/2020/day21/Verifier.java
@@ -14,7 +14,7 @@ public class Verifier
         Vector<Food> foods = Util.loadRules(EXAMPLE_FILE, _debug);
 
         if (_debug)
-            System.out.println("Got back:\n"+foods+" and "+food.size());
+            System.out.println("Got back:\n"+foods+" and "+foods.size());
 
         Babel fish = new Babel(_debug);
 
