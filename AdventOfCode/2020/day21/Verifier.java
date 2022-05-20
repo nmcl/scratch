c9e03f44842739845431d5325bfef6c6fc97e2a3
Mark Little <mlittle@redhat.com>
2022-05-20 13:50:00 +0100

    Update Verifier.java

--- AdventOfCode/2020/day21/Verifier.java
+++ AdventOfCode/2020/day21/Verifier.java
@@ -4,6 +4,8 @@ public class Verifier
 {
     public static final String EXAMPLE_FILE = "example.txt";
 
+    public static final String[] ALLERGEN_FREE = { "kfcds", "nhms", "sbzzf", "trh" }
+
     public Verifier (boolean debug)
     {
         _debug = debug;
