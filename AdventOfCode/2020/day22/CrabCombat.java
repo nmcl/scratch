24bf015597666ffe994bdb0ba539ea59d82df6b9
Mark Little <markclittle@gmail.com>
2022-06-06 20:07:16 +0100

    Update CrabCombat.java

---
+++ AdventOfCode/2020/day22/CrabCombat.java
@@ -0,0 +1,23 @@
+public class CrabCombat
+{
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
