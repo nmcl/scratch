dc0d42a6b209545bd787f618e2e8fadf0d6ad2ca
Mark Little <mlittle@redhat.com>
2022-06-07 11:30:15 +0100

    Update Deck.java

--- AdventOfCode/2020/day22/Deck.java
+++ AdventOfCode/2020/day22/Deck.java
@@ -1,7 +1,25 @@
+import java.util.*;
+
 public class Deck
 {
     public Deck ()
     {
-        
+        _theDeck = new ArrayDeque<Integer>();
     }
+
+    @Override
+    public String toString ()
+    {
+        String str = "";
+        Iterator<Integer> iter = _theDeck.iterator();
+
+        while (iter.hasNext())
+        {
+            str += iter.next();
+        }
+
+        return str;
+    }
+
+    private ArrayDeque<Integer> _theDeck;
 }
