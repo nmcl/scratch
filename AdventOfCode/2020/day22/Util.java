0c7525fc40be1d93bc7db197c9f6845dc61b7840
Mark Little <mlittle@redhat.com>
2022-06-08 15:15:10 +0100

    Update Util.java

--- AdventOfCode/2020/day22/Util.java
+++ AdventOfCode/2020/day22/Util.java
@@ -13,12 +13,19 @@ public class Util
 
         BufferedReader reader = null;
         Deck[] decks = new Deck[2];
+        int player = 0;
 
         try
         {
             String line = null;
 
             reader = new BufferedReader(new FileReader(inputFile));
+
+            line = reader.readLine();
+
+            if (line.startsWith(PLAYER_DESIGNATOR))
+            {
+            }
         }
         catch (Throwable ex)
         {
