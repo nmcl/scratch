d661b1b98ec2602171fdd2efcaacad0fccc3db98
Mark Little <markclittle@gmail.com>
2022-06-09 19:45:42 +0100

    Update Game.java

--- AdventOfCode/2020/day22/Game.java
+++ AdventOfCode/2020/day22/Game.java
@@ -11,26 +11,37 @@ public class Game
 
         while (!decks[0].empty() && !decks[1].empty())
         {
-            System.out.println("-- Round "+round+" --");
+            System.out.println("\n-- Round "+round+" --");
             System.out.println(decks[0]);
             System.out.println(decks[1]);
 
             int playerOneCard = decks[0].draw();
             int playerTwoCard = decks[1].draw();
 
+            System.out.println("Player 1 plays: "+playerOneCard);
+            System.out.println("Player 2 plays: "+playerTwoCard);
+
             if (playerOneCard > playerTwoCard)
             {
+                System.out.println("Player 1 wins the round!");
+
                 decks[0].addToBottom(playerOneCard);
                 decks[0].addToBottom(playerTwoCard);
             }
             else
             {
-                decks[0].addToBottom(playerTwoCard);
-                decks[0].addToBottom(playerOneCard);
+                System.out.println("Player 2 wins the round!");
+
+                decks[1].addToBottom(playerTwoCard);
+                decks[1].addToBottom(playerOneCard);
             }
 
             round++;
         }
+
+        System.out.println("\n== Post-game results ==");
+        System.out.println(decks[0]);
+        System.out.println(decks[1]);
     }
 
     private boolean _debug;
