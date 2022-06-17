a4e13f95c67fe4e3113af729d5456bca62f1edcb
Mark Little <mlittle@redhat.com>
2022-06-17 10:37:50 +0100

    Update Round.java

---
+++ AdventOfCode/2020/day22/part2/Round.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Round
+{
+    public Round (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private Vector<Deck> _rounds;
+    private boolean _debug;
+}
