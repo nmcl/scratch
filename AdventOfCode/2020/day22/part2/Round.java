4fcee09a6cb414de196bfd5c7c48f00aeced766c
Mark Little <mlittle@redhat.com>
2022-06-20 14:04:50 +0100

    Update Round.java

--- AdventOfCode/2020/day22/part2/Round.java
+++ AdventOfCode/2020/day22/part2/Round.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class Round
 {
-    public Round (boolean debug)
+    public Round (int player, boolean debug)
     {
+        _playerId = player;
         _debug = debug;
     }
 
+    private int _playerId;
     private Vector<Deck> _rounds;
     private boolean _debug;
 }
