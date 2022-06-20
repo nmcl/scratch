7aa2b972a30e98489d0a672a3f87fd4ee13809a2
Mark Little <mlittle@redhat.com>
2022-06-20 14:22:05 +0100

    Update Round.java

--- AdventOfCode/2020/day22/part2/Round.java
+++ AdventOfCode/2020/day22/part2/Round.java
@@ -8,6 +8,16 @@ public class Round
         _debug = debug;
     }
 
+    public final void addRound (int round, Deck d)
+    {
+        _rounds.add(round, d);
+    }
+
+    public final boolean identicalRound (Deck d)
+    {
+        return _rounds.contains(d);
+    }
+
     private int _playerId;
     private Vector<Deck> _rounds;
     private boolean _debug;
