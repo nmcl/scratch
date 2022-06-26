fb19399f352aaa5b44cef77cfa6c01a5913802ae
Mark Little <markclittle@gmail.com>
2022-06-26 13:56:07 +0100

    Create CrabCups.java

---
+++ AdventOfCode/2020/day23/CrabCups.java
@@ -0,0 +1,25 @@
+public class CrabCups
+{
+    public static final String INPUT = "523764819";
+
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-debug] [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
