0d9ab7ab26ff1deccbe78c2e6d8a5b14b221b21e
Mark Little <markclittle@gmail.com>
2022-06-26 13:56:12 +0100

    Create Verifier.java

---
+++ AdventOfCode/2020/day23/Verifier.java
@@ -0,0 +1,11 @@
+public class Verifier
+{
+    public static final String EXAMPLE = "389125467";
+
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private boolean _debug;
+}
