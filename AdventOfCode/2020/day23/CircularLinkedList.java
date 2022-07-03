ff44854e741ad20264dabea693bdf6ee947ed4cb
Mark Little <mlittle@redhat.com>
2022-07-03 17:15:05 +0100

    Update CircularLinkedList.java

--- AdventOfCode/2020/day23/CircularLinkedList.java
+++ AdventOfCode/2020/day23/CircularLinkedList.java
@@ -2,7 +2,19 @@ public class CircularLinkedList
 {
     public CircularLinkedList ()
     {
+    }
 
+    public void add (int value)
+    {
+        Node n = new Node(value);
+        
+        if (_head == null)
+            _head = n;
+        else
+            _tail.setNext(n);
+
+        _tail = n;
+        _tail.setNext(_head);
     }
 
     private Node _head = null;
