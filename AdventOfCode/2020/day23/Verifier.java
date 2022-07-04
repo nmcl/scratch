da32a78a1f6fafaacc92796a38bf8cf72f44a5b9
Mark Little <mlittle@redhat.com>
2022-07-04 16:38:35 +0100

    Update Verifier.java

--- AdventOfCode/2020/day23/Verifier.java
+++ AdventOfCode/2020/day23/Verifier.java
@@ -1,6 +1,8 @@
 public class Verifier
 {
     public static final String EXAMPLE = "389125467";
+    public static final String EXAMPLE_RESULT_10 = "92658374";
+    public static final String EXAMPLE_RESULT_100 = "67384529";
 
     public Verifier (boolean debug)
     {
@@ -11,7 +13,19 @@ public class Verifier
     {
         Game theGame = new Game(_debug);
 
-        theGame.play(EXAMPLE, 10);
+        String result = theGame.play(EXAMPLE, 10);
+
+        if (EXAMPLE_RESULT_10.equals(result))
+        {
+            result = theGame.play(EXAMPLE, 100);
+
+            if (EXAMPLE_RESULT_100.equals(result))
+                return true;
+            else
+                System.out.println("Wrong result after 10 rounds: "+result);
+        }
+        else
+            System.out.println("Wrong result after 10 rounds: "+result);
 
         return false;
     }
