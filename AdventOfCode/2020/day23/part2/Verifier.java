8a706831adc42f0c002c8b022c51872395608d52
Mark Little <mlittle@redhat.com>
2022-07-13 11:24:55 +0100

    Update Verifier.java

--- AdventOfCode/2020/day23/part2/Verifier.java
+++ AdventOfCode/2020/day23/part2/Verifier.java
@@ -1,8 +1,7 @@
 public class Verifier
 {
     public static final String EXAMPLE = "389125467";
-    public static final String EXAMPLE_RESULT_CUP_1 = "934001";
-    public static final String EXAMPLE_RESULT_CUP_2 = "159792";
+    public static final String EXAMPLE_RESULT = "149245887792";
 
     public Verifier (boolean debug)
     {
@@ -12,20 +11,12 @@ public class Verifier
     public final boolean verify ()
     {
         Game theGame = new Game(_debug);
+        String result = theGame.play(EXAMPLE, 1000000);
 
-        String result = theGame.play(EXAMPLE, 10);
+        if (EXAMPLE_RESULT.equals(result))
+            return true;
 
-        if (EXAMPLE_RESULT_10.equals(result))
-        {
-            result = theGame.play(EXAMPLE, 100);
-
-            if (EXAMPLE_RESULT_100.equals(result))
-                return true;
-            else
-                System.out.println("Wrong result after 10 rounds: "+result);
-        }
-        else
-            System.out.println("Wrong result after 10 rounds: "+result);
+        System.out.println("Wrong result after 1000000 rounds: "+result);
 
         return false;
     }
