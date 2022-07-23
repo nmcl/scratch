e9dad271db9d71faffdb1728e799d641497d729e
Mark Little <markclittle@gmail.com>
2022-07-23 18:19:39 +0100

    Update Directions.java

--- AdventOfCode/2020/day24/Directions.java
+++ AdventOfCode/2020/day24/Directions.java
@@ -7,6 +7,38 @@ public class Directions
     public static final String NORTH_WEST = "nw";
     public static final String NORTH_EAST = "ne";
 
+    public final static Coordinate getCoordinate (String direction, Coordinate current)
+    {
+        switch (direction)
+        {
+            case EAST:
+            {
+                return new Coordinate(current.getX()+2, current.getY());
+            }
+            case SOUTH_EAST:
+            {
+                return new Coordinate(current.getX()+1, current.getY()-1);
+            }
+            case SOUTH_WEST:
+            {
+                return new Coordinate(current.getX()-1, current.getY()-1);
+            }
+            case WEST:
+            {
+                return new Coordinate(current.getX()-2, current.getY());
+            }
+            case NORTH_WEST:
+            {
+                return new Coordinate(current.getX()-1, current.getY()+1);
+            }
+            case NORTH_EAST:
+            default:
+            {
+                return new Coordinate(current.getX()+1, current.getY()+1);
+            }
+        }
+    }
+
     private Directions ()
     {
     }
