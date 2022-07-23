c53c7e61b058006e527ca802fbe76197670ebcf8
Mark Little <markclittle@gmail.com>
2022-07-23 18:07:41 +0100

    Update Directions.java

---
+++ AdventOfCode/2020/day24/Directions.java
@@ -0,0 +1,13 @@
+public class Directions
+{
+    public static final String EAST = "e";
+    public static final String SOUTH_EAST = "se";
+    public static final String SOUTH_WEST = "sw";
+    public static final String WEST = "w";
+    public static final String NORTH_WEST = "nw";
+    public static final String NORTH_EAST = "ne";
+
+    private Directions ()
+    {
+    }
+}
