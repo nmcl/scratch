a8eb52c9dca6c06a7569a9c3115ed5c1cadd7e8b
Mark Little <markclittle@gmail.com>
2022-07-24 17:41:08 +0100

    Update Verifier.java

--- AdventOfCode/2020/day24/Verifier.java
+++ AdventOfCode/2020/day24/Verifier.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
+    public static final int EXAMPLE_BLACK_TILES = 10;
 
     public Verifier (boolean debug)
     {
@@ -12,7 +13,14 @@ public class Verifier
     public boolean verify ()
     {
         Vector<String> lines = Util.readLines(EXAMPLE_DATA);
-        
+        Renovation rv = new Renovation(_debug);
+        Vector<Coordinate> blackTiles = rv.getBlackTiles(lines);
+
+        if (blackTiles.size() == EXAMPLE_BLACK_TILES)
+            return true;
+        else
+            System.out.println("Wrong number of black tiles: "+blackTiles.size());
+
         return false;
     }
 
