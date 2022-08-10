0378c3f8725f0afdda4344a2de2354946a8eb640
Mark Little <mlittle@redhat.com>
2022-08-10 17:02:34 -0400

    Create Verifier.java

---
+++ AdventOfCode/2020/day25/Verifier.java
@@ -0,0 +1,12 @@
+public class Verifier
+{
+    public static final long EXAMPLE_CARD_PUBLIC_KEY = 5764801;
+    public static final long EXAMPLE_DOOR_PUBLIC_KEY = 17807724;
+    
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    private boolean _debug;
+}
