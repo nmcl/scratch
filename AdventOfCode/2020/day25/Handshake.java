8408df61808ec70dca724e4b1a0747efec3279ae
Mark Little <mlittle@redhat.com>
2022-08-18 06:43:23 -0400

    Update Handshake.java

--- AdventOfCode/2020/day25/Handshake.java
+++ AdventOfCode/2020/day25/Handshake.java
@@ -2,6 +2,7 @@ public class Handshake
 {
     public static final long MAGIC_NUMBER = 20201227;
     public static final int SUBJECT_NUMBER = 7;
+    public static final int START_NUMBER = 1;
 
     private Handshake ()
     {
