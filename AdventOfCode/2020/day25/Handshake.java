6a109a38cdc19d681060abb43b54575a8f022d29
Mark Little <mlittle@redhat.com>
2022-08-19 17:13:23 -0400

    Update Handshake.java

--- AdventOfCode/2020/day25/Handshake.java
+++ AdventOfCode/2020/day25/Handshake.java
@@ -4,7 +4,10 @@ public class Handshake
     public static final int SUBJECT_NUMBER = 7;
     public static final int START_NUMBER = 1;
 
-    private Handshake ()
+    public Handshake (boolean debug)
     {
+        _debug = debug;
     }
+
+    private boolean _debug;
 }
