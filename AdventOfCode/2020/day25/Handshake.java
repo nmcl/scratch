695d93219c56bcd891d879020ac72e4e1ff2fa92
Mark Little <markclittle@gmail.com>
2022-09-03 16:54:10 +0100

    Update Handshake.java

--- AdventOfCode/2020/day25/Handshake.java
+++ AdventOfCode/2020/day25/Handshake.java
@@ -1,19 +1,42 @@
 public class Handshake
 {
-    public static final long MAGIC_NUMBER = 20201227;
-    public static final int SUBJECT_NUMBER = 7;
-    public static final int START_NUMBER = 1;
+    public static final long MAGIC_NUMBER = 20201227L;
+    public static final long SUBJECT_NUMBER = 7L;
+    public static final long START_NUMBER = 1L;
 
     public Handshake (boolean debug)
     {
         _debug = debug;
     }
     
-    public boolean encryptionKey (int cardPublicKey, int doorPublicKey)
+    public long encryptionKey (long cardPublicKey, long doorPublicKey)
     {
-        // fill in the blanks...
+        long count = loopSize(doorPublicKey);
+        long curr = START_NUMBER;
 
-        return false;
+        for (int i = 0; i < count; i++)
+        {
+            curr *= cardPublicKey;
+            curr %= MAGIC_NUMBER;
+        }
+
+        return curr;
+    }
+
+    private long loopSize (long publicKey)
+    {
+        long curr = START_NUMBER;
+        long iter = 0;
+
+        while (value != publicKey)
+        {
+            curr *= SUBJECT_NUMBER;
+            curr %= MAGIC_NUMBER;
+
+            iter++;
+        }
+
+        return iter;
     }
 
     private boolean _debug;
