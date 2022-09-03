c3e3863e4c84af15608cca06b2773337384ad41f
Mark Little <markclittle@gmail.com>
2022-09-03 16:55:26 +0100

    Update ComboBreaker.java

--- AdventOfCode/2020/day25/ComboBreaker.java
+++ AdventOfCode/2020/day25/ComboBreaker.java
@@ -4,7 +4,7 @@ public class ComboBreaker
 {
     public static final String DATA_FILE = "input.txt";
 
-    public static void main (String[] args)
+    public static void main (String[] args) throws Exception
     {
         boolean debug = false;
 
