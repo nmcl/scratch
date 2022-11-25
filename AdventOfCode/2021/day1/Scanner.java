8153ed9a278ca39d5426e00ba8207b4a69514f74
Mark Little <mlittle@redhat.com>
2022-11-25 14:24:46 +0000

    Update Scanner.java

--- AdventOfCode/2021/day1/Scanner.java
+++ AdventOfCode/2021/day1/Scanner.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Scanner
 {
     public Scanner (boolean debug)
@@ -7,7 +9,32 @@ public class Scanner
 
     public int increasingDepth (String dataFile)
     {
-        return 0;
+        Vector<Integer> depths = Util.loadData(dataFile, _debug);
+        int count = 0;
+        int previousDepth = depths.elementAt(0);
+
+        if (_debug)
+            System.out.println("Initial depth: "+previousDepth);
+
+        for (int i = 1; i < depths.size(); i++)
+        {
+            if (depths.elementAt(i) > previousDepth)
+            {
+                if (_debug)
+                    System.out.println("New depth "+depths.elementAt(i)+" is greater than "+previousDepth);
+
+                count++;
+            }
+            else
+            {
+                if (_debug)
+                    System.out.println("New depth "+depths.elementAt(i)+" is less than "+previousDepth);
+            }
+
+            previousDepth = depths.elementAt(i);
+        }
+
+        return count;
     }
 
     private boolean _debug;
