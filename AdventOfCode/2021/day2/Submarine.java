6d35d05109e8e88fa426e502e27d5dbd61df17d2
Mark Little <mlittle@redhat.com>
2022-11-25 15:39:16 +0000

    Update Submarine.java

--- AdventOfCode/2021/day2/Submarine.java
+++ AdventOfCode/2021/day2/Submarine.java
@@ -1,9 +1,16 @@
+import java.util.*;
+
 public class Submarine
 {
     public Submarine (boolean debug)
     {
         _position = new ThreeDPoint(0, 0, 0);
         _debug = debug;
+    }
+
+    public void move (String dataFile)
+    {
+        Vector<Command> cmds = Util.loadData(dataFile, _debug);
     }
 
     private ThreeDPoint _position;
