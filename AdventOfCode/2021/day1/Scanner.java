f75c6e6927842aebb3927ca7c636c7bfaee4f0ed
Mark Little <mlittle@redhat.com>
2022-11-25 14:18:17 +0000

    Update Scanner.java

--- AdventOfCode/2021/day1/Scanner.java
+++ AdventOfCode/2021/day1/Scanner.java
@@ -1,7 +1,14 @@
 public class Scanner
 {
-    public Scanner ()
+    public Scanner (boolean debug)
     {
-        
+        _debug = debug;
     }
+
+    public int increasingDepth (String dataFile)
+    {
+        return 0;
+    }
+
+    private boolean _debug;
 }
