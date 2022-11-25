ac73562a9d208a435372b5a62c645e2e46f2e01a
Mark Little <mlittle@redhat.com>
2022-11-25 15:46:22 +0000

    Update Submarine.java

--- AdventOfCode/2021/day2/Submarine.java
+++ AdventOfCode/2021/day2/Submarine.java
@@ -8,9 +8,39 @@ public class Submarine
         _debug = debug;
     }
 
-    public void move (String dataFile)
+    public ThreeDPoint move (String dataFile)
     {
         Vector<Command> cmds = Util.loadData(dataFile, _debug);
+
+        for (int i = 0; i < cmds.size(); i++)
+        {
+            Command theCommand = cmds.elementAt(i);
+
+            switch (theCommand.cmd())
+            {
+                case Command.FORWARD:
+                {
+                    _position = new ThreeDPoint(_position.getX() + theCommand.amount(), _position.getY(), _position.getZ());
+                }
+                break;
+                case Command.DOWN:
+                {
+                    _position = new ThreeDPoint(_position.getX(), _position.getY(), _position.getZ() + theCommand.amount());
+                }
+                break;
+                case Command.UP:
+                {
+                    _position = new ThreeDPoint(_position.getX(), _position.getY(), _position.getZ() - theCommand.amount());
+                }
+                break;
+                default:
+                {
+                    System.out.println("Unknown command: "+theCommand.cmd());
+                }
+            }
+        }
+
+        return _position;
     }
 
     private ThreeDPoint _position;
