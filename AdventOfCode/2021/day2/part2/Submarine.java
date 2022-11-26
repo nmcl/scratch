3c02c7b336287534e62a08a1f21fcc173bc57ea2
Mark Little <markclittle@gmail.com>
2022-11-26 17:22:33 +0000

    Update Submarine.java

--- AdventOfCode/2021/day2/part2/Submarine.java
+++ AdventOfCode/2021/day2/part2/Submarine.java
@@ -16,21 +16,24 @@ public class Submarine
         {
             Command theCommand = cmds.elementAt(i);
 
+            if (_debug)
+                System.out.println("Submarine command: "+theCommand);
+
             switch (theCommand.cmd())
             {
                 case Command.FORWARD:
                 {
-                    _position = new Course(_position.getPosition() + theCommand.amount(), _position.getAim(), _position.getDepth());
+                    _position = new Course(_position.getPosition() + theCommand.amount(), _position.getAim(), _position.getDepth() * theCommand.amount());
                 }
                 break;
                 case Command.DOWN:
                 {
-                    _position = new Course(_position.getPosition(), _position.getAim(), _position.getDepth() + theCommand.amount());
+                    _position = new Course(_position.getPosition(), _position.getAim() + theCommand.amount(), _position.getDepth());
                 }
                 break;
                 case Command.UP:
                 {
-                    _position = new Course(_position.getPosition(), _position.getAim(), _position.getDepth() - theCommand.amount());
+                    _position = new Course(_position.getPosition(), _position.getAim() - theCommand.amount(), _position.getDepth());
                 }
                 break;
                 default:
@@ -38,6 +41,9 @@ public class Submarine
                     System.out.println("Unknown command: "+theCommand.cmd());
                 }
             }
+
+            if (_debug)
+                System.out.println("Submarine course: "+_position);
         }
 
         return _position;
