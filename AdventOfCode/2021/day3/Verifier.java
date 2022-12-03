9de5dc08bfddabcabdaeb77f90a05373e8a5512b
Mark Little <markclittle@gmail.com>
2022-12-03 13:56:06 +0000

    Update Verifier.java

--- AdventOfCode/2021/day3/Verifier.java
+++ AdventOfCode/2021/day3/Verifier.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
+    public static final String EXAMPLE_GAMMA = "10110";
+    public static final String EXAMPLE_EPSILON = "01001";
 
     public Verifier (boolean debug)
     {
@@ -14,6 +16,9 @@ public class Verifier
         Vector<String> data = Util.loadData(EXAMPLE_DATA, _debug);
         String gamma = Gamma.getGamma(data);
         String epsilon = Epsilon.getEpsilon(data);
+
+        System.out.println("Gamma: "+gamma);
+        System.out.println("Epsilon: "+epsilon);
         
         return false;
     }
