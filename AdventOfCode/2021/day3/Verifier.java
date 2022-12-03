ea7f86180a97c7438aed81df77dc6a4b8a528980
Mark Little <markclittle@gmail.com>
2022-12-03 14:04:11 +0000

    Update Verifier.java

--- AdventOfCode/2021/day3/Verifier.java
+++ AdventOfCode/2021/day3/Verifier.java
@@ -5,6 +5,7 @@ public class Verifier
     public static final String EXAMPLE_DATA = "example.txt";
     public static final String EXAMPLE_GAMMA = "10110";
     public static final String EXAMPLE_EPSILON = "01001";
+    public static final int EXAMPLE_POWER_CONSUMPTION = 198;
 
     public Verifier (boolean debug)
     {
@@ -17,9 +18,23 @@ public class Verifier
         String gamma = Gamma.getGamma(data);
         String epsilon = Epsilon.getEpsilon(data);
 
-        System.out.println("Gamma: "+gamma);
-        System.out.println("Epsilon: "+epsilon);
-        
+        if (EXAMPLE_GAMMA.equals(gamma))
+        {
+            if (EXAMPLE_EPSILON.equals(epsilon))
+            {
+                int gammaRate = Integer.parseInt(gamma, 2);
+                int epsilonRate = Integer.parseInt(epsilon, 2);
+                int powerConsumption = gammaRate * epsilonRate;
+
+                if (powerConsumption == EXAMPLE_POWER_CONSUMPTION)
+                    return true;
+            }
+            else
+                System.out.println("Failed. Wrong epsilon: "+epsilon);
+        }
+        else
+            System.out.println("Failed. Wrong gamma: "+gamma);
+
         return false;
     }
 
