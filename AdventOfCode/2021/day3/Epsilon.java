891899c9773cbaf93582749a76bddbb01f2298aa
Mark Little <markclittle@gmail.com>
2022-12-03 13:35:47 +0000

    Update Epsilon.java

--- AdventOfCode/2021/day3/Epsilon.java
+++ AdventOfCode/2021/day3/Epsilon.java
@@ -1,4 +1,32 @@
+import java.util.*;
+
 public class Epsilon
 {
-    
+    public static final String getEpsilon (Vector<String> data)
+    {
+        String theGamma = "";
+        int index = 0;
+        int numberOfBits = data.elementAt(0).length();
+
+        for (int i = 0; i < numberOfBits; i++)
+        {
+            int ones = 0;
+            int zeros = 0;
+
+            for (int j = 0; j < data.size(); j++)
+            {
+                if (data.elementAt(j).charAt(index) == '1')
+                    ones++;
+                else
+                    zeros++;
+            }
+
+            if (ones < zeros)
+                theGamma += "1";
+            else
+                theGamma += "0";
+        }
+
+        return theGamma;
+    }
 }
