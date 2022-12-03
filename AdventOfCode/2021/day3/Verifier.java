b24d4a2f2c0a7f71d6024a4af73f447e32b46284
Mark Little <markclittle@gmail.com>
2022-12-03 13:50:16 +0000

    Update Verifier.java

--- AdventOfCode/2021/day3/Verifier.java
+++ AdventOfCode/2021/day3/Verifier.java
@@ -1,8 +1,21 @@
+import java.util.*;
+
 public class Verifier
 {
+    public static final String EXAMPLE_DATA = "example.txt";
+
     public Verifier (boolean debug)
     {
         _debug = debug;
+    }
+
+    public boolean verify ()
+    {
+        Vector<String> data = Util.loadData(EXAMPLE_DATA, _debug);
+        String gamma = Gamma.getGamma(data);
+        String epsilon = Epsilon.getEpsilon(data);
+        
+        return false;
     }
 
     private boolean _debug;
