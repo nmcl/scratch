b4fa37313152b548abf97a464bd2fc31d8604962
Mark Little <markclittle@gmail.com>
2022-12-03 13:35:50 +0000

    Update Gamma.java

--- AdventOfCode/2021/day3/Gamma.java
+++ AdventOfCode/2021/day3/Gamma.java
@@ -4,6 +4,29 @@ public class Gamma
 {
     public static final String getGamma (Vector<String> data)
     {
-        return null;
+        String theGamma = "";
+        int index = 0;
+        int numberOfBits = data.elementAt(0).length();
+
+        for (int i = 0; i < numberOfBits; i++)
+        {
+            int ones = 0;
+            int zeros = 0;
+
+            for (int j = 0; j < data.size(); j++)
+            {
+                if (data.elementAt(j).charAt(index) == '1')
+                    ones++;
+                else
+                    zeros++;
+            }
+
+            if (ones > zeros)
+                theGamma += "1";
+            else
+                theGamma += "0";
+        }
+
+        return theGamma;
     }
 }
