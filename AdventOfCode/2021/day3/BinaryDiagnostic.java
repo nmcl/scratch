8dc6d3e6bb51206f57123649ef76823e2782ec37
Mark Little <markclittle@gmail.com>
2022-12-03 13:08:38 +0000

    Update BinaryDiagnostic.java

---
+++ AdventOfCode/2021/day3/BinaryDiagnostic.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class BinaryDiagnostic
+{
+    public static final String DATA_FILE = "input.txt";
+
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+
+        if (verify)
+        {
+            Verifier v = new Verifier(debug);
+            
+            if (v.verify())
+                System.out.println("Verified ok.");
+            else
+                System.out.println("Verify failed.");
+
+            System.exit(0);
+        }
+    }
+}
