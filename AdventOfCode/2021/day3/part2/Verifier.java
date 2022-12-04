c8d79b818248d4cfc26f8ae70000aefbcd0f73a2
Mark Little <markclittle@gmail.com>
2022-12-04 16:14:48 +0000

    Update Verifier.java

--- AdventOfCode/2021/day3/part2/Verifier.java
+++ AdventOfCode/2021/day3/part2/Verifier.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
-    public static final String EXAMPLE_GAMMA = "10110";
-    public static final String EXAMPLE_EPSILON = "01001";
-    public static final int EXAMPLE_POWER_CONSUMPTION = 198;
+    public static final String EXAMPLE_OXYGEN = "10111";
+    public static final String EXAMPLE_CO2 = "01010";
+    public static final int EXAMPLE_LIFESUPPORT_RATING = 230;
 
     public Verifier (boolean debug)
     {
@@ -15,25 +15,25 @@ public class Verifier
     public boolean verify ()
     {
         Vector<String> data = Util.loadData(EXAMPLE_DATA, _debug);
-        String gamma = Gamma.getGamma(data);
-        String epsilon = Epsilon.getEpsilon(data);
+        String oxygen = Oxygen.getOxygen(data, 0);
+        String co2 = CO2.getCO2(data, 0);
 
-        if (EXAMPLE_GAMMA.equals(gamma))
+        if (EXAMPLE_OXYGEN.equals(oxygen))
         {
-            if (EXAMPLE_EPSILON.equals(epsilon))
+            if (EXAMPLE_CO2.equals(co2))
             {
-                int gammaRate = Integer.parseInt(gamma, 2);
-                int epsilonRate = Integer.parseInt(epsilon, 2);
-                int powerConsumption = gammaRate * epsilonRate;
+                int oxygenRating = Integer.parseInt(oxygen, 2);
+                int co2Rating = Integer.parseInt(co2, 2);
+                int lifesupportRating = oxygenRating * co2Rating;
 
-                if (powerConsumption == EXAMPLE_POWER_CONSUMPTION)
+                if (lifesupportRating == EXAMPLE_LIFESUPPORT_RATING)
                     return true;
             }
             else
-                System.out.println("Failed. Wrong epsilon: "+epsilon);
+                System.out.println("Failed. Wrong CO2: "+co2);
         }
         else
-            System.out.println("Failed. Wrong gamma: "+gamma);
+            System.out.println("Failed. Wrong Oxygen: "+oxygen);
 
         return false;
     }
