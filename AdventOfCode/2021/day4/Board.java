91c24c3180ae38b7af6c126499ed96dca6afdc56
Mark Little <markclittle@gmail.com>
2022-12-20 15:06:53 +0000

    Update Board.java

--- AdventOfCode/2021/day4/Board.java
+++ AdventOfCode/2021/day4/Board.java
@@ -3,15 +3,29 @@ public class Board
     public static final int MAX_X = 5;
     public static final int MAX_Y = 5;
 
-    public Board ()
+    public Board (int[][] data)
     {
         _theBoard = new Cell[MAX_X][MAX_Y];
 
         for (int i = 0; i < MAX_X; i++)
         {
             for (int j = 0; j < MAX_Y; j++)
-                _theBoard[i][j] = null;
+                _theBoard[i][j] = new Cell(data[i][j]);
         }
+    }
+
+    public final boolean completed ()
+    {
+        for (int i = 0; i < MAX_X; i++)
+        {
+            for (int j = 0; j < MAX_Y; j++)
+            {
+                if (!_theBoard[i][j].called())
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     private Cell[][] _theBoard;
