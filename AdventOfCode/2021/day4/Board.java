cb3a8d871bc02bc4dc9b088598d2e2ecee4522dd
Mark Little <markclittle@gmail.com>
2022-12-20 15:01:50 +0000

    Update Board.java

--- AdventOfCode/2021/day4/Board.java
+++ AdventOfCode/2021/day4/Board.java
@@ -5,6 +5,14 @@ public class Board
 
     public Board ()
     {
+        _theBoard = new Cell[MAX_X][MAX_Y];
 
+        for (int i = 0; i < MAX_X; i++)
+        {
+            for (int j = 0; j < MAX_Y; j++)
+                _theBoard[i][j] = null;
+        }
     }
+
+    private Cell[][] _theBoard;
 }
