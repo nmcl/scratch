58e36324aa4f96e8aef8c084119e87dad033e205
Mark Little <markclittle@gmail.com>
2022-12-20 15:20:39 +0000

    Update Util.java

--- AdventOfCode/2021/day4/Util.java
+++ AdventOfCode/2021/day4/Util.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Util
 {
-
     public static Vector<Integer> loadNumbers (String inputFile, boolean debug)
     {
         /*
@@ -25,6 +24,53 @@ public class Util
         catch (Throwable ex)
         {
             ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
+        }
+
+        return results;
+    }
+
+    public static Vector<Board> loadBoards (String inputFile, boolean debug)
+    {
+        /*
+         * Open the data file and read it in.
+         */
+
+        BufferedReader reader = null;
+        Vector<Board> results = new Vector<Board>();
+        
+        try
+        {
+            reader = new BufferedReader(new FileReader(inputFile));
+            String line = reader.readLine();  // ignore first line.
+
+            while ((line = reader.readLine()) != null)
+            {
+                results.add(line);
+            }
+        }
+        catch (Throwable ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (Throwable ex)
+            {
+            }
         }
 
         return results;
