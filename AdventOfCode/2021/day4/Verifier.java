e53f71d27353b10e52c880d2c0a40667b00df99d
Mark Little <markclittle@gmail.com>
2022-12-22 11:41:03 +0000

    Update Verifier.java

--- AdventOfCode/2021/day4/Verifier.java
+++ AdventOfCode/2021/day4/Verifier.java
@@ -2,13 +2,20 @@ import java.util.*;
 
 public class Verifier
 {
+    public static final String EXAMPLE_FILE = "example.txt";
+    
     public Verifier ()
     {
-
+        _debug = debug;
     }
 
     public final boolean verify ()
     {
+        Vector<Integer> numbers = Util.loadNumbers(EXAMPLE_FILE, debug);
+        Vector<Board> boards = Util.loadBoards(EXAMPLE_FILE, debug);
+        
         return false;
     }
+
+    private boolean _debug;
 }
