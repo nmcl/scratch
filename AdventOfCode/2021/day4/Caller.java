e277b87a875f8c97b81097ae50e4510bda0be4fc
Mark Little <markclittle@gmail.com>
2022-12-24 14:36:21 +0000

    Update Caller.java

--- AdventOfCode/2021/day4/Caller.java
+++ AdventOfCode/2021/day4/Caller.java
@@ -9,6 +9,17 @@ public class Caller
 
     public Board playTheGame (Vector<Integer> numbers, Vector<Board> boards)
     {
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            for (int j = 0; j < boards.size(); j++)
+            {
+                boards.elementAt(j).call(numbers.elementAt(i));
+
+                if (boards.elementAt(j).completeLine())
+                    return boards.elementAt(j);
+            }
+        }
+
         return null;
     }
 
