bd7428932f03eba42a868b76dd052597c3c9fba5
Mark Little <markclittle@gmail.com>
2022-12-24 14:17:46 +0000

    Update Caller.java

--- AdventOfCode/2021/day4/Caller.java
+++ AdventOfCode/2021/day4/Caller.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Caller
 {
     public Caller (boolean debug)
     {
         _debug = debug;
+    }
+
+    public Board playTheGame (Vector<Integer> numbers, Vector<Board> boards)
+    {
+        return null;
     }
 
     private boolean _debug;
