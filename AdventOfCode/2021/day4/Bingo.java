c7ec525c4aa5c7c9217e0cc5f5153f23c4885f53
Mark Little <markclittle@gmail.com>
2022-12-27 14:00:04 +0000

    Update Bingo.java

--- AdventOfCode/2021/day4/Bingo.java
+++ AdventOfCode/2021/day4/Bingo.java
@@ -32,6 +32,30 @@ public class Bingo
                 System.out.println("Verified ok.");
             else
                 System.out.println("Verify failed!");
+
+            System.exit(0);
         }
+
+        Vector<Integer> numbers = Util.loadNumbers(DATA_FILE, debug);
+        Vector<Board> boards = Util.loadBoards(DATA_FILE, debug);
+
+        if (debug)
+        {
+            System.out.println("Loaded:\n");
+
+            for (int i = 0; i < numbers.size(); i++)
+                System.out.print(numbers.elementAt(i)+",");
+
+            System.out.println("\n");
+
+            for (int i = 0; i < boards.size(); i++)
+                System.out.println(boards.elementAt(i)+"\n");
+        }
+
+        Caller c = new Caller(debug);
+        Result r = c.playTheGame(numbers, boards);
+
+        if (debug)
+            System.out.println("\nResult:\n\n"+r);
     }
 }
