966456e1f5b192a80432f3ef7a8a0dbe09cda9c0
Mark Little <markclittle@gmail.com>
2022-12-27 12:52:22 +0000

    Create Result.java

---
+++ AdventOfCode/2021/day4/Result.java
@@ -0,0 +1,16 @@
+public class Result
+{
+    public Result (Integer[] line, int number)
+    {
+        _line = line;
+        _number = number;
+    }
+
+    private int getResult ()
+    {
+        
+    }
+
+    private Integer[] _line;
+    private int _number;
+}
