f57a37ca61ce68973c252e144e07f0b90d902627
Mark Little <markclittle@gmail.com>
2022-12-30 18:16:13 +0000

    Update Grid.java

--- AdventOfCode/2021/day5/Grid.java
+++ AdventOfCode/2021/day5/Grid.java
@@ -1,9 +1,17 @@
 public class Grid
 {
-    public Grid (boolean debug)
+    public Grid (int maxX, int maxY, boolean debug)
     {
+        _theGrid = new int[maxX][maxY];
         _debug = debug;
+
+        for (int i = 0; i < maxX; i++)
+        {
+            for (int j = 0; j < maxY; j++)
+                _theGrid[i][j] = 0;
+        }
     }
 
+    private int[][] _theGrid;
     private boolean _debug;
 }
