86775968edc419053e8adc68a3fce51c545825e6
Mark Little <markclittle@gmail.com>
2022-12-30 18:33:23 +0000

    Update Grid.java

--- AdventOfCode/2021/day5/Grid.java
+++ AdventOfCode/2021/day5/Grid.java
@@ -12,6 +12,32 @@ public class Grid
         }
     }
 
+    // only horizontal or vertical lines for part 1
+
+    public void plot (Coordinate start, Coordinate finish)
+    {
+        if (start.getX() == finish.getX())
+        {
+            int startY = ((start.getY() < finish.getY() ? start.getY() : finish.getY()));
+            int finishY = ((start.getY() > finish.getY() ? start.getY() : finish.getY()));
+
+            for (int y = startY; y <= finishY; y++)
+            {
+                _theGrid[start.getX()][y]++;
+            }
+        }
+        else
+        {
+            int startX = ((start.getX() < finish.getX() ? start.getX() : finish.getX()));
+            int finishX = ((start.getX() > finish.getX() ? start.getX() : finish.getX()));
+
+            for (int x = startX; x <= finishX; x++)
+            {
+                _theGrid[x][start.getY()]++;
+            }
+        }
+    }
+
     @Override
     public String toString ()
     {
