7040ac8b58558e0bcfb105b518720b4ea4214e0b
Mark Little <markclittle@gmail.com>
2022-12-30 18:56:33 +0000

    Update Vents.java

--- AdventOfCode/2021/day5/Vents.java
+++ AdventOfCode/2021/day5/Vents.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Vents
 {
     public static final String DATA_FILE = "input.txt";
+    public static final int MAX_X = 1000;
+    public static final int MAX_Y = 1000;
 
     public static void main (String[] args)
     {
@@ -32,8 +34,20 @@ public class Vents
                 System.out.println("Verified ok.");
             else
                 System.out.println("Verify failed!");
-                
+
             System.exit(0);
         }
+
+        Vector<Coordinate> lines = Util.loadCoordinates(DATA_FILE, debug);
+        Grid g = new Grid(MAX_X, MAX_Y, debug);
+
+        for (int i = 0; i < lines.size() -1; i++)
+        {
+            g.plot(lines.elementAt(i), lines.elementAt(i+1));
+
+            i++;
+        }
+
+        System.out.println("Number of overlapping lines: "+g.overlappingLines());
     }
 }
