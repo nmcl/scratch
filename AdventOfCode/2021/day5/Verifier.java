849938f138d89b27d63c2cf7b9e1c95cff3ab752
Mark Little <markclittle@gmail.com>
2022-12-30 18:38:51 +0000

    Update Verifier.java

--- AdventOfCode/2021/day5/Verifier.java
+++ AdventOfCode/2021/day5/Verifier.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Verifier
 {
     public static final String EXAMPLE_DATA = "example.txt";
-    public static final String MAX_X = 9;
-    public static final String MAX_Y = 9;
+    public static final int MAX_X = 9;
+    public static final int MAX_Y = 9;
 
     public Verifier (boolean debug)
     {
@@ -33,7 +33,7 @@ public class Verifier
 
         for (int i = 0; i < lines.size() -1; i++)
         {
-            g.plot(lines.elementAt(i), lines.elementAt(i+1);
+            g.plot(lines.elementAt(i), lines.elementAt(i+1));
 
             i++;
         }
