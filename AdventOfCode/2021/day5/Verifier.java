49ad2ca5874339aef272f3196e8fe4d0dfa0e814
Mark Little <markclittle@gmail.com>
2022-12-30 15:50:15 +0000

    Update Verifier.java

---
+++ AdventOfCode/2021/day5/Verifier.java
@@ -0,0 +1,14 @@
+public class Verifier
+{
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    public final boolean verify ()
+    {
+        return false;
+    }
+    
+    private boolean _debug;
+}
