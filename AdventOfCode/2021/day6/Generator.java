fa22aa123649c7bc1e441dca25de238dd8f9877c
Mark Little <markclittle@gmail.com>
2023-01-04 16:24:53 +0000

    Update Generator.java

--- AdventOfCode/2021/day6/Generator.java
+++ AdventOfCode/2021/day6/Generator.java
@@ -7,5 +7,43 @@ public class Generator
         _debug = debug;
     }
 
+    public void evolve (int days, Vector<Integer> fish)
+    {
+        if (_debug)
+        {
+            System.out.print("Initial state: ");
+
+            for (int j = 0; j < fish.size(); j++)
+                System.out.print(fish.elementAt(j)+",");
+        }
+
+        for (int d = 0; d < days; d++)
+        {
+            for (int i = 0; i < fish.size(); i++)
+            {
+                Integer f = fish.elementAt(i);
+
+                if (f == 0)
+                {
+                    fish.set(i, 6);
+                    
+                    fish.add(8);
+                }
+                else
+                    fish.set(i, f -1);
+            }
+
+            if (_debug)
+            {
+                if (_debug)
+                    System.out.print("After "+(d+1)+" days: ");
+
+                for (int j = 0; j < fish.size(); j++)
+                    System.out.print(fish.elementAt(j)+",");
+            }
+
+        }
+    }
+
     private boolean _debug;
 }
