0354f4f4a0d20e2441035c152b2dc877043d3a11
Mark Little <markclittle@gmail.com>
2023-01-04 15:04:13 +0000

    Update Util.java

--- AdventOfCode/2021/day6/Util.java
+++ AdventOfCode/2021/day6/Util.java
@@ -3,43 +3,23 @@ import java.io.*;
 
 public class Util
 {
-    public static final String DELIMITER = " -> ";
-
-    public static Vector<Coordinate> loadCoordinates (String inputFile, boolean debug)
+    public static Vector<Integer> loadAges (String inputFile, boolean debug)
     {
         /*
          * Open the data file and read it in.
          */
 
         BufferedReader reader = null;
-        Vector<Coordinate> results = new Vector<Coordinate>();
+        Vector<Integer> results = new Vector<Integer>();
         
         try
         {
             reader = new BufferedReader(new FileReader(inputFile));
-            String line = null;
+            String line = reader.readLine();
+            String[] numbers = line.split(",");
 
-            if (debug)
-                System.out.println("Loaded:\n");
-
-            while ((line = reader.readLine()) != null)
-            {
-                int first = line.indexOf(",");
-                int X1 = Integer.parseInt(line.substring(0, first));
-                int second = line.indexOf(DELIMITER);
-                int Y1 = Integer.parseInt(line.substring(first +1, second));
-
-                results.add(new Coordinate(X1, Y1));
-
-                int third = line.lastIndexOf(",");
-                int X2 = Integer.parseInt(line.substring(second + DELIMITER.length(), third));
-                int Y2 = Integer.parseInt(line.substring(third +1));
-
-                if (debug)
-                    System.out.println(X1+","+Y1+DELIMITER+X2+","+Y2);
-
-                results.add(new Coordinate(X2, Y2));
-            }
+            for (int i = 0; i < numbers.length; i++)
+                results.add(Integer.parseInt(numbers[i]));
         }
         catch (Throwable ex)
         {
