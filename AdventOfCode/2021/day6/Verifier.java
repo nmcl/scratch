95df8fe9a5b99a78230fce69149059f697878d0b
Mark Little <markclittle@gmail.com>
2023-01-04 16:43:34 +0000

    Update Verifier.java

--- AdventOfCode/2021/day6/Verifier.java
+++ AdventOfCode/2021/day6/Verifier.java
@@ -5,7 +5,7 @@ public class Verifier
     public static final String EXAMPLE_DATA = "example.txt";
     public static final int NUMBER_OF_FISH_18 = 26;
     public static final int NUMBER_OF_FISH_80 = 5934;
-    
+
     public Verifier (boolean debug)
     {
         _debug = debug;
@@ -15,6 +15,20 @@ public class Verifier
     {
         Vector<Integer> ages = Util.loadAges(EXAMPLE_DATA, _debug);
         Generator g = new Generator(_debug);
+        Vector<Integer> fish = g.evolve(18, ages);
+
+        if (fish.size() == NUMBER_OF_FISH_18)
+        {
+            ages = Util.loadAges(EXAMPLE_DATA, _debug);  // reset
+            fish = g.evolve(80, ages);
+
+            if (fish.size() == NUMBER_OF_FISH_80)
+                return true;
+            else
+                System.out.println("Wrong number of fish after 80 days: "+fish.size());
+        }
+        else
+            System.out.println("Wrong number of fish after 18 days: "+fish.size());
 
         return false;
     }
