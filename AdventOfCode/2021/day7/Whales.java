846a06136f9dd387f9d7e5fdb4c994a2a644c836
Mark Little <markclittle@gmail.com>
2024-01-07 17:54:22 +0000

    Create Whales.java

---
+++ AdventOfCode/2021/day7/Whales.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Whales
+{
+    public static final String DATA_FILE = "data.txt";
+
+    public static void main (String[] args)
+    {
+        boolean verify = false;
+        boolean debug = false;
+
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-help".equals(args[i]))
+            {
+                System.out.println("Usage: [-verify] [-help]");
+                System.exit(0);
+            }
+
+            if ("-debug".equals(args[i]))
+                debug = true;
+
+            if ("-verify".equals(args[i]))
+                verify = true;
+        }
+    }
+}
