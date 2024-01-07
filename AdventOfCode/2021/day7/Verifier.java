4793c03b9f4a8d67a318e5b93b458d97a626e06a
Mark Little <markclittle@gmail.com>
2024-01-07 17:54:20 +0000

    Create Verifier.java

---
+++ AdventOfCode/2021/day7/Verifier.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Verifier
+{
+    public static final String EXAMPLE_DATA = "example.txt";
+
+    public Verifier (boolean debug)
+    {
+        _debug = debug;
+    }
+
+    public final boolean verify ()
+    {
+        return false;
+    }
+}
